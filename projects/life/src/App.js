bb94c9ba08fc00e254659d6cf0d599de5b0e5dad
Sara Fritz <31226863+sarafritz88@users.noreply.github.com>
2018-01-25 16:52:25 -0600

    Update App.js

--- projects/life/src/App.js
+++ projects/life/src/App.js
@@ -7,45 +7,70 @@ import './App.css';
  */
 class LifeCanvas extends Component {
 
-  /**
-   * Constructor
-   */
-  constructor(props) {
-    super(props);
+    /**
+     * Constructor
+     */
+    constructor(props) {
+        super(props);
 
-    this.life = new Life(props.width, props.height);
-    this.life.randomize();
-  }
+        this.life = new Life(props.width, props.height);
+        this.life.randomize();
+    }
 
-  /**
-   * Component did mount
-   */
-  componentDidMount() {
-    requestAnimationFrame(() => {this.animFrame()});
-  }
+    /**
+     * Component did mount
+     */
+    componentDidMount() {
+        requestAnimationFrame(() => {this.animFrame()});
+    }
 
-  /**
-   * Handle an animation frame
-   */
-  animFrame() {
-    //
-    // !!!! IMPLEMENT ME !!!!
-    //
+    /**
+     * Handle an animation frame
+     */
+    animFrame() {
+        let width = this.props.width;
+        let height = this.props.height;
 
-    // Request another animation frame
-    // Update life and get cells
-    // Get canvas framebuffer, a packed RGBA array
-    // Convert the cell values into white or black for the canvas
-    // Put the new image data back on the canvas
-    // Next generation of life
-  }
+        // Update life and get cells
+        let cells = this.life.getCells();
 
-  /**
-   * Render
-   */
-  render() {
-    return <canvas ref="canvas" width={this.props.width} height={this.props.height} />
-  }
+        // Get canvas framebuffer, a packed RGBA array
+        let canvas = this.refs.canvas;
+        let ctx = canvas.getContext('2d');
+        let imageData = ctx.getImageData(0, 0, width, height);
+
+        // Convert the cell values into white or black for the canvas
+        for (let y = 0; y < height; y++) {
+            for (let x = 0; x < width; x++) {
+
+                let index = (y * width + x) * 4;                //RGBAlpha
+
+                let lifeStatus = cells[y][x];
+                let color = lifeStatus === 0? 0x00: 0xff;
+
+                imageData.data[index + 0] = color;
+                imageData.data[index + 1] = color;
+                imageData.data[index + 2] = color;
+                imageData.data[index + 3] = 0xff;
+            }
+        }
+
+        // Put the new image data back on the canvas
+        ctx.putImageData(imageData, 0, 0);
+
+        // Next generation of life
+        this.life.step();
+
+        // Request another animation frame
+        requestAnimationFrame(() => {this.animFrame()});
+    }
+
+    /**
+     * Render
+     */
+    render() {
+        return <canvas ref="canvas" width={this.props.width} height={this.props.height} />
+    }
 }
 
 /**
