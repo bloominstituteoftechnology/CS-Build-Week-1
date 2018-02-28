8111f485a41ae5990be0e1f438b28fcc1e972156
BonnW <pwonghansa@gmail.com>
2018-02-28 17:02:51 -0600

    CCA guided demo code added

--- guided-demo/cca/src/cca.js
+++ guided-demo/cca/src/cca.js
@@ -29,6 +29,12 @@ class CCA {
     this.width = width;
     this.height = height;
 
+    this.currentBufferIndex = 0;
+    this.buffer = [
+      Array2D(width, height),
+      Array2D(width, height)
+    ];
+
     this.clear();
   }
 
@@ -38,24 +44,59 @@ class CCA {
    * This should NOT be modified by the caller
    */
   getCells() {
+    return this.buffer[this.currentBufferIndex];
   }
 
   /**
    * Clear the cca grid
    */
   clear() {
+    for (let y = 0; y < this.height; y++) {
+      this.buffer[this.currentBufferIndex][y].fill(0);
+    }
   }
 
   /**
    * Randomize the cca grid
    */
   randomize() {
+    for (let y = 0; y < this.height; y++) {
+      for (let x = 0; x < this.width; x++) {
+        const rand = Math.floor(Math.random() * MODULO);
+        this.buffer[this.currentBufferIndex][y][x] = rand;
+      }
+    }
   }
 
   /**
    * Run the simulation for a single step
    */
   step() {
+    // Fill the offscreen buffer with the next cca generation
+    // from the current buffer.
+
+    let backBufferIndex = this.currentBufferIndex === 0 ? 1 : 0;
+    let currentBuffer = this.buffer[this.currentBufferIndex];
+    let backBuffer = this.buffer[backBufferIndex];
+
+    // See if we have a neighbor to infect this one
+    function hasInfectiousNeighbor(x, y) {
+      const nextvalue = (currentBuffer[y][x] = 1) % MODULO;
+
+      // Check the west neighbor of cell x, y
+      if (x > 0) {
+        if (currentBuffer[y][x - 1] === nextValue) {
+          return true;
+        }
+      }
+
+      // North
+      if (y > 0) {
+        if (currentBuffer[y - 1][x] === nextValue) {
+          return true;
+        }
+      }
+    }
   }
 }
 
