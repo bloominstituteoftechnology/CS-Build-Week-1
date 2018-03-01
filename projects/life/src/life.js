433e0467838ecb7ccb3aa7a52c1a00428c5ee195
Rashmi Baheti <baheti.rash123@gmail.com>
2018-03-01 14:46:23 -0800

    complete, but not working as expected

--- projects/life/src/life.js
+++ projects/life/src/life.js
@@ -25,6 +25,17 @@ class Life {
    */
   constructor(width, height) {
     // !!!! IMPLEMENT ME !!!!
+    this.width = width;
+    this.height = height;
+
+    this.currentBufferIndex = 0;
+
+    // Allocate the double buffer
+    this.buffer = [
+      Array2D(width, height),
+      Array2D(width, height)
+    ];
+    this.clear();
   }
   
   /**
@@ -34,6 +45,7 @@ class Life {
    */
   getCells() {
     // !!!! IMPLEMENT ME !!!!
+    return this.buffer[this.currentBufferIndex];
   }
 
   /**
@@ -41,6 +53,9 @@ class Life {
    */
   clear() {
     // !!!! IMPLEMENT ME !!!!
+    for(let y = 0; y < this.height; ++y) {
+      this.buffer[this.currentBufferIndex][y].fill(0);
+    }
   }
   
   /**
@@ -48,6 +63,11 @@ class Life {
    */
   randomize() {
     // !!!! IMPLEMENT ME !!!!
+    for(let y = 0; y < this.height; ++y) {
+      for(let x = 0; x < this.width; ++x) {
+        this.buffer[this.currentBufferIndex][y][x] = Math.floor(Math.random() * 2);
+      }
+    }
   }
 
   /**
@@ -55,6 +75,109 @@ class Life {
    */
   step() {
     // !!!! IMPLEMENT ME !!!!
+    let backBufferIndex = this.currentBufferIndex === 0 ? 1 : 0;
+    let currentBuffer = this.buffer[this.currentBufferIndex];
+    let backBuffer = this.buffer[backBufferIndex];
+
+    function countNeighbors(x, y, options={border:'zero'}) { // options is optional
+      let neighborCount = 0;
+      // count the neighbors
+      if(options.border === 'zero') {
+        // Treat out-of-bounds as zero (dead)
+        for(let yOffset = -1; yOffset <= 1; yOffset++) {
+          const yPos = y + yOffset;
+          if(yPos < 0 || yPos >= this.height) {
+            // Out of bounds
+            continue;
+          }
+
+          for(let xOffset = -1; xOffset <= 1; xOffset++) {
+            const xPos = x + xOffset;
+            if(yPos < 0 || xPos >= this.width) {
+              // Out of bounds
+              continue;
+            } 
+
+            if(xPos === x && yPos === y) {
+              // you can't be ur own neighbor
+              continue;
+            }
+
+            neighborCount += currentBuffer[yPos][xPos];
+          }
+
+        }
+
+      } else if(options.border === 'wrap') {
+        // wrap-around (check neighbors on the other far side)
+        let north = y - 1;
+        let south = y + 1;
+        let west = x - 1;
+        let east = x + 1;
+
+        // check if we need to wrap
+        // North
+        if(north < 0) {
+          north = this.height - 1;
+        }
+
+        // south
+        if(south >= this.height) {
+          south = 0;
+        }
+
+        // west
+        if(west < 0) {
+          west = this.width-1;
+        }
+
+        // east
+        if(east >= this.width) {
+          east = 0;
+        }
+
+        neighborCount = currentBuffer[north][west] + 
+                        currentBuffer[north][x] + 
+                        currentBuffer[north][east] + 
+                        currentBuffer[south][west] + 
+                        currentBuffer[south][x] + 
+                        currentBuffer[south][east] + 
+                        currentBuffer[y][west] + 
+                        currentBuffer[y][east]
+
+      } else {
+        throw new Error('unknown border option' + options.border);
+      }
+
+      return neighborCount;
+    }
+
+    // Loop through and decide if the next generation is alive or dead
+    // for each cell processed
+    for(let y = 0; y < this.height; ++y) {
+      for(let x = 0; x < this.width; ++x) {
+        // Game of Life rules
+        const neighbors = countNeighbors(x, y);
+        const cell = currentBuffer[y][x];
+
+        if(cell === 1) {
+          // Current alive
+          if (neighbors > 3 || neighbors < 2) {
+            backBuffer[y][x] = 0;
+          } else {
+            backBuffer[y][x] = 1;
+          }
+        } else {
+          // currently dead
+          if(neighbors === 3) {
+            backBuffer[y][x] = 1;
+          } else {
+            backBuffer[y][x] = 0;
+          }
+        }
+      }
+    }
+    this.currentBufferIndex = this.backBufferIndex;
   }
 }
 
