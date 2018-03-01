8a83b163a489dcac8b6bf2083e82af2f4a45d2b8
Austin F <austindanielfrench@gmail.com>
2018-02-28 22:43:23 -0800

    guided demo

--- guided-demo/cca/src/App.js
+++ guided-demo/cca/src/App.js
@@ -10,37 +10,73 @@ const COLORS = [
   [0, 0x5f, 0x7f],
   [0x5f, 0x8f, 0x7f],
   [0x8f, 0xff, 0x7f],
-  [0xff, 0x5f, 0x7f],
-]
+  [0xff, 0x5f, 0x7f]
+];
 
 /**
  * CCA canvas
  */
 class CCACanvas extends Component {
-
   /**
    * Constructor
    */
   constructor(props) {
     super(props);
+    this.cca = new CCA(props.width, props.height);
+    this.cca.randomize();
   }
 
   /**
    * Component did mount
    */
   componentDidMount() {
+    requestAnimationFrame(() => {
+      this.animFrame();
+    });
   }
 
   /**
    * Handle an animation frame
    */
   animFrame() {
+    const cells = this.cca.getCells();
+
+    const canvas = this.refs.canvas;
+    let ctx = canvas.getContext('2d');
+    let imageData = ctx.getImageData(0, 0, width, height);
+
+    for (let y = 0; y < height; y++) {
+      for (let x = 0; x < width; x++) {
+        const state = cells[y][x];
+        const color = COLORS[state];
+        const index = (y * width + x) * 4;
+
+        imageData.data[index + 0] = color[0];
+        imageData.data[index + 1] = color[1];
+        imageData.data[index + 2] = color[2];
+        imageData.data[index + 3] = 0xff;
+      }
+    }
+    ctx.putImageData(imageData, 0, 0);
+
+    this.cca.step();
+
+    requestAnimationFrame(() => {
+      this.animFrame();
+    });
   }
 
   /**
    * Render
    */
   render() {
+    return (
+      <canvas
+        ref="canvas"
+        width={this.props.width}
+        height={this.props.height}
+      />
+    );
   }
 }
 
@@ -48,7 +84,6 @@ class CCACanvas extends Component {
  * CCA holder component
  */
 class CCAApp extends Component {
-
   /**
    * Render
    */
@@ -57,7 +92,7 @@ class CCAApp extends Component {
       <div>
         <CCACanvas width={400} height={300} />
       </div>
-    )
+    );
   }
 }
 
@@ -65,7 +100,6 @@ class CCAApp extends Component {
  * Outer App component
  */
 class App extends Component {
-
   /**
    * Render
    */
