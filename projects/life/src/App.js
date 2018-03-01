590a825cf3bb44090750fc1178d4c04a861c4090
invegat <mark.c.oliver@gmail.com>
2018-03-01 14:41:09 -0500

    some working

--- projects/life/src/App.js
+++ projects/life/src/App.js
@@ -5,8 +5,24 @@ import './App.css';
 /**
  * Life canvas
  */
+// const demoCOLORS = [
+//   [0, 0, 0],
+//   [0x8f, 0, 0x5f],
+//   [0x5f, 0, 0x8f],
+//   [0, 0, 0xff],
+//   [0, 0x5f, 0x7f],
+//   [0x5f, 0x8f, 0x7f],
+//   [0x8f, 0xff, 0x7f],
+//   [0xff, 0x5f, 0x7f],
+// ]
+const COLORS = [
+  [0xFF,0xFF,0xFF],
+  [0,0,0]
+]
+
 class LifeCanvas extends Component {
 
+  
   /**
    * Constructor
    */
@@ -15,13 +31,24 @@ class LifeCanvas extends Component {
 
     this.life = new Life(props.width, props.height);
     this.life.randomize();
+    this.props.update(this.stopStart, this.addGlider, this.clear, this.gliderGun);
+    this.pause = false;
+;
   }
+  addBlock = (event) => {
+    // return { x: event.offsetX, y: event.offsetY };
 
+    const x = event.offsetX;
+    const y = event.offsetY;
+    // console.log(`clicked on x: ${x} y: ${y}`);
+    this.addGlider(Math.floor(x/2),Math.floor(y/2));
+  }
   /**
    * Component did mount
    */
   componentDidMount() {
     requestAnimationFrame(() => {this.animFrame()});
+    this.refs.canvas.addEventListener('click', this.addBlock, false)
   }
 
   /**
@@ -38,13 +65,47 @@ class LifeCanvas extends Component {
     // Convert the cell values into white or black for the canvas
     // Put the new image data back on the canvas
     // Next generation of life
+    const cells = this.life.getCells();
+    const canvas = this.refs.canvas;
+    const height = this.props.height;
+    const width = this.props.width;
+    let ctx = canvas.getContext("2d");
+    let imageData = ctx.getImageData(0,0,width,height);
+    for (let y = 0;y<height;y++) {
+      for (let x = 0;x<width;x++) {
+        const state = cells[y][x] % COLORS.length;
+        const color = COLORS[state];
+        const index = (y * width  + x) * 4;
+        imageData.data[index + 0] = color[0]
+        imageData.data[index + 1] = color[1]
+        imageData.data[index + 2] = color[2]   
+        imageData.data[index + 3] = 0xFF// state > 0 ? 0xff : 0;             
+      }
+    }
+    ctx.putImageData(imageData, 0, 0);
+    this.life.step();
+    if (!this.pause)
+      requestAnimationFrame(() => {this.animFrame()});    
   }
-
+  stopStart = () => {
+    this.pause = !this.pause
+    if (!this.pause)
+      requestAnimationFrame(() => {this.animFrame()});
+  }
+  addGlider = (x=0,y=0) => {
+    this.life.addGLider(x,y);
+  }
+  clear = () => {
+    this.life.clear();
+  }
+  gliderGun = (x=0, y=0) => {
+    this.life.glider_gun(x, y);
+  }
   /**
    * Render
    */
   render() {
-    return <canvas ref="canvas" width={this.props.width} height={this.props.height} />
+    return <canvas ref="canvas" width={this.props.width} height={this.props.height}  />
   }
 }
 
@@ -52,15 +113,50 @@ class LifeCanvas extends Component {
  * Life holder component
  */
 class LifeApp extends Component {
+   constructor(props) {
+     super(props);
+     this.state = {
+       isPausedPrompt: 'Pause'
+     }
+     this.stopStart = null;
+     this.glider = null; 
+     this.clear = null;    
+   }
 
+  setupUpdate = (ss, gl,cl, gg) => {
+    this.stopStart = ss;
+    this.glider = gl;
+    this.clear = cl;
+    this.gliderGun = gg;
+  }
+  clearCanvas = () => {
+    this.clear();
+  }
+  startGlider = () => {
+    this.glider();
+  }
+  startGliderGun = () => {
+    this.gliderGun();
+  }   
+  pause = () => {
+     this.setState({
+       isPausedPrompt: this.state.isPausedPrompt === 'Pause' ? 'Start' : 'Pause'
+     });
+     this.stopStart();
+   }
   /**
    * Render
    */
   render() {
     return (
-      <div>
-        <LifeCanvas width={400} height={300} />
-      </div>
+      <form>
+        <LifeCanvas id='life' width={400} height={300} update={this.setupUpdate} />
+        <button type='button' onClick={this.pause}>{this.state.isPausedPrompt}</button>
+        <button type='button' onClick={this.startGlider}>Start Glider</button>
+        <button type='button' onClick={this.startGliderGun}>Start Glider Gun</button> 
+        <button type='button' onClick={this.clearCanvas}>Clear</button>
+        <input type='submit' value='Restart' />
+      </form>
     )
   }
 }
--- projects/life/src/life.js
+++ projects/life/src/life.js
@@ -1,61 +1,248 @@
 /**
- * Implementation of Conway's game of Life
+ * Implemention of a Life
  */
+
+const MODULO = 2;
 
 /**
  * Make a 2D array helper function
  */
 function Array2D(width, height) {
-  let a = new Array(height);
+    let a = new Array(height);
 
-  for (let i = 0; i < height; i++) {
-    a[i] = new Array(width);
-  }
+    for (let i = 0; i < height; i++) {
+        a[i] = new Array(width);
+    }
 
-  return a;
+    return a;
 }
+const glider_gun_text = `
+........................*
+......................*.*
+............**......**............**
+...........*...*....**............**
+**........*.....*...**
+**........*...*.**....*.*
+..........*.....*.......*
+...........*...*
+............**
+`
 
 /**
  * Life class
  */
 class Life {
 
-  /**
-   * Constructor
-   */
-  constructor(width, height) {
-    // !!!! IMPLEMENT ME !!!!
-  }
-  
-  /**
-   * Return the current active buffer
-   * 
-   * This should NOT be modified by the caller
-   */
-  getCells() {
-    // !!!! IMPLEMENT ME !!!!
-  }
+    /**
+     * Constructor
+     */
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+        this.currentBufferIndex = 0;
+        this.buffer = [
+            Array2D(width, height),
+            Array2D(width, height)
+        ];
+        this.clear();
+        this.noExclusion = [-1, -1]
+        this.exlusionForGun = [79, 22]
+        this.exclusionZone = this.noExclusion;
+        this.isGun = false
+    }
+    rotate = (x, y, radians) => {
+        const ratio = Math.tan(y,x)  /  Math.tan(Math.atan(y, x) + radians);  // ny/nx    nx = x * (old ratio) / (ratio)
+        x *= ratio
+        y /= ratio;
+        return [x, y]
+    }
+    rotate_glider_gun = () => {
+        this.clear();
+        const lines = glider_gun_text.split('\n');
+        console.log(`gun lines: ${lines.length}`);
+        const y = Math.floor((this.height - lines.length) / 2);
+        const x = Math.floor((this.width - 37) / 2);
+        const currentBuffer = this.buffer[this.currentBufferIndex];
+        lines.forEach((t, l) => {
+            for (let i = 0; i < t.length; i++) {
+                const [rx, ry] = this.rotate(i, l, Math.PI / 4);
+                if (y + ry > this.height)
+                    console.log(`height out of bounds y: ${y}  ry: ${ry}`);
+                if (x + rx > this.width)
+                    console.log(`width out of bounds x: ${x}  rx: ${rx}`);
+                if (y + ry <= 0)
+                    console.log(`height out of bounds y: ${y}  ry: ${ry}`);
+                if (x + rx <= 0)
+                    console.log(`width out of bounds x: ${x}  rx: ${rx}`);
+                if (x + rx === 181) {
+                    console.log(`height out of bounds? y: ${y}  ry: ${ry} l: ${l}`);
+                    console.log(`width out of bounds? x: ${x}  rx: ${rx} i: ${i}`);
+                }
+                currentBuffer[y + ry][x + rx] = (t[i] === '*' ? 1 : 0);
+            }
+        });
+    }
+    glider_gun = (x, y) => {
 
-  /**
-   * Clear the life grid
-   */
-  clear() {
-    // !!!! IMPLEMENT ME !!!!
-  }
-  
-  /**
-   * Randomize the life grid
-   */
-  randomize() {
-    // !!!! IMPLEMENT ME !!!!
-  }
+        //return this.rotate_glider_gun();
+        // this.clear();
+        const lines = glider_gun_text.split('\n');
+        // console.log(`gun lines: ${lines.length}`);
+        y = y | Math.floor((this.height - lines.length) / 20);
+        x = x | Math.floor((this.width - 37) / 20);
 
-  /**
-   * Run the simulation for a single step
-   */
-  step() {
-    // !!!! IMPLEMENT ME !!!!
-  }
+        const currentBuffer = this.buffer[this.currentBufferIndex];
+        this.exclusionZone = this.exlusionForGun;
+        let [ex, ey] = this.exclusionZone;
+        // ex += x;
+        // ey += y;
+        // this.exclusionZone = [ex, ey];
+        for (let l = 0; l < ey; l++)
+            for (let c = 0; c < ex; c++)
+                currentBuffer[l][c] = 0;
+        lines.forEach((t, l) => {
+            const L = y + l;
+            for (let i = 0; i < t.length; i++) {
+                currentBuffer[L][x + i] = (t[i] === '*' ? 1 : 0);
+            }
+        });
+        this.isGun = true;
+        this.stepCount = 0;
+    };
+    /**
+     * Return the current active buffer
+     * 
+     * This should NOT be modified by the caller
+     */
+    getCells() {
+        return this.buffer[this.currentBufferIndex];
+    }
+
+    /**
+     * Clear the Life grid
+     */
+    clear() {
+        this.buffer[this.currentBufferIndex].forEach((row, y) => row.fill(0));//this.buffer[this.currentBufferIndex][y].fill(0));
+    }
+    addGLider(x, y) {
+        let currentBuffer = this.buffer[this.currentBufferIndex];
+        if (x === 0 || x === undefined)
+            this.clear();
+        // console.log(` addGlider input x: ${x} y: ${y}`);
+        if (x === 0 || x === undefined) {
+            x = Math.floor(Math.random() * (this.width - 8)) + 4;
+            y = Math.floor(Math.random() * (this.height - 8)) + 4;
+        }
+        try {
+            currentBuffer[y][x + 2] = 1;
+            currentBuffer[y + 1][x + 2] = 1;
+            currentBuffer[y + 2][x + 2] = 1;
+            currentBuffer[y + 2][x + 1] = 1;
+            currentBuffer[y + 1][x] = 1;
+        }
+        catch (error) {
+            console.log(` addGlider error x: ${x} y: ${y}`);
+        }
+    }
+
+    /**
+     * Randomize the Life grid
+     */
+    randomize() {
+        this.forEachCell((cell) => { return Math.floor(Math.random() * MODULO) })
+    }
+    forEachCell = (f) => {
+        this.buffer[this.currentBufferIndex].forEach((row, yi) => {
+            row.forEach((cell, xi) =>
+                this.buffer[this.currentBufferIndex][yi][xi] = f(cell, xi, yi)
+            )
+        })
+    }
+    /**
+     * Run the simulation for a single step
+     */
+    step() {
+        let backBufferIndex = this.currentBufferIndex === 0 ? 1 : 0;
+        let currentBuffer = this.buffer[this.currentBufferIndex];
+        let backBuffer = this.buffer[backBufferIndex];
+        const [ex, ey] = this.exclusionZone;
+        const sendGun = Math.floor(Math.random() * 1.1);
+        if (sendGun === 0 && this.isGun && (this.stepCount % 45) === 0)
+            this.glider_gun();
+        this.stepCount++;
+        const isAlive = (cell, x, y) => {
+            const wrap = (v, d) => {
+                if (v < 0)
+                    return d + v;
+                if (v >= d)
+                    return v - d;
+                return v;
+            }
+            let n = 0;
+            let after = 0
+            for (let i = y - 1; i < y + 2; i++)
+                for (let j = x - 1; j < x + 2; j++) {
+                    if (i === y && j === x)
+                        continue;
+                    const I = wrap(i, this.height)
+                    const J = wrap(j, this.width)
+                    if (currentBuffer[I][J] > 0)
+                        n++;
+                }
+            // const ln = n;
+            // n = 0;
+            // if (x > 0 && currentBuffer[y][x - 1] > 0)
+            //     n++;
+            // if (y > 0 && currentBuffer[y - 1][x] > 0)
+            //     n++;
+            // if (x > 0 && y > 0 && currentBuffer[y - 1][x - 1] > 0)
+            //     n++;
+            // if (x < this.width - 1 && currentBuffer[y][x + 1] > 0) {
+            //     n++;
+            //     after++
+            // }
+            // if (y < this.height - 1 && currentBuffer[y + 1][x] > 0) {
+            //     n++;
+            //     after++
+            // }
+            // if (x < this.width - 1 && y < this.height - 1 && currentBuffer[y + 1][x + 1] > 0) {
+            //     n++;
+            //     after++;
+            // }
+            // if (x > 0 && y < this.height - 1 && currentBuffer[y + 1][x - 1] > 0) {
+            //     after++;
+            //     n++
+            // }
+            // if (x < this.width - 1 && y > 0 && currentBuffer[y - 1][x + 1] > 0) {
+            //     after++;
+            //     n++
+            // }
+            // console.log(`ln: ${ln}  n: ${n}`);
+            switch (n) {
+                case 0:
+                case 1: return false
+                case 2: return cell > 0
+                case 3: return (cell > 0 ? true :
+                    () => {
+                        if (x <= ex && y <= ey && after > 0)
+                            return false;
+                        if (x <= 1 && y <= ey)
+                            return false
+                        if (y <= 1 && x <= ex)
+                            return false
+                        return true;
+                    })
+
+                default: return false
+            }
+
+        }
+        this.forEachCell((cell, x, y) => {
+            backBuffer[y][x] = isAlive(cell, x, y) ? 1 : 0;
+            return cell;
+        });
+        this.currentBufferIndex = this.currentBufferIndex === 0 ? 1 : 0;
+    }
 }
 
 export default Life;
