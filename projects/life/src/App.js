9c4b70bae66d2e06e4d9f4ecac84fdc6ba98cf5b
sulemc <susanna.mcdo@gmail.com>
2018-03-01 14:41:22 -0600

    working basic

--- projects/life/src/App.js
+++ projects/life/src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 
 /**
  * Life canvas
+ * 
+ * 
  */
+const COLORS = [[255,255,255], [0,0,0]];
+
 class LifeCanvas extends Component {
 
   /**
@@ -28,9 +32,35 @@ class LifeCanvas extends Component {
    * Handle an animation frame
    */
   animFrame() {
-    //
-    // !!!! IMPLEMENT ME !!!!
-    //
+    const cells = this.life.getCells();
+    const height = this.props.height;
+    const width = this.props.width;
+    //Get canvas framebuffer, a packed RGBA array
+
+    const canvas = this.refs.canvas;
+    let ctx = canvas.getContext('2d');
+    let imageData = ctx.getImageData(0, 0, width, height);
+
+    //uppdate image Data based on the cells
+    for (let y = 0; y < height; y++) {
+      for (let x = 0; x < width; x ++) {
+        const state = cells[y][x];
+        const color = COLORS[state]
+        const index = (y * width + x) * 4;
+        imageData.data[index + 0] = color[0] // red
+        imageData.data[index + 1] = color[1] // green
+        imageData.data[index + 2] = color[2] //blue
+        imageData.data[index + 3] = 0xff // alpha   
+      }
+    }
+//put the new image data back on the canvas
+ctx.putImageData( imageData, 0, 0);
+
+//itterate next step
+this.life.step();
+
+// request another animation frame
+requestAnimationFrame(() => {this.animFrame()});
 
     // Request another animation frame
     // Update life and get cells
--- projects/life/src/life.js
+++ projects/life/src/life.js
@@ -24,7 +24,16 @@ class Life {
    * Constructor
    */
   constructor(width, height) {
-    // !!!! IMPLEMENT ME !!!!
+    this.width = width;
+    this.height = height;
+
+    this.currentBufferIndex = 0;
+    this.buffer = [
+    Array2D(width, height),
+    Array2D(width, height)
+    ];
+
+    this.clear();
   }
   
   /**
@@ -33,28 +42,116 @@ class Life {
    * This should NOT be modified by the caller
    */
   getCells() {
-    // !!!! IMPLEMENT ME !!!!
+    return this.buffer[this.currentBufferIndex];
   }
 
   /**
    * Clear the life grid
    */
   clear() {
-    // !!!! IMPLEMENT ME !!!!
+    for (let y = 0; y < this.height; y++){
+      this.buffer[this.currentBufferIndex][y].fill(0);
+    }
   }
   
   /**
    * Randomize the life grid
    */
   randomize() {
-    // !!!! IMPLEMENT ME !!!!
+    for (let y = 0; y < this.height; y++){
+      for (let x = 0; x < this.width; x++) {
+        const rand = Math.floor(Math.random() * 2);
+        this.buffer[this.currentBufferIndex][y][x] = rand;
+      }
+    }
   }
 
   /**
    * Run the simulation for a single step
    */
   step() {
-    // !!!! IMPLEMENT ME !!!!
+    let backBufferIndex = this.currentBufferIndex === 0 ? 1 : 0;
+    let currentBuffer = this.buffer[this.currentBufferIndex];
+    let backBuffer = this.buffer[backBufferIndex];
+
+    // See if we have a neighbor to infect this one
+    function hasInfectiousNeighbor(x,y) {
+      let count = 0;
+
+
+      // check the west neighbor of cell x,y
+      if (x > 0) {
+      if (currentBuffer[y][x - 1] === 1){
+        count++;
+      }
+    }
+      // north
+    if ( y > 0) {
+      if (currentBuffer[y-1][x] === 1) {
+        count++;
+      }
+    }
+    // east
+    if ( x < this.width -1 ){
+      if (currentBuffer[y][x+1] === 1) {
+        count++;
+      }
+    }
+    // south
+    if ( y < this.height -1) {
+      if (currentBuffer[y + 1][x] === 1) {
+        count++;
+      }
+    }
+   // southwest
+    if ( y < this.height - 1 && x > 0){
+      if(currentBuffer[y+1][x-1] === 1){
+        count++;
+      }
+    }
+   // southeast
+   if (y < this.height - 1 && x < this.width - 1){
+     if (currentBuffer[y+1][x+1] === 1){
+       count++;
+     }
+   }
+   // northwest
+   if (y > 0 && x > 0){
+     if(currentBuffer[y-1][x-1] === 1){
+       count++;
+     }
+   }
+   // northeast
+   if (y > 0 && x < this.width - 1){
+     if(currentBuffer[y-1][x+1]){
+       count++;
+     }
+   }
+    return count;
+  }
+    //Loop through and decide the state of the next geneeation for each cell processed.
+
+    for(let y = 0; y < this.height; y++) {
+      for (let x = 0; x < this.width; x++) {
+        let result = hasInfectiousNeighbor.call(this, x, y);
+        if (result < 2 || result > 3){
+            backBuffer[y][x] = 0;
+        }
+        else if (currentBuffer[y][x] === 0 && result === 3){
+          backBuffer[y][x] = 1;
+        }
+        else if (currentBuffer[y][x] === 1 && result === 2){
+          backBuffer[y][x] = 1;
+        }
+        else if (currentBuffer[y][x] === 1 && result === 3){
+          backBuffer[y][x] = 1;
+        }
+        else {
+          backBuffer[y][x] = currentBuffer[y][x];
+        }
+      }
+    }
+    this.currentBufferIndex = this.currentBufferIndex === 0? 1 : 0;
   }
 }
 
