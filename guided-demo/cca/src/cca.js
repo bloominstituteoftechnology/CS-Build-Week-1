b0850d93d4b1aec943c798226b2ca97c5058813a
Cassidy SN Avery <tacolim@gmail.com>
2018-03-01 19:19:27 -0500

    it's alive

--- guided-demo/cca/src/cca.js
+++ guided-demo/cca/src/cca.js
@@ -29,6 +29,13 @@ class CCA {
     this.width = width;
     this.height = height;
 
+    this.currentBufferIndex = 0;
+
+    this.buffer = [
+      Array2D(width, height),
+      Array2D(width, height)
+    ];
+
     this.clear();
   }
 
@@ -38,24 +45,86 @@ class CCA {
    * This should NOT be modified by the caller
    */
   getCells() {
+    return this.buffer[this.currentBufferIndex];
   }
 
   /**
-   * Clear the cca grid
+   * Clear the cca grid of the current active buffer
    */
   clear() {
+    for (let y = 0; y < this.height; y++) /* looping over height */{
+      this.buffer[this.currentBufferIndex][y].fill(0);
+    }
   }
 
   /**
    * Randomize the cca grid
    */
   randomize() {
+    for (let y = 0; y < this.height; y++) /* looping over height */{
+      for (let x = 0; x < this.width; x++) /* looping over width */{
+        const rand = Math.floor(Math.random() * MODULO);
+        this.buffer[this.currentBufferIndex][y][x] = rand;
+      }
+    }
   }
 
   /**
    * Run the simulation for a single step
    */
   step() {
+    // Fill the offscreen buffer with the next cca generation built from current buffer
+    let backBufferIndex = this.currentBufferIndex === 0 ? 1 : 0;
+    let currentBuffer = this.buffer[this.currentBufferIndex];
+    let backBuffer = this.buffer[backBufferIndex];
+
+    // See if we have a neighbor to infect this one
+    function hasInfectiousNeighbor(x, y) {
+      const nextValue = (currentBuffer[y][x] + 1) % MODULO;
+
+      // check west neighbor
+      if (x > 0) {
+        if (currentBuffer[y][x - 1] === nextValue) {
+          return true;
+        }
+      }
+
+      // north
+      if (y > 0) {
+        if (currentBuffer[y-1][x] === nextValue) {
+          return true;
+        }
+      }
+
+      // east
+      if (x < this.width - 1) {
+        if (currentBuffer[y][x + 1] === nextValue) {
+          return true;
+        }
+      }
+
+      if (y < this.height - 1) {
+        if (currentBuffer[y + 1][x] === nextValue) {
+          return true;
+        }
+      }
+
+      return false;
+    }
+
+    // loop thru and decide the state of next gerneration for each cell processed
+    for (let y = 0; y < this.height; y++) /* looping over height */{
+      for (let x = 0; x < this.width; x++) /* looping over width */{
+        if (hasInfectiousNeighbor.call(this, x, y)) {
+          backBuffer[y][x] = (currentBuffer[y][x] + 1) % MODULO;
+        } else {
+          backBuffer[y][x] = currentBuffer[y][x];
+        }
+      }
+    }
+
+    this.currentBufferIndex = this.currentBufferIndex === 0 ? 1 : 0;
+
   }
 }
 
--- projects/life/src/life.js
+++ projects/life/src/life.js
@@ -25,6 +25,17 @@ class Life {
    */
   constructor(width, height) {
     // !!!! IMPLEMENT ME !!!!
+    this.width = width;
+    this.height = height;
+
+    this.currentBufferIndex = 0;
+
+    this.buffer = [
+      Array2D(width, height),
+      Array2D(width, height)
+    ];
+
+    this.clear();
   }
   
   /**
@@ -34,6 +45,7 @@ class Life {
    */
   getCells() {
     // !!!! IMPLEMENT ME !!!!
+    return this.buffer[this.currentBufferIndex];
   }
 
   /**
@@ -41,6 +53,9 @@ class Life {
    */
   clear() {
     // !!!! IMPLEMENT ME !!!!
+    for (let y = 0; y < this.height; y++) /* looping over height */{
+      this.buffer[this.currentBufferIndex][y].fill(0);
+    }
   }
   
   /**
@@ -48,6 +63,12 @@ class Life {
    */
   randomize() {
     // !!!! IMPLEMENT ME !!!!
+    for (let y = 0; y < this.height; y++) /* looping over height */{
+      for (let x = 0; x < this.width; x++) /* looping over width */{
+        const rand = Math.floor(Math.random() * 2);
+        this.buffer[this.currentBufferIndex][y][x] = rand;
+      }
+    }
   }
 
   /**
@@ -55,6 +76,101 @@ class Life {
    */
   step() {
     // !!!! IMPLEMENT ME !!!!
+    let backBufferIndex = this.currentBufferIndex === 0 ? 1 : 0;
+    let currentBuffer = this.buffer[this.currentBufferIndex];
+    let backBuffer = this.buffer[backBufferIndex];
+
+    // See if we have a neighbor to infect this one
+    function countNeighbors(x, y) {
+      let livingCount = 0;
+
+      // check west neighbor
+      if (x > 0) {
+        if (currentBuffer[y][x - 1] === 1) {
+          livingCount++;
+        }
+      }
+
+      // north
+      if (y > 0) {
+        if (currentBuffer[y-1][x] === 1) {
+          livingCount++;
+        }
+      }
+
+      // east
+      if (x < this.width - 1) {
+        if (currentBuffer[y][x + 1] === 1) {
+          livingCount++;
+        }
+      }
+      
+      // south
+      if (y < this.height - 1) {
+        if (currentBuffer[y + 1][x] === 1) {
+          livingCount++;
+        }
+      }
+
+      // check north west neighbor
+      if (x > 0 && y > 0) {
+        if (currentBuffer[y - 1][x - 1] === 1) {
+          livingCount++;
+        }
+      }
+
+      // north east
+      if (y > 0 && x < this.width - 1) {
+        if (currentBuffer[y - 1][x + 1] === 1) {
+          livingCount++;
+        }
+      } 
+
+      // south east
+      if (x < this.width - 1 && y < this.height -1) {
+        if (currentBuffer[y + 1][x + 1] === 1) {
+          livingCount++;
+        }
+      }
+
+      // south west
+      if (y < this.height - 1 && x > 0) {
+        if (currentBuffer[y + 1][x - 1] === 1) {
+          livingCount++;
+        }
+      }
+
+      return livingCount;
+    }
+
+    // loop thru and decide the state of next gerneration for each cell processed
+    for (let y = 0; y < this.height; y++) /* looping over height */{
+      for (let x = 0; x < this.width; x++) /* looping over width */{
+
+        let livingCount = (countNeighbors.bind(this))(x, y);
+        // console.log(`livingCount: ${livingCount}`);
+
+        let currentCell = currentBuffer[y][x];
+
+        // check if currentCell alive or dead
+        if (currentCell === 1) {
+          // alive - check if dying or still alive
+          if (livingCount < 2 || livingCount > 3) {
+            backBuffer[y][x] = 0; // die
+          } else {
+            backBuffer[y][x] = 1; // live
+          }
+        } else {
+          // dead - check if coming to life or still dead
+          if (livingCount === 3) {
+            backBuffer[y][x] = 1; // live
+          } else {}
+          backBuffer[y][x] = 0; // die
+        }
+      }
+    }
+
+    this.currentBufferIndex = this.currentBufferIndex === 0 ? 1 : 0;
   }
 }
 
