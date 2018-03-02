c6390bc90c970f3560e6705dd7671eff27d0678c
Samuel Kim <SamuelKim@Owners-MacBook-Air.local>
2018-03-01 23:37:35 -0800

    working solution finally

--- projects/life/src/App.js
+++ projects/life/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Life from './life';
 import './App.css';
 
+
 /**
  * Life canvas
  */
@@ -38,7 +39,37 @@ class LifeCanvas extends Component {
     // Convert the cell values into white or black for the canvas
     // Put the new image data back on the canvas
     // Next generation of life
-  }
+    const cells = this.life.getCells();
+    const height = this.props.height;
+    const width = this.props.width;
+
+    // Get canvas framebuffer, a packed RGBA array
+    const canvas = this.refs.canvas;
+    let ctx = canvas.getContext('2d');
+    let imageData = ctx.getImageData(0, 0, width, height);
+
+    // Update the imageData based on the cells
+    for (let y = 0; y < height; y++) {
+        for (let x = 0; x < width; x++) {
+
+          const color = cells[y][x] === 1 ? 0xff : 0x00;
+          const index = (y * width + x) * 4;
+          imageData.data[index + 0] = color; // red
+          imageData.data[index + 1] = color;  // green
+          imageData.data[index + 2] = color; // blue
+          imageData.data[index + 3] = 0xff  // alpha, 0xff === 255 === opaque
+        }
+    }
+    // put image data back on the canvas
+    ctx.putImageData(imageData, 0, 0);
+
+    //iterate the game state
+    this.life.step();
+
+    requestAnimationFrame(() => {
+      this.animFrame();
+    });
+  };
 
   /**
    * Render
--- projects/life/src/life.js
+++ projects/life/src/life.js
@@ -25,6 +25,15 @@ class Life {
    */
   constructor(width, height) {
     // !!!! IMPLEMENT ME !!!!
+    this.width = width;
+    this.height = height;
+
+    this.buffer = [
+      Array2D(width, height),
+      Array2D(width, height)
+    ];
+    this.curBuffInd = 0;
+    this.clear();
   }
   
   /**
@@ -34,13 +43,17 @@ class Life {
    */
   getCells() {
     // !!!! IMPLEMENT ME !!!!
+    return this.buffer[this.curBuffInd];
   }
 
   /**
    * Clear the life grid
    */
   clear() {
-    // !!!! IMPLEMENT ME !!!!
+    // !!!! IMPLEMENT ME !!!! 
+    for (let y = 0; y < this.height; y++) {
+      this.buffer[this.curBuffInd][y].fill(0)
+    }
   }
   
   /**
@@ -48,6 +61,12 @@ class Life {
    */
   randomize() {
     // !!!! IMPLEMENT ME !!!!
+    for (let y = 0; y < this.height; y++) {
+      for (let x = 0; x < this.width; x++) {
+        const rand = Math.floor(Math.random() * 2);
+        this.buffer[this.curBuffInd][y][x] = rand;
+      }
+    }
   }
 
   /**
@@ -55,6 +74,92 @@ class Life {
    */
   step() {
     // !!!! IMPLEMENT ME !!!!
+    const backBuffInd = this.curBuffInd === 0 ? 1:0;
+    const currentBuff = this.buffer[this.curBuffInd];
+    let backBuff = this.buffer[backBuffInd];
+
+    const countNeighbors = (x, y, options={border:'zero'}) => {
+      let neighborCount = 0;
+      // adding all of the alive neighbors
+      if (options.border === 'zero') {
+        for (let yOffset = -1; yOffset <= 1; yOffset++) {
+          const yPos = y + yOffset;
+          if (yPos < 0 || yPos > this.height - 1) {
+            continue;
+          }
+          for (let xOffset = -1; xOffset <= 1; xOffset++) {
+            const xPos = x + xOffset;
+            if (xPos < 0 || xPos > this.width - 1) {
+              continue
+            }
+            if (xPos === x && yPos === y) {
+              continue;
+            }
+            neighborCount += currentBuff[yPos][xPos];
+          }
+        }
+      } else if (options.border === 'wrap') {
+        let north = y - 1;
+        let south = y + 1;
+        let west = x - 1;
+        let east = x + 1;
+
+        //Check if out of bounds and need to wrap
+        if (north < 0) {
+          north = this.height - 1;
+        }
+        if (south > this.height - 1) {
+          south = 0;
+        } 
+        if (west < 0) {
+          west =  this.width - 1;
+        }
+        if (east > this.width - 1) {
+          east = 0;
+        }
+
+        neighborCount = 
+        currentBuff[north][west] +
+        currentBuff[north][x] +
+        currentBuff[north][east] +
+        currentBuff[south][west] +
+        currentBuff[south][x] +
+        currentBuff[south][east]+
+        currentBuff[y][west] +
+        currentBuff[y][east];
+      
+      } else {
+        throw new Error ('unknown border option' + options.border);
+      }
+      return neighborCount;
+    }
+    // loop through and decide if the next genertion is alive or dead for 
+    // each possessed cells
+    for (let y = 0; y < this.height; y++) {
+      for (let x = 0; x < this.width; x++) {
+        // life rules
+        const neighbors = countNeighbors(x, y);
+        const cell = currentBuff[y][x];
+
+        if (cell === 1) {
+          // kill cells 
+          if (neighbors > 3 || neighbors < 2) {
+            backBuff[y][x] = 0;
+          }
+          else {
+            // keep alive
+              backBuff[y][x] = 1;
+          }
+        } else {
+          if (neighbors === 3) {
+            backBuff[y][x] = 1;
+          } else {
+            backBuff[y][x] = 0;
+          }
+        }
+      }
+    }
+    this.curBuffInd = backBuffInd;
   }
 }
 
