4cf536af7f029449ee15039407ff2e82c394e49a
David Long <davdaarn@live.com>
2018-03-02 16:53:24 -0600

    wrap around

--- projects/life/src/life.js
+++ projects/life/src/life.js
@@ -263,51 +263,160 @@ class CCA {
   /**
    * Run the simulation for a single step
    */
+  // step() {
+  //   let backBufferIndex = this.currentBufferIndex === 0 ? 1 : 0;
+  //   let currentBuffer = this.buffer[this.currentBufferIndex];
+  //   let backBuffer = this.buffer[backBufferIndex];
+
+  //   function checkNeighbours(x, y) {
+  //     // console.log(status);
+  //     let NLN = 0;
+
+  //     if (x > 0 && y > 0 && x < this.width - 1 && y < this.height - 1) {
+  //       // northeast [y-1][x-1]
+  //       if (currentBuffer[y - 1][x - 1] === 1) NLN++;
+  //       // north [y-1]
+  //       if (currentBuffer[y - 1][x] === 1) NLN++;
+  //       // northwest [y-1][x+1]
+  //       if (currentBuffer[y - 1][x + 1] === 1) NLN++;
+  //       // west [x+1]
+  //       if (currentBuffer[y][x + 1] === 1) NLN++;
+  //       // southwest [y+1][x+1]
+  //       if (currentBuffer[y + 1][x + 1] === 1) NLN++;
+  //       // south [y+1]
+  //       if (currentBuffer[y + 1][x] === 1) NLN++;
+  //       // southeast [y+1][x-1]
+  //       if (currentBuffer[y + 1][x - 1] === 1) NLN++;
+  //       // east [x-1]
+  //       if (currentBuffer[y][x - 1] === 1) NLN++;
+  //     }
+
+  //     return NLN;
+  //   }
+
+  //   for (let y = 0; y < this.height; y++) {
+  //     for (let x = 0; x < this.width; x++) {
+  //       let neighborCount = checkNeighbours.call(this, x, y);
+
+  //       if (currentBuffer[y][x] === 1) {
+  //         if (neighborCount === 2 || neighborCount === 3) {
+  //           backBuffer[y][x] = 1;
+  //         } else {
+  //           backBuffer[y][x] = 0;
+  //         }
+  //       }
+
+  //       if (currentBuffer[y][x] === 0) {
+  //         if (neighborCount === 3) {
+  //           backBuffer[y][x] = 1;
+  //         } else {
+  //           backBuffer[y][x] = 0;
+  //         }
+  //       }
+  //     }
+  //   }
+  //   this.currentBufferIndex = this.currentBufferIndex === 0 ? 1 : 0;
+  // }
+
   step() {
+    // initialize backBufferIndex to 0, and toggle back and forth between the two
     let backBufferIndex = this.currentBufferIndex === 0 ? 1 : 0;
+    // initialize current buffer to this.buffer of the current index
     let currentBuffer = this.buffer[this.currentBufferIndex];
+    // initialize the back buffer to this.buffer of the back buffer index, which will always be in opposition to the current buffer
     let backBuffer = this.buffer[backBufferIndex];
 
-    function checkNeighbours(x, y) {
-      // console.log(status);
-      let NLN = 0;
+    // function to check for living neighbors
+    const hasLivingNeighbor = (x, y, options = { wrap: false }) => {
+      let LNCounter = 0,
+        zCounter = 0,
+        zMod = 2;
 
-      if (x > 0 && y > 0 && x < this.width - 1 && y < this.height - 1) {
-        // northeast [y-1][x-1]
-        if (currentBuffer[y - 1][x - 1] === 1) NLN++;
-        // north [y-1]
-        if (currentBuffer[y - 1][x] === 1) NLN++;
-        // northwest [y-1][x+1]
-        if (currentBuffer[y - 1][x + 1] === 1) NLN++;
-        // west [x+1]
-        if (currentBuffer[y][x + 1] === 1) NLN++;
-        // southwest [y+1][x+1]
-        if (currentBuffer[y + 1][x + 1] === 1) NLN++;
-        // south [y+1]
-        if (currentBuffer[y + 1][x] === 1) NLN++;
-        // southeast [y+1][x-1]
-        if (currentBuffer[y + 1][x - 1] === 1) NLN++;
-        // east [x-1]
-        if (currentBuffer[y][x - 1] === 1) NLN++;
+      // option to wrap
+      if (!options.wrap) {
+        // confine your looping to the size of the canvas
+        // if (y > 0 && y < this.height - 1 && x > 0 && x < this.width - 1) {
+        //   // North neighbor of cell x, y
+        //   if (currentBuffer[y - 1][x] % zMod === 1) {
+        //     LNCounter++;
+        //     if (currentBuffer[y - 1][x] === 3) zCounter++;
+        //   }
+
+        //   // northeast
+        //   if (currentBuffer[y - 1][x + 1] === 1) LNCounter++;
+
+        //   // East
+        //   if (currentBuffer[y][x + 1] === 1) LNCounter++;
+
+        //   // southeast
+        //   if (currentBuffer[y + 1][x + 1] === 1) LNCounter++;
+
+        //   // South
+        //   if (currentBuffer[y + 1][x] === 1) LNCounter++;
+
+        //   //southwest
+        //   if (currentBuffer[y + 1][x - 1] === 1) LNCounter++;
+
+        //   // West
+        //   if (currentBuffer[y][x - 1] === 1) LNCounter++;
+
+        //   // northwest
+        //   if (currentBuffer[y - 1][x - 1] === 1) LNCounter++;
+        // }
+        // } else if (options.wrap) {
+        let north = y - 1;
+        let east = x + 1;
+        let south = y + 1;
+        let west = x - 1;
+
+        // check to see if we need to wrap
+        if (north < 0) north = this.height - 1;
+        if (east >= this.width) east = 0;
+        if (south >= this.height) south = 0;
+        if (west < 0) west = this.width - 1;
+
+        LNCounter =
+          currentBuffer[north][x] +
+          currentBuffer[north][east] +
+          currentBuffer[y][east] +
+          currentBuffer[south][east] +
+          currentBuffer[south][x] +
+          currentBuffer[south][west] +
+          currentBuffer[y][west] +
+          currentBuffer[north][west];
+      } else {
+        throw new Error('unknown border option' + options.wrap);
       }
 
-      return NLN;
-    }
+      // return count of our neighbors is alive for use in the RULES!
+      return LNCounter;
+    };
 
+    // loop through and decide on the state of the next generation for each cell processed
     for (let y = 0; y < this.height; y++) {
       for (let x = 0; x < this.width; x++) {
-        let neighborCount = checkNeighbours.call(this, x, y);
+        // grab the neighborCounter
+        let neighborCounter = hasLivingNeighbor(x, y);
+        // grab the currently displayed cell (less typing)
+        let currentCell = currentBuffer[y][x];
 
-        if (currentBuffer[y][x] === 1) {
-          if (neighborCount === 2 || neighborCount === 3) {
-            backBuffer[y][x] = 1;
-          } else {
+        // if this cell is currently alive
+        if (currentCell === 1) {
+          // if the live neighbors are <2 or >3
+          if (neighborCounter < 2 || neighborCounter > 3) {
+            // this cell dies in the next buffer
             backBuffer[y][x] = 0;
           }
+          // if the live neighbors equal 2 or 3
+          if (neighborCounter === 2 || neighborCounter === 3) {
+            // this cell lives in the next buffer
+            backBuffer[y][x] = 1;
+          }
         }
-
-        if (currentBuffer[y][x] === 0) {
-          if (neighborCount === 3) {
+        // if this cell is currently dead
+        if (currentCell === 0) {
+          // if live neighbors is exactly 3
+          if (neighborCounter === 3) {
             backBuffer[y][x] = 1;
           } else {
             backBuffer[y][x] = 0;
@@ -315,6 +424,7 @@ class CCA {
         }
       }
     }
+    // switch the currentBufferIndex
     this.currentBufferIndex = this.currentBufferIndex === 0 ? 1 : 0;
   }
 }
