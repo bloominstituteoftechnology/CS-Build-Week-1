46b9eda236c71bb3e6d44249d493c4a738e8848d
Jamie Domingo <jaybmw@aol.com>
2018-04-04 15:24:55 -0700

    updated with code

--- projects/life/src/life.js
+++ projects/life/src/life.js
@@ -25,8 +25,18 @@ class Life {
    */
   constructor(width, height) {
     // !!!! IMPLEMENT ME !!!!
+    this.width = width;
+    this.height = height;
+
+    this.currentBuffer = 0;
+    this.buffer = [
+      Array2D(width, height),
+      Array2D(width, height),
+    ];
+
+    this.clear();
   }
-  
+
   /**
    * Return the current active buffer
    * 
@@ -34,6 +44,7 @@ class Life {
    */
   getCells() {
     // !!!! IMPLEMENT ME !!!!
+    return this.buffer[this.currentBufferIndex];
   }
 
   /**
@@ -41,13 +52,23 @@ class Life {
    */
   clear() {
     // !!!! IMPLEMENT ME !!!!
+    for (let y = 0; y < this.height; y++) {
+      this.buffer[this.currentBufferIndex][y].fill(0);
+    }
   }
-  
+
   /**
    * Randomize the life grid
    */
   randomize() {
     // !!!! IMPLEMENT ME !!!!
+    let buffer = this.buffer[this.currentBufferIndex];
+
+    for (let y = 0; y < this.height; y++) {
+      for (let x = 0; x < this.width; x++) {
+        buffer[y][x] = Math.floor(Math.random() * MODULO);
+      }
+    }
   }
 
   /**
@@ -55,7 +76,91 @@ class Life {
    */
   step() {
     // !!!! IMPLEMENT ME !!!!
+    let backBufferIndex = this.currentBufferIndex === 0 ? 1 : 0;
+    let currentBuffer = this.buffer[this.currentBufferIndex];
+    let backBuffer = this.buffer[backBufferIndex];
+
+    const hasInfectiousNeighbor = (x, y) => {
+      const nextValue = (currentBuffer[y][x] + 1) % MODULO;
+      let count = 0;
+
+      // west
+      if (x > 0) { // as long as it's not the first cell in the row
+        if (currentBuffer[y][x - 1] === nextValue) {
+          return true;
+          count++;
+        }
+      }
+
+      // north west 
+      if (x > 0 && y > 0) {
+        if (currentBuffer[y - 1][x - 1] === nextValue) {
+          return true;
+          count++;
+        }
+      }
+
+      // north
+      if (y > 0) { // if not very top row
+        if (currentBuffer[y - 1][x] === nextValue) {
+          count++;
+        }
+      }
+
+      // north east
+      if (y > 0 && x < this.width - 1) {
+        if (currentBuffer[y - 1][x + 1] === nextValue) {
+          return true;
+          count++;
+        }
+      }
+
+      // east
+      if (x < this.width - 1) {
+        if (currentBuffer[y][x + 1] === nextValue) {
+          count++;
+        }
+      }
+
+      // south east
+      if (x < this.width - 1 && y < this.height - 1) {
+        if (currentBuffer[y + 1][x + 1] === nextValue) {
+          count++;
+        }
+      }
+
+      // south
+      if (y < this.height - 1) {
+        if (currentBuffer[y + 1][x] === nextValue) {
+          count++;
+        }
+      }
+
+      // south west
+      if (x > 0 && y < this.height - 1) {
+        if (currentBuffer[y + 1][x - 1] === nextValue) {
+          count++;
+        }
+      }
+
+      return count;
+    }
+
+    for (let y = 0; y < this.height; y++) {
+      for (let x = 0; x < this.width; x++) {
+        if (count === 2 || count === 3) {
+          backBuffer[y][x] = (currentBuffer[y][x] + 1) % MODULO;
+
+        } else
+        // if alive, count is 2 or 3, then remain alive
+        // else dies
+        // if dead, and has 2 neighbors, then come back to life
+        // else stay dead
+      }
+    }
+    this.currentBufferIndex = backBufferIndex;
   }
+
 }
 
 export default Life;
