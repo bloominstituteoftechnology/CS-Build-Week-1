f195e16f6b651210182c8af07440e38a99d8ab32
Adam Fetters <adamfetterscoder@gmail.com>
2018-04-04 16:57:45 -0400

    MVP Completed / Working

--- guided-demo/cca/src/App.js
+++ guided-demo/cca/src/App.js
@@ -11,36 +11,73 @@ const COLORS = [
   [0x5f, 0x8f, 0x7f],
   [0x8f, 0xff, 0x7f],
   [0xff, 0x5f, 0x7f],
-]
+];
 
 /**
  * CCA canvas
  */
 class CCACanvas extends Component {
-
   /**
    * Constructor
    */
   constructor(props) {
     super(props);
+
+    this.cca = new CCA(props.width, props.height);
+    this.cca.randomize();
   }
 
   /**
    * Component did mount
    */
   componentDidMount() {
+    requestAnimationFrame(() => {
+      this.animFrame();
+    });
   }
 
   /**
    * Handle an animation frame
    */
   animFrame() {
+    const width = this.props.width;
+    const height = this.props.height;
+    let cells = this.cca.getCells();
+
+    let canvas = this.refs.canvas;
+    let ctx = canvas.getContext('2d');
+    let imageData = ctx.getImageData(0, 0, width, height);
+
+    for (let y = 0; y < height; y++) {
+      for (let x = 0; x < width; x++) {
+        const index = (y * width + x) * 4;
+        const status = cells[y][x];
+
+        // Actually update the colors!
+        imageData.data[index + 0] = COLORS[status][0];
+        imageData.data[index + 1] = COLORS[status][1];
+        imageData.data[index + 2] = COLORS[status][2];
+        imageData.data[index + 3] = 0xff; // alpha, 0xff = opaque
+      }
+    }
+
+    // Put the image data back on the canvas
+    ctx.putImageData(imageData, 0, 0);
+
+    // Iterate the CCA
+    this.cca.step();
+
+    // Request another animation frame
+    requestAnimationFrame(() => {
+      this.animFrame();
+    });
   }
 
   /**
    * Render
    */
   render() {
+    return <canvas ref="canvas" width={this.props.width} height={this.props.height} />;
   }
 }
 
@@ -48,16 +85,15 @@ class CCACanvas extends Component {
  * CCA holder component
  */
 class CCAApp extends Component {
-
   /**
    * Render
    */
   render() {
     return (
       <div>
-        <CCACanvas width={400} height={300} />
+        <CCACanvas width={1000} height={1000} />
       </div>
-    )
+    );
   }
 }
 
@@ -65,7 +101,6 @@ class CCAApp extends Component {
  * Outer App component
  */
 class App extends Component {
-
   /**
    * Render
    */
