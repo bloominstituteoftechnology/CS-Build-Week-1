c03f03c0265634d735c9bcb3fa9a412588702329
Ting Wang <felix.wang.1026@gmail.com>
2018-04-04 23:46:26 -0400

    finished the mvp of GOL

--- projects/life/src/App.js
+++ projects/life/src/App.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import Life from './life';
 import './App.css';
 
+const COLORS = [[255, 255, 255], [0, 0, 0]];
+
 /**
  * Life canvas
  */
 class LifeCanvas extends Component {
-
   /**
    * Constructor
    */
@@ -21,30 +22,61 @@ class LifeCanvas extends Component {
    * Component did mount
    */
   componentDidMount() {
-    requestAnimationFrame(() => {this.animFrame()});
+    requestAnimationFrame(() => {
+      this.animFrame();
+    });
   }
 
   /**
    * Handle an animation frame
    */
   animFrame() {
-    //
-    // !!!! IMPLEMENT ME !!!!
-    //
-
     // Request another animation frame
     // Update life and get cells
     // Get canvas framebuffer, a packed RGBA array
     // Convert the cell values into white or black for the canvas
     // Put the new image data back on the canvas
     // Next generation of life
+    const width = this.props.width;
+    const height = this.props.height;
+    let cells = this.life.getCells();
+
+    let canvas = this.refs.canvas;
+    let ctx = canvas.getContext('2d');
+    let imageData = ctx.getImageData(0, 0, width, height);
+
+    for (let y = 0; y < height; y++) {
+      for (let x = 0; x < width; x++) {
+        const index = (y * width + x) * 4;
+        const status = cells[y][x];
+
+        imageData.data[index + 0] = COLORS[status][0];
+        imageData.data[index + 1] = COLORS[status][1];
+        imageData.data[index + 2] = COLORS[status][2];
+        imageData.data[index + 3] = 0xff;
+      }
+    }
+
+    ctx.putImageData(imageData, 0, 0);
+
+    this.life.step();
+
+    requestAnimationFrame(() => {
+      this.animFrame();
+    });
   }
 
   /**
    * Render
    */
   render() {
-    return <canvas ref="canvas" width={this.props.width} height={this.props.height} />
+    return (
+      <canvas
+        ref="canvas"
+        width={this.props.width}
+        height={this.props.height}
+      />
+    );
   }
 }
 
@@ -52,7 +84,6 @@ class LifeCanvas extends Component {
  * Life holder component
  */
 class LifeApp extends Component {
-
   /**
    * Render
    */
@@ -61,7 +92,7 @@ class LifeApp extends Component {
       <div>
         <LifeCanvas width={400} height={300} />
       </div>
-    )
+    );
   }
 }
 
@@ -69,7 +100,6 @@ class LifeApp extends Component {
  * Outer App component
  */
 class App extends Component {
-
   /**
    * Render
    */
--- projects/life/src/life.js
+++ projects/life/src/life.js
@@ -19,42 +19,115 @@ function Array2D(width, height) {
  * Life class
  */
 class Life {
-
   /**
    * Constructor
    */
   constructor(width, height) {
-    // !!!! IMPLEMENT ME !!!!
+    this.width = width;
+    this.height = height;
+    this.currentBufferIndex = 0;
+    this.buffer = [Array2D(width, height), Array2D(width, height)];
+    this.clear();
   }
-  
+
   /**
    * Return the current active buffer
-   * 
+   *
    * This should NOT be modified by the caller
    */
   getCells() {
-    // !!!! IMPLEMENT ME !!!!
+    return this.buffer[this.currentBufferIndex];
   }
 
   /**
    * Clear the life grid
    */
   clear() {
-    // !!!! IMPLEMENT ME !!!!
+    for (let i = 0; i < this.height; i++) {
+      this.buffer[this.currentBufferIndex][i].fill(0);
+    }
   }
-  
+
   /**
    * Randomize the life grid
    */
   randomize() {
     // !!!! IMPLEMENT ME !!!!
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        this.buffer[this.currentBufferIndex][i][j] = Math.round(Math.random());
+      }
+    }
   }
 
   /**
    * Run the simulation for a single step
    */
   step() {
-    // !!!! IMPLEMENT ME !!!!
+    let backBufferIndex = this.currentBufferIndex === 0 ? 1 : 0;
+    let currentBuffer = this.buffer[this.currentBufferIndex];
+    let backBuffer = this.buffer[backBufferIndex];
+
+    const getCell = (x, y) => {
+      if (x < 0 || x > this.width - 1 || y < 0 || y > this.height - 1) {
+        return false;
+      }
+      return currentBuffer[y][x];
+    };
+
+    const countInfectiousNeighbor = (x, y) => {
+      let count = 0;
+      if (getCell(x - 1, y - 1)) {
+        ++count;
+      }
+      if (getCell(x, y - 1)) {
+        ++count;
+      }
+      if (getCell(x + 1, y - 1)) {
+        ++count;
+      }
+      if (getCell(x + 1, y)) {
+        ++count;
+      }
+      if (getCell(x + 1, y + 1)) {
+        ++count;
+      }
+      if (getCell(x, y + 1)) {
+        ++count;
+      }
+      if (getCell(x - 1, y + 1)) {
+        ++count;
+      }
+      if (getCell(x - 1, y)) {
+        ++count;
+      }
+      return count;
+    };
+
+    for (let y = 0; y < this.height; y++) {
+      for (let x = 0; x < this.width; x++) {
+        if (currentBuffer[y][x]) {
+          if (countInfectiousNeighbor(x, y) < 2) {
+            backBuffer[y][x] = 0;
+          } else if (
+            countInfectiousNeighbor(x, y) > 1 &&
+            countInfectiousNeighbor(x, y) < 4
+          ) {
+            backBuffer[y][x] = 1;
+          } else {
+            backBuffer[y][x] = 0;
+          }
+        } else if (!currentBuffer[y][x]) {
+          if (countInfectiousNeighbor(x, y) === 3) {
+            backBuffer[y][x] = 1;
+          } else {
+            backBuffer[y][x] = 0;
+          }
+        }
+      }
+    }
+
+    this.currentBufferIndex = backBufferIndex;
   }
 }
 
