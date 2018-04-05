e643e06357507d42f7d4accc05c9120717526b6f
Ting Wang <felix.wang.1026@gmail.com>
2018-04-05 12:33:18 -0400

    check all neighbors around the cell

--- guided-demo/cca/src/cca.js
+++ guided-demo/cca/src/cca.js
@@ -8,116 +8,121 @@ const MODULO = 8;
  * Make a 2D array helper function
  */
 function Array2D(width, height) {
-    let a = new Array(height);
+  let a = new Array(height);
 
-    for (let i = 0; i < height; i++) {
-        a[i] = new Array(width);
-    }
+  for (let i = 0; i < height; i++) {
+    a[i] = new Array(width);
+  }
 
-    return a;
+  return a;
 }
 
 /**
  * CCA class
  */
 class CCA {
-    /**
-     * Constructor
-     */
-    constructor(width, height) {
-        this.width = width;
-        this.height = height;
+  /**
+   * Constructor
+   */
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
 
-        this.currentBufferIndex = 0;
-        // allocate the double buffer
-        this.buffer = [Array2D(width, height), Array2D(width, height)];
-        this.clear();
+    this.currentBufferIndex = 0;
+    // allocate the double buffer
+    this.buffer = [Array2D(width, height), Array2D(width, height)];
+    this.clear();
+  }
+
+  /**
+   * Return the current active buffer
+   *
+   * This should NOT be modified by the caller
+   */
+  getCells() {
+    return this.buffer[this.currentBufferIndex];
+  }
+
+  /**
+   * Clear the cca grid
+   * fill with 0s
+   */
+  clear() {
+    for (let y = 0; y < this.height; y++) {
+      this.buffer[this.currentBufferIndex][y].fill(0);
     }
+  }
 
-    /**
-     * Return the current active buffer
-     *
-     * This should NOT be modified by the caller
-     */
-    getCells() {
-        return this.buffer[this.currentBufferIndex];
+  /**
+   * Randomize the cca grid
+   */
+  randomize() {
+    let buffer = this.buffer[this.currentBufferIndex];
+    for (let y = 0; y < this.height; y++) {
+      for (let x = 0; x < this.width; x++) {
+        buffer[y][x] = Math.floor(Math.random() * MODULO);
+      }
     }
+  }
 
-    /**
-     * Clear the cca grid
-     * fill with 0s
-     */
-    clear() {
-        for (let y = 0; y < this.height; y++) {
-            this.buffer[this.currentBufferIndex][y].fill(0);
+  /**
+   * Run the simulation for a single step
+   */
+  step() {
+    let backBufferIndex = this.currentBufferIndex === 0 ? 1 : 0;
+    let currentBuffer = this.buffer[this.currentBufferIndex];
+    let backBuffer = this.buffer[backBufferIndex];
+
+    const hasInfectiousNeighbor = (x, y) => {
+      const nextValue = (currentBuffer[y][x] + 1) % MODULO;
+      const getCell = (x, y) => {
+        if (x < 0 || x > this.width - 1 || y < 0 || y > this.height - 1) {
+          return false;
         }
-    }
+        return currentBuffer[y][x];
+      };
 
-    /**
-     * Randomize the cca grid
-     */
-    randomize() {
-        let buffer = this.buffer[this.currentBufferIndex];
-        for (let y = 0; y < this.height; y++) {
-            for (let x = 0; x < this.width; x++) {
-                buffer[y][x] = Math.floor(Math.random() * MODULO);
-            }
+      if (getCell(x - 1, y - 1) === nextValue) {
+        return true;
+      }
+      if (getCell(x, y - 1) === nextValue) {
+        return true;
+      }
+      if (getCell(x + 1, y - 1) === nextValue) {
+        return true;
+      }
+      if (getCell(x + 1, y) === nextValue) {
+        return true;
+      }
+      if (getCell(x + 1, y + 1) === nextValue) {
+        return true;
+      }
+      if (getCell(x, y + 1) === nextValue) {
+        return true;
+      }
+      if (getCell(x - 1, y + 1) === nextValue) {
+        return true;
+      }
+      if (getCell(x - 1, y) === nextValue) {
+        return true;
+      }
+      return false;
+    };
+
+    // loop through the currentBuffer and populate
+    // the backBuffer (next generation) based on above helper function
+    for (let y = 0; y < this.height; y++) {
+      for (let x = 0; x < this.width; x++) {
+        if (hasInfectiousNeighbor(x, y)) {
+          backBuffer[y][x] = (currentBuffer[y][x] + 1) % MODULO;
+        } else {
+          backBuffer[y][x] = currentBuffer[y][x];
         }
+      }
     }
 
-    /**
-     * Run the simulation for a single step
-     */
-    step() {
-        let backBufferIndex = this.currentBufferIndex === 0 ? 1 : 0;
-        let currentBuffer = this.buffer[this.currentBufferIndex];
-        let backBuffer = this.buffer[backBufferIndex];
-
-        const hasInfectiousNeighbor = (x, y) => {
-            const nextValue = (currentBuffer[y][x] + 1) % MODULO;
-
-            // west neighbor
-            if (x > 0) {
-                if (currentBuffer[y][x - 1] === nextValue) {
-                    return true;
-                }
-            }
-            // north neighbor
-            if (y > 0) {
-                if (currentBuffer[y - 1][x] === nextValue) {
-                    return true;
-                }
-            }
-            // east neighbor
-            if (x < this.width - 1) {
-                if (currentBuffer[y][x + 1] === nextValue) {
-                    return true;
-                }
-            }
-            // south neighbor
-            if (y < this.height - 1) {
-                if (currentBuffer[y + 1][x] === nextValue) {
-                    return true;
-                }
-            }
-
-            return false;
-        };
-
-        // loop through the currentBuffer and populate
-        // the backBuffer (next generation) based on above helper function
-        for (let y = 0; y < this.height; y++) {
-            for (let x = 0; x < this.width; x++) {
-                if (hasInfectiousNeighbor(x, y)) {
-                    backBuffer[y][x] = (currentBuffer[y][x] + 1) % MODULO;
-                } else {
-                    backBuffer[y][x] = currentBuffer[y][x];
-                }
-            }
-        }
-
-        this.currentBufferIndex = backBufferIndex;
-    }
+    this.currentBufferIndex = backBufferIndex;
+  }
 }
 
 export default CCA;
