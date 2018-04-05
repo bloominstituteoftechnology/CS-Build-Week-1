94fd63e720c6d61c0bfa7b48d1bde352effaf570
Jason Metten <metten@Jasons-MacBook-Air.local>
2018-04-05 13:17:51 -0500

    MVP complete - moving to stretch

--- projects/life/src/App.js
+++ projects/life/src/App.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
 import Life from './life';
 import './App.css';
+const COLORS = [
+  [0, 0, 0],
+  [0x8f, 0, 0x5f],
+  [0x5f, 0, 0x8f],
+  [0, 0, 0xff],
+  [0, 0x5f, 0x7f],
+  [0x5f, 0x8f, 0x7f],
+  [0x8f, 0xff, 0x7f],
+  [0xff, 0x5f, 0x7f]
+];
 
 /**
  * Life canvas
@@ -11,8 +21,20 @@ class LifeCanvas extends Component {
    */
   constructor(props) {
     super(props);
+    this.rockPaperScissors = () => {
+      const rPS = ['rock', 'paper', 'scissors'];
+      const choices = [];
+      for (let a = 0; a < 3; a++)
+        for (let b = 0; b < 3; b++)
+          for (let c = 0; c < 3; c++) {
+            choices.push(`${rPS[a]},${rPS[b]},${rPS[c]}`);
+            console.log(`${rPS[a]},${rPS[b]},${rPS[c]}`);
+          }
 
+      return choices[Math.floor(Math.random() * choices.length)];
+    };
     this.life = new Life(props.width, props.height);
+
     this.life.randomize();
   }
 
@@ -20,6 +42,7 @@ class LifeCanvas extends Component {
    * Component did mount
    */
   componentDidMount() {
+    this.life.randomize();
     requestAnimationFrame(() => {
       this.animFrame();
     });
@@ -29,41 +52,29 @@ class LifeCanvas extends Component {
    * Handle an animation frame
    */
   animFrame() {
-    //
-    // !!!! IMPLEMENT ME !!!!
-    //
-    // Update life and get cells
-    // Convert the cell values into white or black for the canvas
-    // Next generation of life
-
-    const width = this.props.width;
-    const height = this.props.height;
-    let cells = this.life.getCells();
-
-    // Get canvas framebuffer, a packed RGBA array
+    const { width, height } = this.props;
+    const cells = this.life.getCells();
     let canvas = this.refs.canvas;
     let ctx = canvas.getContext('2d');
     let imageData = ctx.getImageData(0, 0, width, height);
-
-    for (let y = 0; y < height; ++y) {
-      for (let x = 0; x < width; ++x) {
+    //[r1,b1,g1,r2,b2,g2,r3,b3,g3]
+    for (let y = 0; y < height; y++)
+      for (let x = 0; x < width; x++) {
         const index = (y * width + x) * 4;
-        const color = cells[y][x] === 0 ? 0x00 : 0xff;
-
-        imageData.data[index + 0] = COLORS[status][0]; // R
-        imageData.data[index + 1] = COLORS[status][1]; // G
-        imageData.data[index + 2] = COLORS[status][2]; // B
-        imageData.data[index + 3] = 0xff; // A will be opaque
+        const color = cells[y][x] ? 0xff : 0x00;
+        imageData.data[index + 0] = color;
+        imageData.data[index + 1] = color;
+        imageData.data[index + 2] = color;
+        imageData.data[index + 3] = 0xff; //alpha 0xff = opaque
       }
-    }
-    // Put the new image data back on the canvas
     ctx.putImageData(imageData, 0, 0);
 
-    // Request another animation frame
+    this.life.step();
     requestAnimationFrame(() => {
       this.animFrame();
     });
   }
+
   /**
    * Render
    */
--- projects/life/src/life.js
+++ projects/life/src/life.js
@@ -1,6 +1,7 @@
 /**
  * Implementation of Conway's game of Life
  */
+const MODULO = 2;
 
 /**
  * Make a 2D array helper function
@@ -19,21 +20,30 @@ function Array2D(width, height) {
  * Life class
  */
 class Life {
-
   /**
    * Constructor
    */
   constructor(width, height) {
     // !!!! IMPLEMENT ME !!!!
+    this.width = width;
+    this.height = height;
+    this.currentBufferIndex = 0;
+    this.buffer = [Array2D(width, height), Array2D(width, height)];
+    this.north = 1;
+    this.south = 1;
+    this.east = 1;
+    this.west = 1;
+    this.clear();
   }
-  
+
   /**
    * Return the current active buffer
-   * 
+   *
    * This should NOT be modified by the caller
    */
   getCells() {
     // !!!! IMPLEMENT ME !!!!
+    return this.buffer[this.currentBufferIndex];
   }
 
   /**
@@ -41,13 +51,19 @@ class Life {
    */
   clear() {
     // !!!! IMPLEMENT ME !!!!
+    for (let y = 0; y < this.height; y++)
+      this.buffer[this.currentBufferIndex][y].fill(0);
   }
-  
+
   /**
    * Randomize the life grid
    */
   randomize() {
     // !!!! IMPLEMENT ME !!!!
+    let buffer = this.buffer[this.currentBufferIndex];
+    for (let y = 0; y < this.height; y++)
+      for (let x = 0; x < this.width; x++)
+        buffer[y][x] = Math.floor(Math.random() * MODULO);
   }
 
   /**
@@ -55,6 +71,117 @@ class Life {
    */
   step() {
     // !!!! IMPLEMENT ME !!!!
+    let backBufferIndex = this.currentBufferIndex === 0 ? 1 : 0;
+    let currentBuffer = this.buffer[this.currentBufferIndex];
+    let backBuffer = this.buffer[backBufferIndex];
+    /*
+	const hasInfectiousNeighbor = (x,y) => {
+	const nextValue = (currentBuffer[y][x] + 1 ) % MODULO;
+	
+	//West
+	if(this.west)
+	if (x > 0)
+	if(currentBuffer[y][x-1] === nextValue)
+	return true;
+	//North
+	if(this.north)
+	if (y > 0)
+	if (currentBuffer[y - 1][x] === nextValue)
+	return true;
+	
+	//East
+	if(this.east)
+	if (x < this.width - 1)
+	if (currentBuffer[y][x + 1] === nextValue)
+	return true;
+	
+	//South
+	if(this.south)
+	if (y < this.height - 1)
+	if (currentBuffer[y + 1][x] === nextValue)
+	return true;
+	
+	return false;
+	}
+	
+	for (let y = 0; y < this.height; y++)
+	for (let x = 0; x < this.width; x++)
+	if (hasInfectiousNeighbor(x,y))
+	backBuffer[y][x] = (currentBuffer[y][x] + 1 ) % MODULO;
+	else
+	backBuffer[y][x] = currentBuffer[y][x];
+	
+	this.currentBufferIndex = backBufferIndex;
+	*/
+
+    const countNeighbors = (x, y, options = { border: 'wrap' }) => {
+      let neightborCount = 0;
+      if (options.border === 'nowrap') {
+        for (let yOffset = -1; yOffset <= 1; yOffset++) {
+          let yPos = y + yOffset;
+          if (yPos < 0 || yPos >= this.height) {
+            continue;
+          }
+          for (let xOffset = -1; xOffset <= 1; xOffset++) {
+            let xPos = x + xOffset;
+            if (xPos < 0 || xPos >= this.width) continue;
+
+            if (yPos === y && xPos === x) continue;
+
+            neightborCount += currentBuffer[yPos][xPos];
+          }
+        }
+      } else if (options.border === 'wrap') {
+        let north = y - 1;
+        let south = y + 1;
+        let west = x - 1;
+        let east = x + 1;
+        if (north < 0) {
+          north = this.height - 1;
+        }
+        if (south > this.height - 1) {
+          south = 0;
+        }
+        if (west < 0) {
+          west = this.width - 1;
+        }
+        if (east > this.width - 1) {
+          east = 0;
+        }
+        neightborCount =
+          currentBuffer[north][west] +
+          currentBuffer[north][x] +
+          currentBuffer[north][x] +
+          currentBuffer[north][east] +
+          currentBuffer[y][west] +
+          currentBuffer[y][east] +
+          currentBuffer[south][x] +
+          currentBuffer[south][east] +
+          currentBuffer[south][west];
+      } else {
+        throw new Error('Unknow border option:' + options.border);
+      }
+      return neightborCount;
+    };
+    for (let y = 0; y < this.height; y++)
+      for (let x = 0; x < this.width; x++) {
+        const neighbors = countNeighbors(x, y, { border: 'nowrap' });
+        const thisCell = currentBuffer[y][x];
+        if (thisCell) {
+          if (neighbors < 2 || neighbors > 3) {
+            backBuffer[y][x] = 0;
+          } else {
+            backBuffer[y][x] = 1;
+          }
+        } else {
+          if (neighbors === 3) {
+            backBuffer[y][x] = 1;
+          } else {
+            backBuffer[y][x] = 0;
+          }
+        }
+      }
+    this.currentBufferIndex = backBufferIndex;
   }
 }
 
