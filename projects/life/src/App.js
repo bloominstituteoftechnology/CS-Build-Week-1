805c1e78a740db26acc3a7ec16611237c3e81b74
Ting Wang <felix.wang.1026@gmail.com>
2018-04-05 19:26:50 -0400

    implemented the buttons and working

--- projects/life/src/App.js
+++ projects/life/src/App.js
@@ -16,6 +16,7 @@ class LifeCanvas extends Component {
 
     this.life = new Life(props.width, props.height);
     this.life.randomize();
+    this.continueAnimaiton = true;
   }
 
   /**
@@ -27,6 +28,9 @@ class LifeCanvas extends Component {
     });
   }
 
+  componentWillUnmount() {
+    this.continueAnimaiton = false;
+  }
   /**
    * Handle an animation frame
    */
@@ -61,21 +65,64 @@ class LifeCanvas extends Component {
 
     this.life.step();
 
-    requestAnimationFrame(() => {
-      this.animFrame();
-    });
+    if (this.continueAnimaiton) {
+      requestAnimationFrame(() => {
+        this.animFrame();
+      });
+    }
   }
+
+  handleStop = () => {
+    this.continueAnimaiton = false;
+  };
+
+  handleStart = () => {
+    this.continueAnimaiton = true;
+    this.animFrame();
+  };
+
+  handleClear = () => {
+    this.continueAnimaiton = false;
+    this.refs.canvas
+      .getContext('2d')
+      .clearRect(0, 0, this.props.width, this.props.height);
+    this.life.clear();
+  };
+
+  handleRandom = () => {
+    this.continueAnimaiton = false;
+    this.life.randomize();
+    this.animFrame();
+  };
+
+  // handleDropGlider = () => {
+  //   let width = this.props.width;
+  //   let height = this.props.height;
+  //   let x = Math.floor(Math.random() * width);
+  //   let y = Math.floor(Math.random() * height);
+  //   let canvas = this.refs.canvas;
+  //   let ctx = canvas.getContext('2d');
+  //   let imageData = ctx.getImageData(0, 0, width, height);
+  //   const index = (y * width + x) * 4;
+  //   if (imageData.data[index])
+  // };
 
   /**
    * Render
    */
   render() {
     return (
-      <canvas
-        ref="canvas"
-        width={this.props.width}
-        height={this.props.height}
-      />
+      <div>
+        <canvas
+          ref="canvas"
+          width={this.props.width}
+          height={this.props.height}
+        />
+        <button onClick={this.handleStop}>STOP</button>
+        <button onClick={this.handleStart}>START</button>
+        <button onClick={this.handleClear}>CLEAR</button>
+        <button onClick={this.handleRandom}>RANDOM</button>
+      </div>
     );
   }
 }
@@ -90,7 +137,7 @@ class LifeApp extends Component {
   render() {
     return (
       <div>
-        <LifeCanvas width={400} height={300} />
+        <LifeCanvas width={500} height={400} />
       </div>
     );
   }
--- projects/life/src/life.js
+++ projects/life/src/life.js
@@ -52,7 +52,6 @@ class Life {
    * Randomize the life grid
    */
   randomize() {
-    // !!!! IMPLEMENT ME !!!!
     for (let i = 0; i < this.height; i++) {
       for (let j = 0; j < this.width; j++) {
         this.buffer[this.currentBufferIndex][i][j] = Math.round(Math.random());
@@ -68,38 +67,87 @@ class Life {
     let currentBuffer = this.buffer[this.currentBufferIndex];
     let backBuffer = this.buffer[backBufferIndex];
 
-    const getCell = (x, y) => {
-      if (x < 0 || x > this.width - 1 || y < 0 || y > this.height - 1) {
-        return false;
-      }
-      return currentBuffer[y][x];
-    };
+    // const getCell = (x, y) => {
+    //   if (x < 0 || x > this.width - 1 || y < 0 || y > this.height - 1) {
+    //     return false;
+    //   }
+    //   return currentBuffer[y][x];
+    // };
 
-    const countInfectiousNeighbor = (x, y) => {
+    const countInfectiousNeighbor = (x, y, options = { border: 'wrap' }) => {
       let count = 0;
-      if (getCell(x - 1, y - 1)) {
-        ++count;
-      }
-      if (getCell(x, y - 1)) {
-        ++count;
-      }
-      if (getCell(x + 1, y - 1)) {
-        ++count;
-      }
-      if (getCell(x + 1, y)) {
-        ++count;
-      }
-      if (getCell(x + 1, y + 1)) {
-        ++count;
-      }
-      if (getCell(x, y + 1)) {
-        ++count;
-      }
-      if (getCell(x - 1, y + 1)) {
-        ++count;
-      }
-      if (getCell(x - 1, y)) {
-        ++count;
+      if (options.border === 'wrap') {
+        let north = y - 1;
+        let south = y + 1;
+        let west = x - 1;
+        let east = x + 1;
+        if (north < 0) {
+          north = this.height - 1;
+        }
+        if (south > this.height - 1) {
+          south = 0;
+        }
+        if (west < 0) {
+          west = this.width - 1;
+        }
+        if (east > this.width - 1) {
+          east = 0;
+        }
+
+        count =
+          currentBuffer[north][west] +
+          currentBuffer[north][x] +
+          currentBuffer[north][east] +
+          currentBuffer[y][east] +
+          currentBuffer[y][west] +
+          currentBuffer[south][x] +
+          currentBuffer[south][east] +
+          currentBuffer[south][west];
+      } else if (options.border === 'wrap') {
+        for (let yOffset = -1; yOffset <= 1; yOffset++) {
+          let yPos = y + yOffset;
+          if (yPos < 0 || yPos >= this.height) {
+            continue;
+          }
+
+          for (let xOffset = -1; xOffset <= 1; xOffset++) {
+            let xPos = x + xOffset;
+            if (xPos < 0 || xPos >= this.width) {
+              continue;
+            }
+            if (yPos === y && xPos === x) {
+              continue;
+            }
+
+            count += currentBuffer[yPos][xPos];
+          }
+        }
+        // if (getCell(x - 1, y - 1)) {
+        //   count++;
+        // }
+        // if (getCell(x, y - 1)) {
+        //   count++;
+        // }
+        // if (getCell(x + 1, y - 1)) {
+        //   count++;
+        // }
+        // if (getCell(x + 1, y)) {
+        //   count++;
+        // }
+        // if (getCell(x + 1, y + 1)) {
+        //   count++;
+        // }
+        // if (getCell(x, y + 1)) {
+        //   count++;
+        // }
+        // if (getCell(x - 1, y + 1)) {
+        //   count++;
+        // }
+        // if (getCell(x - 1, y)) {
+        //   count++;
+        // }
+      } else {
+        throw new Error('Unknown border option: ' + options.border);
       }
       return count;
     };
@@ -128,6 +176,34 @@ class Life {
     }
 
     this.currentBufferIndex = backBufferIndex;
+  }
+
+  dropGlider(x, y) {
+    let backBufferIndex = this.currentBufferIndex === 0 ? 1 : 0;
+    let currentBuffer = this.buffer[this.currentBufferIndex];
+    let backBuffer = this.buffer[backBufferIndex];
+
+    const getCell = (x, y) => {
+      if (x < 0 || x > this.width - 1 || y < 0 || y > this.height - 1) {
+        return null;
+      }
+      return currentBuffer[y][x];
+    };
+
+    if (
+      getCell(x, y - 1) === 0 &&
+      getCell(x + 1, y) === 0 &&
+      getCell(x + 1, y + 1) === 0 &&
+      getCell(x, y + 1) === 0 &&
+      getCell(x - 1, y + 1) === 0
+    ) {
+      backBuffer[y - 1][x] = 1;
+      backBuffer[y][x + 1] = 1;
+      backBuffer[y + 1][x + 1] = 1;
+      backBuffer[y + 1][x] = 1;
+      backBuffer[y + 1][x - 1] = 1;
+      this.currentBufferIndex = backBufferIndex;
+    }
   }
 }
 
