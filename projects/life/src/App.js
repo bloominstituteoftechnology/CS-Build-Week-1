ba8fb31077bd7735d533b126b7a4a67581ad7a02
AustenHowes <austie702@gmail.com>
2018-04-05 15:50:37 -0500

    Final stage. Opting out of stretch goals due to time constraints

--- projects/life/src/App.js
+++ projects/life/src/App.js
@@ -15,8 +15,6 @@ class LifeCanvas extends Component {
 
     this.life = new Life(props.width, props.height);
     this.life.randomize();
-
-    // let ctx = canvas.getContext('2d');
   }
 
   /**
@@ -30,44 +28,45 @@ class LifeCanvas extends Component {
    * Handle an animation frame
    */
   animFrame() {
-    //
-    // !!!! IMPLEMENT ME !!!!
-    //
+    let width = this.props.width;
+    let height = this.props.height;
 
-    // Request another animation frame
     // Update life and get cells
-    // Get canvas framebuffer, a packed RGBA array
-    // Convert the cell values into white or black for the canvas
-    // Put the new image data back on the canvas
-    // Next generation of life
-    const width = this.props.width;
-    const height = this.props.height;
+    let cells = this.life.getCells();
 
+    // Get canvas framebuffer, a packed RGBA array
     let canvas = this.refs.canvas;
     let ctx = canvas.getContext('2d');
-    let imageData = ctx.getImagedata(0, 0, width, height);
-    let cells = this.cca.getCells();
+    let imageData = ctx.getImageData(0, 0, width, height);
 
+    // Convert the cell values into white or black for the canvas
     for (let y = 0; y < height; y++) {
       for (let x = 0; x < width; x++) {
-        let index = (y * width + x) * 4;
-        let color = cells[y][x] ? 0xff : 0x00;
 
-        imageData.data[index + 0] = color; // Red
-        imageData.data[index + 1] = color; // Green
-        imageData.data[index + 2] = color; // Blue
-        imageData.data[index + 3] = 0xff; // Alpha
+        // Index needs to be multiplied by 4 because there are 4 array
+        // entries per pixel, Red, Green, Blue, and Alpha:
+        let index = (y * width + x) * 4;
+
+        let lifeStatus = cells[y][x];
+        let color = lifeStatus === 0? 0xFF: 0x00;
+
+        // FYI: Alpha channel controls how transparent a pixel is.
+
+        imageData.data[index + 0] = color; // Red channel
+        imageData.data[index + 1] = color; // Green channel
+        imageData.data[index + 2] = color; // Blue channel
+        imageData.data[index + 3] = 0xff;  // Alpha channel, 0xff = opaque
       }
     }
 
-    // Put the image data back on the canvas
+    // Put the new image data back on the canvas
     ctx.putImageData(imageData, 0, 0);
-
-    // Iterate the CCA
-    this.cca.step();
+    
+    // Next generation of life
+    this.life.step();
 
     // Request another animation frame
-    requestAnimationFrame(() => { this.animFrame() });
+    requestAnimationFrame(() => {this.animFrame()});
   }
 
   /**
@@ -89,7 +88,7 @@ class LifeApp extends Component {
   render() {
     return (
       <div>
-        <LifeCanvas width={1000} height={1000} />
+        <LifeCanvas width={2400} height={1200} />
       </div>
     )
   }
--- projects/life/src/life.js
+++ projects/life/src/life.js
@@ -1,5 +1,5 @@
 /**
- * Implementation of Conway's game of Life
+ * Implemention of Conway's game of Life
  */
 
 /**
@@ -24,15 +24,17 @@ class Life {
    * Constructor
    */
   constructor(width, height) {
-    // !!!! IMPLEMENT ME !!!!
     this.width = width;
     this.height = height;
 
+    this.currentBufferIndex = 0;
+
+    // Allocate the double buffer
     this.buffer = [
       Array2D(width, height),
       Array2D(width, height)
     ];
-
+    
     this.clear();
   }
   
@@ -42,7 +44,6 @@ class Life {
    * This should NOT be modified by the caller
    */
   getCells() {
-    // !!!! IMPLEMENT ME !!!!
     return this.buffer[this.currentBufferIndex];
   }
 
@@ -50,7 +51,6 @@ class Life {
    * Clear the life grid
    */
   clear() {
-    // !!!! IMPLEMENT ME !!!!
     for (let y = 0; y < this.height; y++) {
       this.buffer[this.currentBufferIndex][y].fill(0);
     }
@@ -60,11 +60,11 @@ class Life {
    * Randomize the life grid
    */
   randomize() {
-    // !!!! IMPLEMENT ME !!!!
     let buffer = this.buffer[this.currentBufferIndex];
+
     for (let y = 0; y < this.height; y++) {
       for (let x = 0; x < this.width; x++) {
-        buffer[y][x] = Math.floor(Math.random() * 2)
+        buffer[y][x] = (Math.random()*2)|0; // Random 0 or 1
       }
     }
   }
@@ -73,104 +73,125 @@ class Life {
    * Run the simulation for a single step
    */
   step() {
-    // !!!! IMPLEMENT ME !!!!
-    let backBufferIndex = this.currentBufferIndex === 0 ? 1 : 0;
+    // Fill the offscreen buffer with the next life generation built
+    // from the current buffer.
+
+    let backBufferIndex = this.currentBufferIndex === 0? 1: 0;
     let currentBuffer = this.buffer[this.currentBufferIndex];
     let backBuffer = this.buffer[backBufferIndex];
 
-    const countNeighbors = (x, y, options={border: 'nowrap'}) => {
+    /**
+     * Count the neighbors of a cell
+     */
+    function countNeighbors(x, y, options={border:'zero'}) {
       let neighborCount = 0;
-      // Actually count living neighbors
+
       if (options.border === 'wrap') {
-        // Count neighbors, wrapping around the edges
         let north = y - 1;
         let south = y + 1;
         let west = x - 1;
         let east = x + 1;
-        
+
+        // Wrap around the edges
+
         if (north < 0) {
           north = this.height - 1;
         }
-        if (south > this.height - 1) {
+        
+       if (south === this.height) {
           south = 0;
         }
+
         if (west < 0) {
           west = this.width - 1;
-        }
-        if (east > this.width - 1) {
+        } 
+
+        if (east === this.width) {
           east = 0;
         }
 
-        neighborCount = 
-          currentBuffer[north][west] + 
+        neighborCount =
+          currentBuffer[north][west] +
           currentBuffer[north][x] +
+          currentBuffer[north][east] +
           currentBuffer[y][west] +
           currentBuffer[y][east] +
+          currentBuffer[south][west] +
           currentBuffer[south][x] +
-          currentBuffer[south][east] +
-          currentBuffer[south][west]
+          currentBuffer[south][east];
 
-      } else if (options.border === 'nowrap') {
-        // Treat out-of-bounds as zero
+      } else if (options.border === 'zero') {
+
+        // Treat out of bounds as zero
         for (let yOffset = -1; yOffset <= 1; yOffset++) {
           let yPos = y + yOffset;
-          if (yPos < 0 || yPos >= this.height) {
-            // Out of bounds!
+
+          if (yPos < 0 || yPos === this.height) {
+            // Out of bounds
             continue;
           }
 
-          for (let xOffset = -1; xOffset <= 1; yOffset++) {
+          for (let xOffset = -1; xOffset <= 1; xOffset++) {
             let xPos = x + xOffset;
-            if (xPos < 0 || xPos >= this.width) {
-              // out of bounds!
+
+            if (xPos < 0 || xPos === this.width) {
+              // Out of bounds
               continue;
             }
-            if (yPos === y && xPos === x) {
-              // Can't be your own neighbor!
+
+            // Don't count center element
+            if (xOffset === 0 && yOffset === 0) {
               continue;
             }
 
             neighborCount += currentBuffer[yPos][xPos];
-
           }
         }
+
       } else {
-        throw new Error('Unknown border option: ' + options.border);
+        throw new Error('unknown border option' + options.border);
       }
 
       return neighborCount;
-    };
 
-    // Update backBuffer to have the next time state
+    } // countNeighbors()
+
+    // Loop through and decide if the next generation is alive or dead
+    // for each cell processed.
+
     for (let y = 0; y < this.height; y++) {
-      for (let x = 0; y< this.width; x++) {
-        const neighbors = countNeighbors(x, y);
-        const thisCell = currentBuffer[y][x];
+      for (let x = 0; x < this.width; x++) {
 
-        // Implement Game Of Life rules
-        if (thisCell) {
-          // Current cell is alive
-          if (neighbors < 2 || neighbors > 3) {
-            // Death due to underpopulation or overpopulation
+        let neighborCount = (countNeighbors.bind(this))(x, y);
+
+        let thisCell = currentBuffer[y][x];
+
+        if (thisCell === 1) {
+          // We're alive. Let's check if we're dying.
+          if (neighborCount < 2 || neighborCount > 3) {
+            // Wake up. Time to die.
             backBuffer[y][x] = 0;
           } else {
-            // Still alive!
+            // We're still alive!
             backBuffer[y][x] = 1;
           }
         } else {
-          // Current cell is dead
-          if (neighbors === 3) {
-            // A cell is born!
+          // We're dead. Let's see if we come to life.
+          if (neighborCount === 3) {
+            // A rebirth!
             backBuffer[y][x] = 1;
           } else {
-            // Still dead.
+            // We're still dead
             backBuffer[y][x] = 0;
           }
         }
       }
     }
 
-    this.currentBufferIndex = backBufferIndex;
+    // Now the backBuffer is populated with the next generation life
+    // data. So we declare that to be the new current buffer.
+    
+    this.currentBufferIndex = this.currentBufferIndex === 0? 1: 0;
   }
 }
 
