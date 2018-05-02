adc2cba678a36ddea7b772087f847b18920bbc21
Samuel Cha <sunchipssam@gmail.com>
2018-05-02 14:05:10 -0700

    Completed guided demo per lect

--- guided-demo/cca/src/App.js
+++ guided-demo/cca/src/App.js
@@ -11,36 +11,79 @@ const COLORS = [
   [0x5f, 0x8f, 0x7f],
   [0x8f, 0xff, 0x7f],
   [0xff, 0x5f, 0x7f],
-]
+];
+
+const canvasWidth = 700;
+const canvasHeight = 400;
 
 /**
  * CCA canvas
  */
 class CCACanvas extends Component {
-
   /**
    * Constructor
    */
   constructor(props) {
     super(props);
+
+    this.cca = new CCA(props.width, props.height);
+    this.cca.randomize();
   }
 
   /**
    * Component did mount
    */
   componentDidMount() {
+    requestAnimationFrame(_ => this.animFrame());
   }
 
   /**
    * Handle an animation frame
    */
   animFrame() {
+    const width = this.props.width;
+    const height = this.props.height;
+
+    const canvas = this.refs.canvas;
+    const ctx = canvas.getContext('2d');
+
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, width, height);
+    ctx.fill();
+
+    const imageData = ctx.getImageData(0, 0, width, height);
+
+    for (let row = 0; row < height; row++) {
+      for (let col = 0; col < width; col++) {
+        const CCAGrid = this.cca.getCells();
+        const index = (row * width + col) * 4; /* 4 for each pixel  */
+        const color = CCAGrid[row][col];
+
+        imageData.data[index + 0] = COLORS[color][0];
+        imageData.data[index + 1] = COLORS[color][1];
+        imageData.data[index + 2] = COLORS[color][2];
+
+        imageData.data[index + 3] = 0xff; /* alpha channel solid */
+      }
+    }
+
+    ctx.putImageData(imageData, 0, 0);
+    requestAnimationFrame(_ => this.animFrame());
+
+    this.cca.step();
   }
 
   /**
    * Render
    */
   render() {
+    return (
+      <canvas
+        ref="canvas"
+        width={this.props.width}
+        height={this.props.height}
+      />
+    );
   }
 }
 
@@ -48,16 +91,15 @@ class CCACanvas extends Component {
  * CCA holder component
  */
 class CCAApp extends Component {
-
   /**
    * Render
    */
   render() {
     return (
       <div>
-        <CCACanvas width={400} height={300} />
+        <CCACanvas width={canvasWidth} height={canvasHeight} />
       </div>
-    )
+    );
   }
 }
 
@@ -65,7 +107,6 @@ class CCAApp extends Component {
  * Outer App component
  */
 class App extends Component {
-
   /**
    * Render
    */
--- guided-demo/cca/src/cca.js
+++ guided-demo/cca/src/cca.js
@@ -9,20 +9,19 @@ const MODULO = 8;
  */
 function Array2D(width, height) {
   //NOTE:  Iterate through Array2D row first then column
-	let a = new Array(height);
-  
-	for (let i = 0; i < height; i++) {
-	  a[i] = new Array(width);
-	}
-  
-	return a;
+  let a = new Array(height);
+
+  for (let i = 0; i < height; i++) {
+    a[i] = new Array(width);
+  }
+
+  return a;
 }
-  
+
 /**
  * CCA class
  */
 class CCA {
-
   /**
    * Constructor
    */
@@ -30,33 +29,83 @@ class CCA {
     this.width = width;
     this.height = height;
 
+    this.bufferI = 0;
+
+    this.buffer = [Array2D(width, height), Array2D(width, height)];
+
     this.clear();
   }
 
   /**
    * Return the current active buffer
-   * 
+   *
    * This should NOT be modified by the caller
    */
   getCells() {
+    return this.buffer[this.bufferI];
   }
 
   /**
    * Clear the cca grid
    */
   clear() {
+    for (let row = 0; row < this.height; row++) {
+      this.buffer[this.bufferI][row].fill(0);
+    }
   }
 
   /**
    * Randomize the cca grid
    */
   randomize() {
+    for (let row = 0; row < this.height; row++) {
+      for (let col = 0; col < this.width; col++) {
+        this.buffer[this.bufferI][row][col] = (Math.random() * MODULO) | 0;
+      }
+    }
   }
 
   /**
    * Run the simulation for a single step
    */
   step() {
+    const backBufferI = this.bufferI === 0 ? 1 : 0;
+    const backBuffer = this.buffer[backBufferI];
+    const currentBuffer = this.buffer[this.bufferI];
+
+    const hasInfectiousNeighbor = (row, col) => {
+      const nextVal = (currentBuffer[row][col] + 1) % MODULO;
+
+      /* north */
+      if (row > 0) {
+        if (currentBuffer[row - 1][col] === nextVal) return true;
+      }
+
+      /* east */
+      if (col < this.width - 1)
+        if (currentBuffer[row][col + 1] === nextVal) return true;
+
+      /* south */
+      if (row < this.height - 1)
+        if (currentBuffer[row + 1][col] === nextVal) return true;
+
+      /* west */
+      if (col > 0) {
+        if (currentBuffer[row][col - 1] === nextVal) return true;
+      }
+
+      return false;
+    };
+
+    for (let row = 0; row < this.height; row++) {
+      for (let col = 0; col < this.width; col++) {
+        if (hasInfectiousNeighbor(row, col))
+          backBuffer[row][col] = (currentBuffer[row][col] + 1) % MODULO;
+        else backBuffer[row][col] = currentBuffer[row][col];
+      }
+    }
+
+    this.bufferI = this.bufferI === 0 ? 1 : 0;
   }
 }
 
