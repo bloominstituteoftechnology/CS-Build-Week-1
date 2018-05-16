a16cd6c25c32efbc55800d2548f95de1b15936a6
Smithface <kylewibblesmith@gmail.com>
2018-05-16 17:25:54 -0400

    finished life.js

--- projects/life/src/life.js
+++ projects/life/src/life.js
@@ -25,7 +25,19 @@ class Life {
    * Constructor
    */
   constructor(width, height) {
-    // !!!! IMPLEMENT ME !!!!
+    this.width = width;
+    this.height = height;
+
+    this.currentIndex = 0;
+
+    this.frames = [
+      Array2D(width, height),
+      Array2D(width, height)
+    ];
+
+    this.randomize();
+
+    this.clear();
   }
   
   /**
@@ -34,28 +46,115 @@ class Life {
    * This should NOT be modified by the caller
    */
   getCells() {
-    // !!!! IMPLEMENT ME !!!!
+    return this.frames[this.currentIndex];
   }
 
   /**
    * Clear the life grid
    */
   clear() {
-    // !!!! IMPLEMENT ME !!!!
+    const choice = Math.floor(Math.random() * 2);
+    let current = this.cells[this.currentIndex];
+    for(let row = 0; row < this.height; row++) {
+      for(let col = 0; col < this.height; col++) {
+        current[row][col] = choice;
+      }
+    }
   }
   
   /**
    * Randomize the life grid
    */
   randomize() {
-    // !!!! IMPLEMENT ME !!!!
+    let current = this.cells[this.currentIndex];
+    for(let row = 0; row < this.height; row++) {
+      for(let col = 0; col < this.height; col++) {
+        current[row][col] = (Math.floor(Math.random() * 2)) | 0;
+      }
+    }
   }
+  
 
   /**
    * Run the simulation for a single step
    */
   step() {
-    // !!!! IMPLEMENT ME !!!!
+    let bufferIndex = this.currentIndex === 0 ? 1 : 0;
+    let current = this.cells[this.currentIndex];
+    let buffer = this.cells[this.bufferIndex];
+
+    function countNeighbors(row, col) {
+      let neighbors = 0;
+
+      // Top-left
+      if (col > 0 && row > 0) {
+        if (current[row - 1][col - 1] === 1) {
+          neighbors++;
+        }
+      }
+      // Top
+      if (row > 0) {
+        if (current[row - 1][col] === 1) {
+          neighbors++;
+        }
+      }
+      // Top-right
+      if (col < this.width - 1 && row > 0) {
+        if (current[row - 1][col + 1] === 1) {
+          neighbors++;
+        }
+      }
+
+      // Left
+      if (col > 0) {
+        if (current[row][col - 1] === 1) {
+          neighbors++;
+        }
+      }
+      // Right
+      if (col < this.width - 1) {
+        if (current[row][col + 1] === 1) {
+          neighbors++;
+        }
+      }
+
+      // Bottom-left
+      if (col > 0 && row < this.height - 1) {
+        if (current[row + 1][col - 1] === 1) {
+          neighbors++;
+        }
+      }
+      // Bottom
+      if (row < this.height - 1) {
+        if (current[row + 1][col] === 1) {
+          neighbors++;
+        }
+      }
+      // Bottom-right
+      if (col < this.width - 1 && row < this.height - 1) {
+        if (current[row + 1][col + 1] === 1) {
+          neighbors++;
+        }
+      }
+
+      return neighbors;
+    }
+
+    for(let row = 0; row < this.height; row++) {
+      for(let col = 0; col < this.width; col++) {
+        const alive = current[row][col];        
+        const numberOfNeighbors = countNeighbors.call(this, row, col);
+
+        if (alive) {
+          buffer[row][col] = (numberOfNeighbors != 2 && numberOfNeighbors != 3) ?
+          0 : 1;
+        } else {
+          buffer[row][col] = (numberOfNeighbors === 3) ?
+          1 : 0;
+        }
+      }
+    }
+    this.currentIndex = this.currentIndex === 0 ? 1 : 0;
   }
 }
 
