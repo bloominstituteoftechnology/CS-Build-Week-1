3f9a5eaa72e002b6c9760545be97776882f33f5f
Dixie Korley <dixiekorley@gmail.com>
2018-05-16 18:35:39 -0400

    Lecture notes

--- guided-demo/cca/src/cca.js
+++ guided-demo/cca/src/cca.js
@@ -14,7 +14,6 @@ function Array2D(width, height) {
 	for (let i = 0; i < height; i++) {
 	  a[i] = new Array(width);
 	}
-  
 	return a;
 }
   
@@ -39,24 +38,81 @@ class CCA {
    * This should NOT be modified by the caller
    */
   getCells() {
+    return this.buffer[this.currentBufferIndex];
   }
 
   /**
    * Clear the cca grid
    */
   clear() {
+    for (let y = 0; y < this.height; y++) {
+      this.buffer[this.currentBufferIndex][y].fill(0);
+    }
   }
 
   /**
    * Randomize the cca grid
    */
   randomize() {
+    const buffer = this.buffer[this.currentBufferIndex];
+
+    for (let y = 0; y < this.height; y++) {
+      for (let x = 0; x < this.width; x++) {
+        buffer[y][x] = Math.floor(Math.random() * MODULO);
+      }
+    }
   }
 
   /**
    * Run the simulation for a single step
    */
   step() {
+    let backBufferIndex = this.currentBufferIndex === 0? 1: 0;
+    let currentBuffer = this.buffer[this.currentBufferIndex];
+    let backBuffer = this.buffer[backBufferIndex];
+
+    // Helper function to see if cell has "infectious" neighbor
+    const hasInfectiousNeighbor = (x, y) => {
+      const nextValue = (currentBuffer[y][x] + 1) % MODULO;
+
+      // West neighbor
+      if (x > 0) {
+        if (currentBuffer[y][x - 1] === nextValue) {
+          return true;
+        }
+      }
+      // North neighbor
+      if (y > 0) {
+        if (currentBuffer[y - 1][x + 1] === nextValue) {
+          return true;
+        }
+      }
+      // East neighbor
+      if (x < this.width - 1) {
+        if (currentBuffer[y][x + 1] === nextValue) {
+          return true;
+        }
+      }
+      // South neighbor
+      if (y < this.height - 1) {
+        if (currentBuffer[y + 1][x] === nextValue) {
+          return true;
+        }
+      }
+      return false;
+    }
+
+    // Loop through the currentBuffer and populate the backBuffer (next generation) based on above helper
+    for (let y = 0; y < this.height; y++) {
+      for (let x = 0; x < this.width; x++) {
+        if (hasInfectiousNeighbor(x, y)) {
+          backBuffer[y][x] = (currentBuffer[y][x] + 1) % MODULO;
+        } else {
+          backBuffer[y][x] = currentBuffer[y][x];
+        }
+      }
+    }
+    this.currentBufferIndex = backBufferIndex
   }
 }
 
