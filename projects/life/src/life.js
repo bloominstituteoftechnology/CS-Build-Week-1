5842a6c0bfc90ce532cea70be4d2ad07a19f40cf
Dixie Korley <dixiekorley@gmail.com>
2018-05-17 16:47:05 -0400

    First draft of step()

--- projects/life/src/life.js
+++ projects/life/src/life.js
@@ -77,6 +77,126 @@ class Life {
    */
   step() {
     // !!!! IMPLEMENT ME !!!!
+
+    // Fill the offscreen buffer with the next life generation built
+    // from the current buffer.
+
+    let backBufferIndex = this.currentBufferIndex === 0? 1: 0;
+    let currentBuffer = this.buffer[this.currentBufferIndex];
+    let backBuffer = this.buffer[backBufferIndex];
+
+    /**
+     * Count the neighbors of a cell
+     */
+    function countNeighbors(x, y, options={border:'zero'}) {
+      let neighborCount = 0;
+
+      if (options.border === 'wrap') {
+        let north = y - 1;
+        let south = y + 1;
+        let west = x - 1;
+        let east = x + 1;
+
+        // Wrap around the edges
+
+        if (north < 0) {
+          north = this.height - 1;
+        }
+        
+       if (south === this.height) {
+          south = 0;
+        }
+
+        if (west < 0) {
+          west = this.width - 1;
+        } 
+
+        if (east === this.width) {
+          east = 0;
+        }
+
+        neighborCount =
+          currentBuffer[north][west] +
+          currentBuffer[north][x] +
+          currentBuffer[north][east] +
+          currentBuffer[y][west] +
+          currentBuffer[y][east] +
+          currentBuffer[south][west] +
+          currentBuffer[south][x] +
+          currentBuffer[south][east];
+
+      } else if (options.border === 'zero') {
+
+        // Treat out of bounds as zero
+        for (let yOffset = -1; yOffset <= 1; yOffset++) {
+          let yPos = y + yOffset;
+
+          if (yPos < 0 || yPos === this.height) {
+            // Out of bounds
+            continue;
+          }
+
+          for (let xOffset = -1; xOffset <= 1; xOffset++) {
+            let xPos = x + xOffset;
+
+            if (xPos < 0 || xPos === this.width) {
+              // Out of bounds
+              continue;
+            }
+
+            // Don't count center element
+            if (xOffset === 0 && yOffset === 0) {
+              continue;
+            }
+
+            neighborCount += currentBuffer[yPos][xPos];
+          }
+        }
+
+      } else {
+        throw new Error('unknown border option' + options.border);
+      }
+
+      return neighborCount;
+
+    } // countNeighbors()
+
+    // Loop through and decide if the next generation is alive or dead
+    // for each cell processed.
+
+    for (let y = 0; y < this.height; y++) {
+      for (let x = 0; x < this.width; x++) {
+
+        let neighborCount = (countNeighbors.bind(this))(x, y);
+
+        let thisCell = currentBuffer[y][x];
+
+        if (thisCell === 1) {
+       
+          if (neighborCount < 2 || neighborCount > 3) {
+        
+            backBuffer[y][x] = 0;
+          } else {
+          
+            backBuffer[y][x] = 1;
+          }
+        } else {
+   
+          if (neighborCount === 3) {
+     
+            backBuffer[y][x] = 1;
+          } else {
+  
+            backBuffer[y][x] = 0;
+          }
+        }
+      }
+    }
+
+    // Now the backBuffer is populated with the next generation life
+    // data. So we declare that to be the new current buffer.
+    
+    this.currentBufferIndex = this.currentBufferIndex === 0? 1: 0;
   }
 }
 
