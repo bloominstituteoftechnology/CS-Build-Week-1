73f7423636af79db04adf5f1d258034b6d32d003
Peter Gray <peter@petergraycreative.com>
2018-05-16 22:32:52 -0500

    Working cellular automata
    NOT conways games

--- projects/life/src/App.js
+++ projects/life/src/App.js
@@ -2,6 +2,17 @@ import React, { Component } from 'react';
 import Life from './life';
 import './App.css';
 
+const COLORS = [
+  [0, 0, 0],
+  [0x8f, 0, 0x5f],
+  [0x5f, 0, 0x8f],
+  [0, 0, 0xff],
+  [0, 0x5f, 0x7f],
+  [0x5f, 0x8f, 0x7f],
+  [0x8f, 0xff, 0x7f],
+  [0xff, 0x5f, 0x7f],
+]
+
 /**
  * Life canvas
  */
@@ -28,16 +39,44 @@ class LifeCanvas extends Component {
    * Handle an animation frame
    */
   animFrame() {
-    //
-    // !!!! IMPLEMENT ME !!!!
-    //
+    let cells = this.life.getCells();
 
-    // Request another animation frame
-    // Update life and get cells
-    // Get canvas framebuffer, a packed RGBA array
-    // Convert the cell values into white or black for the canvas
-    // Put the new image data back on the canvas
-    // Next generation of life
+    let canvas = this.refs.canvas;
+    let ctx = canvas.getContext('2d');
+
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, this.props.width, this.props.height)
+
+    let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+    // // Here is the screen buffer array we can manipulate:
+
+    // imageData.data[0] = 0;
+    // imageData.data[1] = 0;
+    // imageData.data[2] = 0;
+
+    // Set the pixel at 10,20 to pure red and display on the canvas:
+
+    let buffer = imageData.data; // Obtained from getImageData()
+
+    for(let row = 0; row < this.props.height; row++) {
+      for(let col = 0; col < this.props.width; col++){
+        let index = (row * this.props.width + col) * 4;
+
+        let currentNumber = cells[row][col];
+
+        buffer[index] = COLORS[currentNumber][0];
+        buffer[index + 1] = COLORS[currentNumber][1];
+        buffer[index + 2] = COLORS[currentNumber][2];
+        buffer[index + 3] = 0xff;
+      }
+    }
+
+    ctx.putImageData(imageData, 0, 0);
+
+    //ctx.putImageData(imageData, 0, 0);
+    this.life.step();
+    requestAnimationFrame(() => {this.animFrame()});
   }
 
   /**
--- projects/life/src/life.js
+++ projects/life/src/life.js
@@ -1,3 +1,4 @@
+const secureRandom = require('secure-random')
 /**
  * Implementation of Conway's game of Life
  */
@@ -25,7 +26,19 @@ class Life {
    * Constructor
    */
   constructor(width, height) {
-    // !!!! IMPLEMENT ME !!!!
+    this.width = width;
+    this.height = height;
+
+    this.currentBufferIndex = 0;
+
+    this.cells = [
+      Array2D(width, height),
+      Array2D(width, height)
+    ]
+
+    this.randomize();
+
+    this.clear();
   }
   
   /**
@@ -35,6 +48,7 @@ class Life {
    */
   getCells() {
     // !!!! IMPLEMENT ME !!!!
+    return this.cells[this.currentBufferIndex];
   }
 
   /**
@@ -42,6 +56,7 @@ class Life {
    */
   clear() {
     // !!!! IMPLEMENT ME !!!!
+
   }
   
   /**
@@ -49,13 +64,66 @@ class Life {
    */
   randomize() {
     // !!!! IMPLEMENT ME !!!!
+    let buffer = this.cells[this.currentBufferIndex];
+    for(let row = 0; row < this.height; row++) {
+      for(let col = 0; col < this.width; col++) {
+        buffer[row][col] = Math.random() * 1 | 0;
+      }
+    }
   }
 
   /**
    * Run the simulation for a single step
    */
-  step() {
-    // !!!! IMPLEMENT ME !!!!
+    step() {
+    let backBufferIndex = this.currentBufferIndex === 0 ? 1 : 0;
+    let currentBuffer = this.cells[this.currentBufferIndex];
+    let backBuffer = this.cells[backBufferIndex];
+
+    function hasInfectiousNeighbor(row, col){
+      const nextValue = (currentBuffer[row][col] + 1) % 8;
+
+      // West
+      if(col > 0){
+        if (currentBuffer[row][col - 1] === nextValue){
+          return true;
+        }
+      }
+      
+      // North
+      if(row > 0){
+        if (currentBuffer[row - 1][col] === nextValue){
+          return true;
+        }
+      }
+
+      // East
+      if(col < this.width - 1){
+        if (currentBuffer[row][col + 1] === nextValue){
+          return true;
+        }
+      }
+
+      // South
+      if(row < this.height - 1){
+        if (currentBuffer[row + 1][col] === nextValue) {
+          return true;
+        }
+      }
+
+      return false;
+    }
+
+    for(let row = 0; row < this.height; row++) {
+      for(let col = 0; col < this.width; col++) {
+        if (hasInfectiousNeighbor.call(this, row, col)){
+          backBuffer[row][col] = (currentBuffer[row][col] + 1 ) % 8;  //Change to infection
+        } else {
+          backBuffer[row][col] = currentBuffer[row][col]; //no change
+        }
+      }
+    }
+    this.currentBufferIndex = this.currentBufferIndex === 0? 1: 0;
   }
 }
 
