28975cfb2680c4e8597efb856b2f8bbf7c5798ec
Charlie Sparks <csparks@antioch.edu>
2018-05-17 13:16:23 -0700

    fixed an error that happened when looping over arrays

--- projects/life/src/life.js
+++ projects/life/src/life.js
@@ -10,7 +10,7 @@ function Array2D(width, height) {
   let a = new Array(height);
 
   for (let i = 0; i < height; i++) {
-    a[i] = new Array(width);
+    a[i] = new Array(width).fill(0);
   }
 
   return a;
@@ -29,10 +29,10 @@ class Life {
     this.width = width;
     this.height = height;
 
-    this.currentBuffer = 0;
+    this.currentBufferIndex = 0;
     this.buffer = [
-      Array2D(width, height),
-      Array2D(width, height)
+      Array2D(this.width, this.height),
+      Array2D(this.width, this.height)
     ]
   }
   
@@ -43,7 +43,7 @@ class Life {
    */
   getCells() {
     // !!!! IMPLEMENT ME !!!!
-    return this.buffer[this.currentBuffer];
+    return this.buffer[this.currentBufferIndex];
   }
 
   /**
@@ -51,9 +51,9 @@ class Life {
    */
   clear() {
     // !!!! IMPLEMENT ME !!!!
-    for (let x = 0; x <= this.height; x++){
-      for (let y = 0; y <= this.width; y++){
-        this.buffer[this.currentBuffer][x][y] = 0;
+    for (let x = 0; x < this.buffer[this.currentBufferIndex].length; x++){
+      for (let y = 0; y < this.buffer[this.currentBufferIndex][x].length; y++){
+        this.buffer[this.currentBufferIndex][x][y] = 0;
       }
     }
   }
@@ -63,9 +63,9 @@ class Life {
    */
   randomize() {
     // !!!! IMPLEMENT ME !!!!
-    for (let x = 0; x <= this.height; x++){
-      for (let y = 0; y <= this.width; y++){
-        this.buffer[this.currentBuffer][x][y] = Math.floor(Math.random() * 2);
+    for (let x = 0; x < this.height; x++){
+      for (let y = 0; y < this.width; y++){
+        this.buffer[this.currentBufferIndex][x][y] = Math.floor(Math.random() * 2);
       }
     }
   }
@@ -75,6 +75,16 @@ class Life {
    */
   step() {
     // !!!! IMPLEMENT ME !!!!
+    var backBufferIndex;
+    if (this.currentBufferIndex) backBufferIndex = 0;
+    else backBufferIndex = 1;
+
+    const currentBuffer = this.buffer[this.currentBufferIndex];
+    const backBuffer = this.buffer[backBufferIndex];
+
+    const checkCells = (x, y) => {
+
+    }
   }
 }
 
