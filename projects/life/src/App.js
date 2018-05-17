81f9b4a8b05af8143d40fad9e1c8e2fc7cfb52d7
Christopher Beards <beardscm@gmail.com>
2018-05-17 13:13:35 -0400

    Working Implementation

--- projects/life/src/App.js
+++ projects/life/src/App.js
@@ -1,12 +1,16 @@
-import React, { Component } from 'react';
-import Life from './life';
-import './App.css';
+import React, { Component } from "react";
+import Life from "./life";
+import "./App.css";
+
+const COLORS = [
+  [0x00, 0x00, 0x00], // Black
+  [0x00, 0xff, 0xff]  // White
+];
 
 /**
  * Life canvas
  */
 class LifeCanvas extends Component {
-
   /**
    * Constructor
    */
@@ -21,30 +25,64 @@ class LifeCanvas extends Component {
    * Component did mount
    */
   componentDidMount() {
-    requestAnimationFrame(() => {this.animFrame()});
+    requestAnimationFrame(() => {
+      this.animFrame();
+    });
   }
 
   /**
    * Handle an animation frame
    */
   animFrame() {
-    //
-    // !!!! IMPLEMENT ME !!!!
-    //
+    const cells = this.life.getCells();
+    const height = this.props.height;
+    const width = this.props.width;
+
+    // Get canvas framebuffer, a packed RGBA array
+    const canvas = this.refs.canvas;
+    let ctx = canvas.getContext("2d");
+    
+    canvas.style.width = (canvas.width * 1) + "px";
+    canvas.style.height = (canvas.height * 1) + "px";
+    let imageData = ctx.getImageData(0, 0, width, height);
+
+    // Convert the cell values into white or black for the canvas
+    for (let y = 0; y < height; y++) {
+      for (let x = 0; x < width; x++) {
+        const state = cells[y][x];
+        const color = COLORS[state];
+        const index = (y * width + x) * 4;
+
+        imageData.data[index + 0] = color[0]; // red
+        imageData.data[index + 1] = color[1]; // green
+        imageData.data[index + 2] = color[2]; // blue
+        imageData.data[index + 3] = 0xff; // alpha, 0xff === 255 === opaque
+      }
+    }
+
+    // Put the new image data back on the canvas
+    ctx.putImageData(imageData, 0, 0);
+
+    // Next generation of life
+    this.life.step();
 
     // Request another animation frame
-    // Update life and get cells
-    // Get canvas framebuffer, a packed RGBA array
-    // Convert the cell values into white or black for the canvas
-    // Put the new image data back on the canvas
-    // Next generation of life
+    requestAnimationFrame(() => {
+      this.animFrame();
+    });
   }
 
   /**
    * Render
    */
   render() {
-    return <canvas ref="canvas" width={this.props.width} height={this.props.height} />
+    return (
+      <canvas
+        ref="canvas"
+        width={this.props.width}
+        height={this.props.height}
+      />
+    );
   }
 }
 
@@ -52,16 +90,19 @@ class LifeCanvas extends Component {
  * Life holder component
  */
 class LifeApp extends Component {
-
   /**
    * Render
+   * <LifeCanvas width={600} height={500} style="width:1200px;height:1000px;"/>
+   * <LifeCanvas width={600} height={500}/>
    */
   render() {
     return (
       <div>
-        <LifeCanvas width={400} height={300} />
+        <div class="canvas">
+          <LifeCanvas width={600} height={500} />
+        </div>
       </div>
-    )
+    );
   }
 }
 
@@ -69,7 +110,6 @@ class LifeApp extends Component {
  * Outer App component
  */
 class App extends Component {
-
   /**
    * Render
    */
--- projects/life/src/life.js
+++ projects/life/src/life.js
@@ -6,13 +6,10 @@
  * Make a 2D array helper function
  */
 function Array2D(width, height) {
-  //NOTE:  Iterate through Array2D row first then column
   let a = new Array(height);
-
   for (let i = 0; i < height; i++) {
     a[i] = new Array(width);
   }
-
   return a;
 }
 
@@ -20,28 +17,27 @@ function Array2D(width, height) {
  * Life class
  */
 class Life {
-
   /**
    * Constructor
    */
   constructor(width, height) {
-    // !!!! IMPLEMENT ME !!!!
     this.width = width;
     this.height = height;
-    this.currentBuffer = 0; //current buffer index
-    this.buffer = [Array2D(width, height), Array2D(width, height)];
-    this.clear();
 
+    this.currentBufferIndex = 0;
+    this.buffer = [Array2D(width, height), Array2D(width, height)];
+
+    this.clear();
   }
-  
+
   /**
    * Return the current active buffer
-   * 
+   *
    * This should NOT be modified by the caller
    */
   getCells() {
     // !!!! IMPLEMENT ME !!!!
-    return this.buffer[this.currentBuffer]; //returns at current index
+    return this.buffer[this.currentBufferIndex];
   }
 
   /**
@@ -49,18 +45,20 @@ class Life {
    */
   clear() {
     // !!!! IMPLEMENT ME !!!!
-    this.buffer[this.currentBuffer].forEach(elem => elem.fill(0));
+    // for (let y = 0; y < this.height; y++) {
+    //   this.buffer[this.currentBufferIndex][y].fill(0);
+    // }
+    this.buffer[this.currentBufferIndex].forEach(each => each.fill(0));
   }
-  
+
   /**
    * Randomize the life grid
    */
   randomize() {
     // !!!! IMPLEMENT ME !!!!
-    let buffer = this.buffer[this.currentBuffer];
-    for (let y = 0; y < this.height; y ++) {
+    for (let y = 0; y < this.height; y++) {
       for (let x = 0; x < this.width; x++) {
-        buffer[x][y] = (Math.random() * 2);
+        this.buffer[this.currentBufferIndex][y][x] = Math.floor(Math.random() * 2);
       }
     }
   }
@@ -69,7 +67,103 @@ class Life {
    * Run the simulation for a single step
    */
   step() {
-    // !!!! IMPLEMENT ME !!!!
+    let backBufferIndex = this.currentBufferIndex === 0 ? 1 : 0;
+    let currentBuffer = this.buffer[this.currentBufferIndex];
+    let backBuffer = this.buffer[backBufferIndex];
+
+    function hasLivingNeighbor(x, y) {
+      let counter = 0;
+
+      // North 
+      if (y > 0) {
+        if (currentBuffer[y - 1][x] === 1) {
+          counter++;
+        }
+      }
+
+      // Northeast
+      if (y > 0 && x < this.width - 1) {
+        if (currentBuffer[y - 1][x + 1] === 1) {
+          counter++;
+        }
+      }
+
+      // East
+      if (x < this.width - 1) {
+        if (currentBuffer[y][x + 1] === 1) {
+          counter++;
+        }
+      }
+
+      // Southeast
+      if (x < this.width - 1 && y < this.height - 1) {
+        if (currentBuffer[y + 1][x + 1] === 1) {
+          counter++;
+        }
+      }
+
+      // South
+      if (y < this.height - 1) {
+        if (currentBuffer[y + 1][x] === 1) {
+          counter++;
+        }
+      }
+
+      // Southwest
+      if (y < this.height - 1 && x > 0) {
+        if (currentBuffer[y + 1][x - 1] === 1) {
+          counter++;
+        }
+      }
+
+      // West
+      if (x > 0) {
+        if (currentBuffer[y][x - 1] === 1) {
+          counter++;
+        }
+      }
+
+      // Northwest
+      if (x > 0 && y > 0) {
+        if (currentBuffer[y - 1][x - 1] === 1) {
+          counter++;
+        }
+      }
+
+      return counter;
+    }
+
+    // loop through and decide on the state of the next generation for each cell processed
+    for (let y = 0; y < this.height; y++) {
+      for (let x = 0; x < this.width; x++) {
+        let neighborCounter = hasLivingNeighbor.call(this, x, y);
+
+        // alive
+        if (currentBuffer[y][x] === 1) {
+          // if more than 2 but less than 3
+          if (neighborCounter < 2 || neighborCounter > 3) {
+            // cell dies
+            backBuffer[y][x] = 0;
+          }
+          // if there are 2 or 3 live neighbors
+          if (neighborCounter === 2 || neighborCounter === 3) {
+            // cell lives
+            backBuffer[y][x] = 1;
+          }
+        }
+        // if this cell is dead
+        if (currentBuffer[y][x] === 0) {
+          // if there are 3 live neighbors
+          if (neighborCounter === 3) {
+            backBuffer[y][x] = 1;
+          } else {
+            backBuffer[y][x] = 0;
+          }
+        }
+      }
+    }
+
+    this.currentBufferIndex = this.currentBufferIndex === 0 ? 1 : 0;
   }
 }
 
