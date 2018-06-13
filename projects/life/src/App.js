0076204dec25e346d94fff08a7b0401f9ab6ffe0
zackhitch <zackhitch@users.noreply.github.com>
2018-06-13 15:46:36 -0700

    Conways-Life guided demo and life project - ZH

--- projects/life/src/App.js
+++ projects/life/src/App.js
@@ -2,11 +2,18 @@ import React, { Component } from 'react';
 import Life from './life';
 import './App.css';
 
+const canvasWidth = 400;
+const canvasHeight = 300;
+
+const COLORS = [
+  [0x00, 0x00, 0x00], // Black
+  [0xff, 0xff, 0xff], // White
+];
+
 /**
  * Life canvas
  */
 class LifeCanvas extends Component {
-
   /**
    * Constructor
    */
@@ -21,7 +28,9 @@ class LifeCanvas extends Component {
    * Component did mount
    */
   componentDidMount() {
-    requestAnimationFrame(() => {this.animFrame()});
+    requestAnimationFrame(() => {
+      this.animFrame();
+    });
   }
 
   /**
@@ -31,20 +40,52 @@ class LifeCanvas extends Component {
     //
     // !!!! IMPLEMENT ME !!!!
     //
-
     // Request another animation frame
     // Update life and get cells
     // Get canvas framebuffer, a packed RGBA array
     // Convert the cell values into white or black for the canvas
     // Put the new image data back on the canvas
     // Next generation of life
+    let canvas = this.refs.canvas;
+    let ctx = canvas.getContext('2d');
+    let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    let cells = this.life.getCells();
+    let height = this.props.height;
+    let width = this.props.width;
+    let buffer = imageData.data;
+
+    for (let h = 0; h < height; h++) {
+      for (let w = 0; w < width; w++) {
+        let index = (h * width + w) * 4;
+
+        let lifeStatus = cells[h][w];
+
+        buffer[index + 0] = COLORS[lifeStatus][0];
+        buffer[index + 1] = COLORS[lifeStatus][1];
+        buffer[index + 2] = COLORS[lifeStatus][2];
+        buffer[index + 3] = 255;
+      }
+    }
+
+    ctx.putImageData(imageData, 0, 0);
+
+    this.life.step();
+    requestAnimationFrame(() => {
+      this.animFrame();
+    });
   }
 
   /**
    * Render
    */
   render() {
-    return <canvas ref="canvas" width={this.props.width} height={this.props.height} />
+    return (
+      <canvas
+        ref="canvas"
+        width={this.props.width}
+        height={this.props.height}
+      />
+    );
   }
 }
 
@@ -52,16 +93,15 @@ class LifeCanvas extends Component {
  * Life holder component
  */
 class LifeApp extends Component {
-
   /**
    * Render
    */
   render() {
     return (
       <div>
-        <LifeCanvas width={400} height={300} />
+        <LifeCanvas width={this.props.width} height={this.props.height} />
       </div>
-    )
+    );
   }
 }
 
@@ -69,7 +109,6 @@ class LifeApp extends Component {
  * Outer App component
  */
 class App extends Component {
-
   /**
    * Render
    */
--- projects/life/src/life.js
+++ projects/life/src/life.js
@@ -20,21 +20,31 @@ function Array2D(width, height) {
  * Life class
  */
 class Life {
-
   /**
    * Constructor
    */
   constructor(width, height) {
     // !!!! IMPLEMENT ME !!!!
+    this.width = width;
+    this.height = height;
+
+    this.cells = [Array2D(width, height), Array2D(width, height)];
+
+    this.currentBufferIndex = 0;
+
+    this.randomize();
+
+    this.clear();
   }
-  
+
   /**
    * Return the current active buffer
-   * 
+   *
    * This should NOT be modified by the caller
    */
   getCells() {
     // !!!! IMPLEMENT ME !!!!
+    return this.cells[this.currentBufferIndex];
   }
 
   /**
@@ -42,13 +52,19 @@ class Life {
    */
   clear() {
     // !!!! IMPLEMENT ME !!!!
+    this.cells[this.currentBufferIndex].forEach(item => item.fill(0));
   }
-  
+
   /**
    * Randomize the life grid
    */
   randomize() {
     // !!!! IMPLEMENT ME !!!!
+    for (let h = 0; h < this.height; h++) {
+      for (let w = 0; w < this.width; w++) {
+        this.cells[this.currentBufferIndex][h][w] = (Math.random() * 2) | 0;
+      }
+    }
   }
 
   /**
@@ -56,6 +72,85 @@ class Life {
    */
   step() {
     // !!!! IMPLEMENT ME !!!!
+    let currentBuffer = this.cells[this.currentBufferIndex];
+    let backBuffer = this.cells[this.currentBufferIndex === 0 ? 1 : 0];
+
+    function deadOrAlive(height, width) {
+      const nextValue = (currentBuffer[height][width] + 1) % 2;
+      let counter = 0;
+      // North
+      if (height > 0) {
+        if (currentBuffer[height - 1][width] === nextValue) {
+          counter++;
+        }
+      }
+      //NE
+      if (height > 0 && width < this.width - 1) {
+        if (currentBuffer[height - 1][width + 1] === nextValue) {
+          counter++;
+        }
+      }
+      // NW
+      if (height > 0 && width > 0) {
+        if (currentBuffer[height - 1][width - 1] === nextValue) {
+          counter++;
+        }
+      }
+      // South
+      if (height < this.height - 1) {
+        if (currentBuffer[height + 1][width] === nextValue) {
+          counter++;
+        }
+      }
+      //Southwest
+      if (height < this.height - 1 && width > 0) {
+        if (currentBuffer[height + 1][width - 1] === nextValue) {
+          counter++;
+        }
+      }
+      //Southeast
+      if (height < this.height - 1 && width > this.width - 1) {
+        if (currentBuffer[height + 1][width + 1] === nextValue) {
+          counter++;
+        }
+      }
+      // East
+      if (width < this.width - 1) {
+        if (currentBuffer[height][width + 1] === nextValue) {
+          counter++;
+        }
+      }
+      // West
+      if (width > 0) {
+        if (currentBuffer[height][width - 1] === nextValue) {
+          counter++;
+        }
+      }
+    }
+
+    for (let h = 0; h < this.height; h++) {
+      for (let w = 0; w < this.width; w++) {
+        let count = deadOrAlive.call(this, h, w);
+
+        if (currentBuffer[h][w] === 1) {
+          if (count < 2 || count > 3) {
+            backBuffer[h][w] = 0;
+          }
+          if (count === 2 || count === 3) {
+            backBuffer[h][w] = 1;
+          }
+        }
+        if (currentBuffer[h][w] === 0) {
+          if (count === 3) {
+            backBuffer[h][w] = 1;
+          } else {
+            backBuffer[h][w] = 0;
+          }
+        }
+      }
+    }
+
+    this.currentBufferIndex = this.currentBufferIndex === 0 ? 1 : 0;
   }
 }
 
