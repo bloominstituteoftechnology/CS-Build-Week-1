cd548c9cca36c0dd2f3810c92e12a7708bf0b901
Kenny <khpham24@gmail.com>
2018-06-13 16:25:11 -0600

    First commit

--- guided-demo/cca/src/App.js
+++ guided-demo/cca/src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import CCA from './cca';
 import './App.css';
 
+const canvasWidth = 400;
+const canvasHeight = 300;
+
 const COLORS = [
   [0, 0, 0],
   [0x8f, 0, 0x5f],
@@ -10,37 +13,72 @@ const COLORS = [
   [0, 0x5f, 0x7f],
   [0x5f, 0x8f, 0x7f],
   [0x8f, 0xff, 0x7f],
-  [0xff, 0x5f, 0x7f],
-]
+  [0xff, 0x5f, 0x7f]
+];
 
 /**
  * CCA canvas
  */
 class CCACanvas extends Component {
-
   /**
    * Constructor
    */
   constructor(props) {
     super(props);
+
+    this.cca = new CCA(canvasWidth, canvasHeight);
   }
 
   /**
    * Component did mount
    */
   componentDidMount() {
+    this.animFrame();
   }
 
   /**
    * Handle an animation frame
    */
   animFrame() {
+    let canvas = this.refs.canvas;
+    let ctx = canvas.getContext('2d');
+
+    let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    let cells = this.cca.getCells();
+
+    // Here is the screen buffer array we can manipulate:
+
+    let screenBuffer = imageData.data;
+
+    for (let height = 0; height < canvasHeight; height++) {
+      for (let width = 0; width < canvasWidth; width++) {
+        //convert xy to index
+        let index = (height * canvasWidth + width) * 4;
+
+        let ccaStatus = cells[height][width];
+
+        // change pixels to index to match ccaStatus
+
+        screenBuffer[index + 0] = COLORS[ccaStatus][0];
+        screenBuffer[index + 1] = COLORS[ccaStatus][1];
+        screenBuffer[index + 2] = COLORS[ccaStatus][2];
+        screenBuffer[index + 3] = 255;
+      }
+    }
+
+    console.log('screenBuffer in animFrame: ', screenBuffer);
+
+    ctx.putImageData(imageData, 0, 0);
+
+    // step the sim forward
+    this.cca.step();
   }
 
   /**
    * Render
    */
   render() {
+    return <canvas ref="canvas" width={canvasWidth} height={canvasHeight} />;
   }
 }
 
@@ -48,16 +86,15 @@ class CCACanvas extends Component {
  * CCA holder component
  */
 class CCAApp extends Component {
-
   /**
    * Render
    */
   render() {
     return (
       <div>
-        <CCACanvas width={400} height={300} />
+        <CCACanvas width={canvasWidth} height={canvasHeight} />
       </div>
-    )
+    );
   }
 }
 
@@ -65,7 +102,6 @@ class CCAApp extends Component {
  * Outer App component
  */
 class App extends Component {
-
   /**
    * Render
    */
