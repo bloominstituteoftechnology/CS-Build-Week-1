ba92d4f6677500d65d55bd9a0b719fe91d910690
gooseandmegander <meganawilliamson@gmail.com>
2018-06-13 15:14:55 -0700

    wip - going through lecture

--- guided-demo/cca/src/App.js
+++ guided-demo/cca/src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import CCA from './cca';
 import './App.css';
 
+const canvasWidth = 400;
+const canvasHeight = 300;
+
 const COLORS = [
   [0, 0, 0],
   [0x8f, 0, 0x5f],
@@ -11,36 +14,58 @@ const COLORS = [
   [0x5f, 0x8f, 0x7f],
   [0x8f, 0xff, 0x7f],
   [0xff, 0x5f, 0x7f],
-]
+];
 
 /**
  * CCA canvas
  */
 class CCACanvas extends Component {
-
   /**
    * Constructor
    */
   constructor(props) {
     super(props);
+
+    this.cca = new CCA(canvasWidth, canvasHeight);
   }
 
   /**
    * Component did mount
    */
   componentDidMount() {
+    this.animFrame();
   }
 
   /**
    * Handle an animation frame
    */
   animFrame() {
+    let canvas = this.refs.canvas;
+    let ctx = canvas.getContext('2d');
+
+    let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+    // Here is the screen buffer array we can manipulate:
+
+    let screenBuffer = imageData.data;
+
+    for (let i = 0; i < 1000; i += 4) {
+      screenBuffer[i + 0] = 0; //R
+      screenBuffer[i + 1] = 0; //G
+      screenBuffer[i + 2] = 0; //B
+      screenBuffer[i + 3] = 255; //A
+    }
+
+    console.log('screenBuffer in animFrame: ', screenBuffer);
+
+    ctx.putImageData(imageData, 0, 0);
   }
 
   /**
    * Render
    */
   render() {
+    return <canvas ref="canvas" width={canvasWidth} height={canvasHeight} />;
   }
 }
 
@@ -48,16 +73,15 @@ class CCACanvas extends Component {
  * CCA holder component
  */
 class CCAApp extends Component {
-
   /**
    * Render
    */
   render() {
     return (
       <div>
-        <CCACanvas width={400} height={300} />
+        <CCACanvas width={canvasWidth} height={canvasHeight} />
       </div>
-    )
+    );
   }
 }
 
@@ -65,7 +89,6 @@ class CCAApp extends Component {
  * Outer App component
  */
 class App extends Component {
-
   /**
    * Render
    */
