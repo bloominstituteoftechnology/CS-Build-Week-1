c1d14bb8fe7a005278bb22396adfd1a54a3f0914
Robert Rak <robertrak@hotmail.com>
2018-06-13 15:59:03 -0400

    cyclic cellular automata is functional

--- guided-demo/cca/src/cca.js
+++ guided-demo/cca/src/cca.js
@@ -30,6 +30,13 @@ class CCA {
     this.width = width;
     this.height = height;
 
+    this.cells = [
+      Array2D(width, height),
+      Array2D(width, height)
+    ];
+
+    this.currentBufferIndex = 0;
+    this.randomize();
     this.clear();
   }
 
@@ -39,6 +46,7 @@ class CCA {
    * This should NOT be modified by the caller
    */
   getCells() {
+    return this.cells[this.currentBufferIndex];
   }
 
   /**
@@ -51,12 +59,61 @@ class CCA {
    * Randomize the cca grid
    */
   randomize() {
+    for (let height = 0; height < this.height; height++) {
+      for (let width = 0; width < this.width; width++) {
+        this.cells[this.currentBufferIndex][height][width] = (Math.random() * MODULO) | 0;
+      }
+    }
   }
 
   /**
    * Run the simulation for a single step
    */
   step() {
+    let currentBuffer = this.cells[this.currentBufferIndex];
+    let backBuffer = this.cells[this.currentBufferIndex === 0 ? 1 : 0];
+
+    // see if we have a neighbor that can infect this cell and change it's color
+    function hasInfectiousNeighbor(height, width) {
+
+      const nextValue = (currentBuffer[height][width] + 1) % MODULO;
+      // west
+      if (width > 0) {
+        if (currentBuffer[height][width - 1] === nextValue) {
+          return true;
+        }
+      }
+      // north
+      if (height > 0) {
+        if (currentBuffer[height - 1][width] === nextValue) {
+          return true;
+        }
+      }
+      // east
+      if (width < this.width - 1) {
+        if (currentBuffer[height][width + 1] === nextValue) {
+          return true;
+        }
+      }
+      // south
+      if (height < this.height - 1) {
+        if (currentBuffer[height + 1][width] === nextValue) {
+          return true;
+        }
+      }
+    }
+
+    for (let h = 0; h < this.height; h++) {
+      for (let w = 0; w < this.width; w++) {
+        if (hasInfectiousNeighbor.call(this, h, w)) {
+          backBuffer[h][w] = (currentBuffer[h][w] + 1) % MODULO;
+        } else {
+          backBuffer[h][w] = currentBuffer[h][w];
+        }
+      }
+    }
+
+    this.currentBufferIndex = this.currentBufferIndex === 0 ? 1 : 0;
   }
 }
 
