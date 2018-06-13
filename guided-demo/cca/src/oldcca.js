9fbcf87cfeb66289864f86fce0ee228d9c15da9f
Khepf <jeremy1975@gmail.com>
2018-06-13 16:15:30 -0400

    messing around with CA perimeters

--- guided-demo/cca/src/oldcca.js
+++ guided-demo/cca/src/oldcca.js
@@ -30,12 +30,7 @@ class CCA {
     this.height = height;
 
     this.cells = [Array2D(width, height), Array2D(width, height)];
-
-    this.currentBufferIndex = 0;
-
     this.randomize();
-
-    console.log('cells array', this.cells);
 
     this.clear();
   }
@@ -73,53 +68,44 @@ class CCA {
     let currentBuffer = this.cells[this.currentBufferIndex];
     let backBuffer = this.cells[this.currentBufferIndex === 0 ? 1 : 0];
 
-    // See if we have a neighbor that can infect this cell and change its color
     function hasInfectiousNeighbor(height, width) {
-      if (currentBuffer === undefined) {
-        console.log('BROKEN!');
-      }
-
       const nextValue = (currentBuffer[height][width] + 1) % MODULO;
-      // West
+      // WEST
       if (width > 0) {
         if (currentBuffer[height][width - 1] === nextValue) {
           return true;
         }
       }
-      // North
+      // NORTH
       if (height > 0) {
         if (currentBuffer[height - 1][width] === nextValue) {
           return true;
         }
       }
-      // East
+      // EAST
       if (width < this.width - 1) {
         if (currentBuffer[height][width + 1] === nextValue) {
           return true;
         }
       }
-      // South
+      // SOUTH
       if (height < this.height - 1) {
         if (currentBuffer[height + 1][width] === nextValue) {
           return true;
         }
       }
     }
-
-    for (let h = 0; h < this.height; h++) {
-      for (let w = 0; w < this.width; w++) {
-        if (hasInfectiousNeighbor.call(this, h, w)) {
-          backBuffer[h][w] = (currentBuffer[h][w] + 1) % MODULO;
+    for (let height = 0; height < this.height; height++) {
+      for (let width = 0; width < this.width; width++) {
+        if (hasInfectiousNeighbor.call(this, height, width)) {
+          backBuffer[height][width] =
+            (currentBuffer[height][width] + 1) % MODULO;
         } else {
-          backBuffer[h][w] = currentBuffer[h][w];
+          backBuffer[height][width] = currentBuffer[height][width];
         }
       }
     }
-
     this.currentBufferIndex = this.currentBufferIndex === 0 ? 1 : 0;
-
-    // console.log('current', currentBuffer);
-    // console.log('back', backBuffer);
   }
 }
 
