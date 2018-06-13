acb402612b1569c7ecb44bef2913c99f923aad05
Juber Ahmed <a.juber@hotmail.com>
2018-06-13 17:10:52 -0400

    Getting the image height only

--- guided-demo/cca/src/App.js
+++ guided-demo/cca/src/App.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import CCA from './cca';
 import './App.css';
 
+const canvasWidth = 400;
+
+const canvasHeight = 300;
+
 const COLORS = [
   [0, 0, 0],
   [0x8f, 0, 0x5f],
@@ -10,37 +14,62 @@ const COLORS = [
   [0, 0x5f, 0x7f],
   [0x5f, 0x8f, 0x7f],
   [0x8f, 0xff, 0x7f],
-  [0xff, 0x5f, 0x7f],
-]
+  [0xff, 0x5f, 0x7f]
+];
 
 /**
  * CCA canvas
  */
 class CCACanvas extends Component {
-
   /**
    * Constructor
    */
   constructor(props) {
     super(props);
+
+    this.cca = new CCA(canvasWidth, canvasHeight);
   }
 
   /**
    * Component did mount
    */
   componentDidMount() {
+    this.animFrame();
   }
 
   /**
    * Handle an animation frame
    */
   animFrame() {
+    let canvas = this.refs.canvas;
+    let context = canvas.getContext('2d');
+    let imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+
+    let cells = this.cca.getCells();
+    let screenBuffer = imageData.data;
+
+    for (let height = 0; height < canvasHeight; height++) {
+      for (let width = 0; width < canvasWidth; width++) {
+        let index = (height * canvasWidth * width) * 4;
+        let ccaStatus = cells[height][width];
+
+        screenBuffer[index + 0] = COLORS[ccaStatus][0];
+        screenBuffer[index + 1] = COLORS[ccaStatus][1];
+
+        screenBuffer[index + 2] = COLORS[ccaStatus][2];
+
+        screenBuffer[index + 3] = 255;
+      }
+    }
+
+    context.putImageData(imageData, 0, 0);
   }
 
   /**
    * Render
    */
   render() {
+    return <canvas ref="canvas" width={canvasWidth} height={canvasHeight}></canvas>;
   }
 }
 
@@ -48,16 +77,15 @@ class CCACanvas extends Component {
  * CCA holder component
  */
 class CCAApp extends Component {
-
   /**
    * Render
    */
   render() {
     return (
       <div>
-        <CCACanvas width={400} height={300} />
+        <CCACanvas width={canvasWidth} height={canvasHeight} />
       </div>
-    )
+    );
   }
 }
 
@@ -65,7 +93,6 @@ class CCAApp extends Component {
  * Outer App component
  */
 class App extends Component {
-
   /**
    * Render
    */
