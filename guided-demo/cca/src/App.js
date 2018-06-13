d28c966d498ce13746140041619000ed47ad45b6
Tiffany Robbins-Cabell <lillialskye@yahoo.com>
2018-06-13 17:48:53 -0400

    Tiffany Robbins

--- guided-demo/cca/src/App.js
+++ guided-demo/cca/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import CCA from './cca';
 import './App.css';
-
+const canvasWidth=400;
+const canvasHeight= 300;
 const COLORS = [
   [0, 0, 0],
   [0x8f, 0, 0x5f],
@@ -23,24 +24,60 @@ class CCACanvas extends Component {
    */
   constructor(props) {
     super(props);
+
+    this.cca= new CCA(canvasWidth, canvasHeight);
   }
 
   /**
    * Component did mount
    */
   componentDidMount() {
+    //  requestAnimationFrame() {
+        this.animFrame();
+ // };
   }
 
   /**
    * Handle an animation frame
    */
   animFrame() {
+
+    let canvas=this.refs.canvas;
+    let ctx= canvas.getContext('2d');
+
+    let imageData=ctx.getImageData(0, 0, canvas.width. canvas.height);
+   let cells =this.cca.getCells();
+   //
+    let screenBuffer=imageData.data;
+
+    for(let height=0; height<canvasHeight; height++) {
+      for(let width=0; width<canvasWidth; width++) {
+      //conver xy to index
+      let index= (height *canvasWidth+width) *4;
+
+      let ccaStatus=cells[height][width];
+    // change pixels at index to match ccaStatus
+    screenBuffer[index+0] =COLORS[ccaStatus][0]
+    screenBuffer[index+1] =COLORS[ccaStatus][1]
+    screenBuffer[index+2] =COLORS[ccaStatus][2]
+    screenBuffer[index+3] =255;
+
+      }
+    }
+
+    for(let i=0; i<1000; i+=4) {
+      screenBuffer[i+0] =0;
+      screenBuffer[i+1] =0;
+      screenBuffer[i+2] =0;
+      screenBuffer[i+3] =255;
+    }
   }
 
   /**
    * Render
    */
   render() {
+    return <canvas ref="canvas" width={canvasWidth} height={canvasHeight}></canvas>
   }
 }
 
@@ -55,7 +92,7 @@ class CCAApp extends Component {
   render() {
     return (
       <div>
-        <CCACanvas width={400} height={300} />
+        <CCACanvas width={canvasWidth} height={canvasHeight} />
       </div>
     )
   }
--- guided-demo/cca/src/cca.js
+++ guided-demo/cca/src/cca.js
@@ -1,7 +1,7 @@
 /**
  * Implemention of a CCA
  */
-
+export default CCA;
 const MODULO = 8;
 
 /**
@@ -30,7 +30,13 @@ class CCA {
     this.width = width;
     this.height = height;
 
+  this.currentBufferIndex=0;
+  this.cells= [Array2D(width, height), Array2D(width, height)];
+  
+  this.randomize();
+
     this.clear();
+
   }
 
   /**
@@ -39,25 +45,73 @@ class CCA {
    * This should NOT be modified by the caller
    */
   getCells() {
+  
+    return this.cells[this.currentBufferIndex];
+
   }
 
   /**
    * Clear the cca grid
    */
-  clear() {
-  }
+  clear()  {}
 
   /**
    * Randomize the cca grid
    */
   randomize() {
+    for(let height=0; this.height; height++) {
+     for(let width=0; this.width; width++){
+      this.cells[this.currentBufferIndex][height][width]=(Math.random() *MODULO) | 0;
+     } 
+    }
+
   }
 
   /**
    * Run the simulation for a single step
    */
   step() {
+    let currentBuffer =this.cells[this.currentBufferIndex];
+    let backBuffer =this.cells[this.currentBufferIndex === 0? 1:0];
+
+//see if we have a neighbor that can infect this cell and change its color
+    function hasInfectiousNeighbor(height, width){
+     if(currentBuffer ===undefined){
+      console.log('broken');
+     }
+      const nextValue =(currentBuffer[height][width] +1) % MODULO;
+
+      //west
+    if (width > 0) {
+      if(currentBuffer[height][width-1] ===nextValue) {
+        return true;
+
+      }
+    }
+//north
+if (height > 0) {
+  if(currentBuffer[height-1][width]===nextValue){
+    return true;
+    }
+  }
+  //east
+  if (width <this.width-1){
+    if(currentBuffer[height][width+1]===nextValue){
+      return true;
+    }
+  }
+  //south
+  if (height > 0) {
+  if(currentBuffer[height+1][width]===nextValue){
+    return true;
   }
 }
+for(let h=0; height<this.height; h++){
+  for(let w=0; width<this.width; w++){
+if(hasInfectiousNeighbor.call(this, this.h, this.w)){
+   backbuffer[height][width] =(currentBuffer[height][width] +1) % MODULO;
 
-export default CCA;
+}else{
+  backbuffer[height][width] =currentBuffer[height][width];
+}
+this.currentBufferIndex=this.currentBufferIndex=== 0 ? 1: 0;
