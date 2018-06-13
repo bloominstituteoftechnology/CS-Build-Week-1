7bc601906c68809a534753891462feab04e69f50
Edward Manda <lilkups@yahoo.com>
2018-06-13 17:57:12 -0500

    game of life

--- projects/life/src/life.js
+++ projects/life/src/life.js
@@ -1,3 +1,4 @@
+const MODULO = 8;
 /**
  * Implementation of Conway's game of Life
  */
@@ -20,21 +21,28 @@ function Array2D(width, height) {
  * Life class
  */
 class Life {
-
   /**
    * Constructor
    */
   constructor(width, height) {
     // !!!! IMPLEMENT ME !!!!
+    this.width = width;
+    this.height = height;
+
+    this.cells = [Array2D(width, height), Array2D(width, height)];
+    this.currentBufferIndex = 0;
+
+    this.clear();
   }
-  
+
   /**
    * Return the current active buffer
-   * 
+   *
    * This should NOT be modified by the caller
    */
   getCells() {
     // !!!! IMPLEMENT ME !!!!
+    return this.cells[this.currentBufferIndex];
   }
 
   /**
@@ -43,12 +51,18 @@ class Life {
   clear() {
     // !!!! IMPLEMENT ME !!!!
   }
-  
+
   /**
    * Randomize the life grid
    */
   randomize() {
     // !!!! IMPLEMENT ME !!!!
+    for (let height = 0; height < this.height; height++) {
+      for (let width = 0; width < this.width; width++) {
+        this.cells[this.currentBufferIndex][height][width] =
+          (Math.random() * MODULO) | 0;
+      }
+    }
   }
 
   /**
@@ -56,6 +70,8 @@ class Life {
    */
   step() {
     // !!!! IMPLEMENT ME !!!!
+    let currentBuffer = this.cells[this.currentBufferIndex];
+    let backBuffer = this.cells[this.currentBufferIndex % 1];
   }
 }
 
