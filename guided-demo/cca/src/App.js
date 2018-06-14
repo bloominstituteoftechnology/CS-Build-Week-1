65894dd4e0578b484cfc9c7ac453c361c35c13b5
Shaun Kelly <Shaun@redskelly.local>
2018-06-13 23:26:03 -0400

    guided-demo walkthrough/study

--- guided-demo/cca/src/App.js
+++ guided-demo/cca/src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import CCA from './cca';
 import './App.css';
 
+const canvasWidth = 400;
+const canvasHeight = 300;
+
 const COLORS = [
   [0, 0, 0],
   [0x8f, 0, 0x5f],
@@ -11,36 +14,85 @@ const COLORS = [
   [0x5f, 0x8f, 0x7f],
   [0x8f, 0xff, 0x7f],
   [0xff, 0x5f, 0x7f],
-]
+];
 
 /**
  * CCA canvas
  */
 class CCACanvas extends Component {
-
   /**
    * Constructor
    */
   constructor(props) {
     super(props);
+
+    this.cca = new CCA(canvasWidth, canvasHeight);
   }
 
   /**
    * Component did mount
    */
   componentDidMount() {
+    requestAnimationFrame(() => {
+      this.animFrame();
+    });
   }
 
   /**
    * Handle an animation frame
    */
   animFrame() {
+    let canvas = this.refs.canvas;
+    let ctx = canvas.getContext('2d');
+
+    let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    let cells = this.cca.getCells();
+
+    // Here is the screen buffer array we can manipulate:
+
+    let screenBuffer = imageData.data;
+
+    // for (let i = 0; i < 1000; i += 4) {
+    //   screenBuffer[i + 0] = 0; //R
+    //   screenBuffer[i + 1] = 0; //G
+    //   screenBuffer[i + 2] = 0; //B
+    //   screenBuffer[i + 3] = 255; //A
+    // }
+    for (let height = 0; height < canvasHeight; height++) {
+      for (let width = 0; width < canvasWidth; width++) {
+        // convert xy to index (see training kit)
+        let index = (height * canvasWidth + width) * 4;
+
+        let ccaStatus = cells[height][width];
+
+        // change pixels at index to match ccaStatus
+
+        screenBuffer[index + 0] = COLORS[ccaStatus][0];
+        screenBuffer[index + 1] = COLORS[ccaStatus][1];
+        screenBuffer[index + 2] = COLORS[ccaStatus][2];
+        screenBuffer[index + 3] = 255;
+      }
+    }
+
+    // console.log('screenBuffer in animFrame: ', screenBuffer);
+
+    ctx.putImageData(imageData, 0, 0);
+
+    // "step" the information forward
+    this.cca.step();
+    requestAnimationFrame(() => {
+      this.animFrame();
+    });
+
+    ctx.imageSmoothingEnabled = false;
+    ctx.drawImage(canvas, 0, 0, 7 * canvas.width, 7 * canvas.height);
   }
 
   /**
    * Render
    */
   render() {
+    return <canvas ref="canvas" width={canvasWidth} height={canvasHeight} />;
   }
 }
 
@@ -48,16 +100,15 @@ class CCACanvas extends Component {
  * CCA holder component
  */
 class CCAApp extends Component {
-
   /**
    * Render
    */
   render() {
     return (
       <div>
-        <CCACanvas width={400} height={300} />
+        <CCACanvas width={canvasWidth} height={canvasHeight} />
       </div>
-    )
+    );
   }
 }
 
@@ -65,7 +116,6 @@ class CCAApp extends Component {
  * Outer App component
  */
 class App extends Component {
-
   /**
    * Render
    */
