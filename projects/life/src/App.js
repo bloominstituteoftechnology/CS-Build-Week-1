e93c55ac3288da3e2e1245844db0e8be1d20b91c
unknown <luisd824@gmail.com>
2018-06-14 17:18:24 -0400

    Have one implementation of g.o.l where pink cells are slowly fading away

--- projects/life/src/App.js
+++ projects/life/src/App.js
@@ -4,6 +4,18 @@ import './App.css';
 
 const cWidth = 400;
 const cHeight = 300;
+
+const COLORS = [
+  [0, 0, 0],
+  [0x8f, 0, 0x5f],
+  [0x5f, 0, 0x8f],
+  [0, 0, 0xff],
+  [0, 0x5f, 0x7f],
+  [0x5f, 0x8f, 0x7f],
+  [0x8f, 0xff, 0x7f],
+  [0xff, 0x5f, 0x7f]
+];
+
 /**
  * Life canvas
  */
@@ -37,33 +49,37 @@ class LifeCanvas extends Component {
     let canvas = this.refs.canvas;
     let ctx = canvas.getContext('2d');
 
+    let cells = this.life.getCells();
+
     let imageData = ctx.getImageData(0, 0, canvas.height, canvas.width);
 
     let screenBuffer = imageData.data;
 
     for (let height = 0; height < cHeight; height++) {
       for (let width = 0; width < cWidth; width++) {
-        let index = height * +width * 4;
+        let index = (height * cWidth + width) * 4;
 
-        // let red = screenBuffer[index + 0];
-        // let green = screenBuffer[index + 1];
-        // let blue = screenBuffer[index + 2];
-        // let alpha = screenBuffer[index + 3];
+        let lifeStatus = cells[height][width];
+
+        screenBuffer[index + 0] = COLORS[lifeStatus][0];
+        screenBuffer[index + 1] = COLORS[lifeStatus][1];
+        screenBuffer[index + 2] = COLORS[lifeStatus][2];
         screenBuffer[index + 3] = 255;
       }
     }
 
     ctx.putImageData(imageData, 0, 0);
 
+    this.life.step();
+
+    requestAnimationFrame(() => {
+      this.animFrame();
+    });
     // Update life and get cells
     // Get canvas framebuffer, a packed RGBA array
     // Convert the cell values into white or black for the canvas
     // Put the new image data back on the canvas
     // Next generation of life
-
-    requestAnimationFrame(() => {
-      this.animFrame();
-    });
   }
 
   /**
@@ -84,7 +100,7 @@ class LifeApp extends Component {
   render() {
     return (
       <div>
-        <LifeCanvas width={cWidth} height={cHeight} />
+        <LifeCanvas width={400} height={300} />
       </div>
     );
   }
