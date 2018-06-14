a89ce31fa57da2ea607e8566d77d9da1723c5ae5
SaaSha <saasha.pina@gmail.com>
2018-06-14 19:15:31 -0400

    SaaSha W.I.P

--- guided-demo/cca/src/App.js
+++ guided-demo/cca/src/App.js
@@ -23,24 +23,58 @@ class CCACanvas extends Component {
    */
   constructor(props) {
     super(props);
+
+    this.cca = new CCA(props.width, props.height);
++   this.cca.randomize();
   }
 
   /**
    * Component did mount
    */
   componentDidMount() {
+    requestAnimationFrame(() => {
+            this.animFrame();
+          });
   }
 
   /**
    * Handle an animation frame
    */
   animFrame() {
+    const width = this.props.width;
+    const height = this.props.height;
+    let cells = this.cca.getCells();
+
+    let canvas = this.refs.canvas;
+    let ctx = canvas.getContext("2d");
+    let imageData = ctx.getImageData(0, 0, width, height);
+
+    for (let y = 0; y < height; y++) {
+        for (let x = 0; x < width; x++) {
+            //Get the index into imageData
+            const index = (y * width + x) * 4;
+            const status = cells[y][x];
+
+            // Actually update the colors:
+            imageData.data[index + 0] = COLORS[status][0]; //red
+            imageData.data[index + 1] = COLORS[status][1]; //green
+            imageData.data[index + 2] = COLORS[status][2]; //blue
+            imageData.data[index + 3] = 0xff; //alpha, 0xff = opaque
+        }
+    }
   }
 
   /**
    * Render
    */
   render() {
+    return (
+            <canvas
+              ref="canvas"
+              width={this.props.width}
+              height={this.props.height}
+            />
+          );
   }
 }
 
