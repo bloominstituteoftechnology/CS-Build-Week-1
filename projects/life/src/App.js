298c632188034743c33ec9b30affa04c87583740
Austin F <austindanielfrench@gmail.com>
2018-06-14 15:47:05 -0700

    animFrame in app.js

--- projects/life/src/App.js
+++ projects/life/src/App.js
@@ -28,16 +28,35 @@ class LifeCanvas extends Component {
    * Handle an animation frame
    */
   animFrame() {
-    //
-    // !!!! IMPLEMENT ME !!!!
-    //
+    const cells = this.life.getCells();
+    const height = this.props.height;
+    const width = this.props.width;
 
-    // Request another animation frame
-    // Update life and get cells
-    // Get canvas framebuffer, a packed RGBA array
-    // Convert the cell values into white or black for the canvas
-    // Put the new image data back on the canvas
-    // Next generation of life
+    const canvas = this.refs.canvas;
+    const ctx = canvas.getContext('2d');
+    canvas.style.width = canvas.width * 2 + 'px';
+    canvas.style.height = canvas.height * 2 + 'px';
+    let imageData = ctx.getImageData(0, 0, width, height);
+
+    for (let y = 0; y < height; y++) {
+      for (let x = 0; x < width; x++) {
+        const state = cells[y][x];
+        const color = COLORS[state];
+        const index = (y * width + x) * 4;
+
+        imageData.data[index + 0] = color[0];
+        imageData.data[index + 1] = color[1];
+        imageData.data[index + 2] = color[2];
+        imageData.data[index + 3] = 0xff;
+      }
+    }
+    ctx.putImageData(imageData, 0, 0);
+
+    this.life.step();
+
+    requestAnimationFrame(() => {
+      this.animFrame();
+    });
   }
 
   /**
