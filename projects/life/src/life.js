dcc5858fb6e2c78c2c0b44cb4b5dadc3b62b75f5
AJ Genung <DragonRydrz@me.com>
2018-06-14 19:02:48 -0400

    refactoring

--- projects/life/src/life.js
+++ projects/life/src/life.js
@@ -73,26 +73,11 @@ class Life {
     // !!!! IMPLEMENT ME !!!!
     const currentCells = this.cells[this.activeBuffer];
     const nextCells = this.cells[this.activeBuffer === 1 ? 0 : 1];
-    // console.log('start step');
+
     for (let h = 0; h < this.height; h++) {
       for (let w = 0; w < this.width; w++) {
-        // this.flag = false;
-        // if (h === 1 && w === 1) {
-        //   console.log('count: ', this.getLiveNeighborCount.call(this, h, w));
-
-        //   this.flag = true;
-        // }
         const count = this.getLiveNeighborCount.call(this, h, w);
-        // console.log(
-        //   'count: ',
-        //   count,
-        //   'h: ',
-        //   h,
-        //   'w: ',
-        //   w,
-        //   'value: ',
-        //   currentCells[h][w]
-        // );
+
         if (currentCells[h][w]) {
           if (count < 2 || count > 3) {
             nextCells[h][w] = false;
@@ -108,16 +93,6 @@ class Life {
         }
       }
     }
-    // console.log(0, 0, this.cells[this.activeBuffer][0][0]);
-    // console.log(0, 1, this.cells[this.activeBuffer][0][1]);
-    // console.log(0, 2, this.cells[this.activeBuffer][0][2]);
-    // console.log(1, 0, this.cells[this.activeBuffer][1][0]);
-    // console.log(1, 1, this.cells[this.activeBuffer][1][1]);
-    // console.log(1, 2, this.cells[this.activeBuffer][1][2]);
-    // console.log(2, 0, this.cells[this.activeBuffer][2][0]);
-    // console.log(2, 1, this.cells[this.activeBuffer][2][1]);
-    // console.log(2, 2, this.cells[this.activeBuffer][2][2]);
-    // console.log('end step');
     this.activeBuffer = this.activeBuffer === 1 ? 0 : 1;
   }
   // 1. Any live cell with fewer than two live neighbors dies, as if by under population.
@@ -126,44 +101,16 @@ class Life {
     const currentCells = this.cells[this.activeBuffer];
     let c = 0;
 
-    if (h - 1 >= 0) {
-      if (currentCells[h - 1][w - 1]) {
-        if (this.flag) console.log(1, h - 1, w - 1);
-        ++c;
-      }
-      if (currentCells[h - 1][w - 0]) {
-        if (this.flag) console.log(2, h - 1, w);
-        ++c;
-      }
-      if (currentCells[h - 1][w + 1]) {
-        if (this.flag) console.log(3, h - 1, w + 1);
-        ++c;
-      }
-    }
+    if (currentCells[h - 1] && currentCells[h - 1][w - 1]) ++c;
+    if (currentCells[h - 1] && currentCells[h - 1][w - 0]) ++c;
+    if (currentCells[h - 1] && currentCells[h - 1][w + 1]) ++c;
 
-    if (currentCells[h][w - 1]) {
-      if (this.flag) console.log(4, h, w - 1);
-      ++c;
-    }
-    if (currentCells[h][w + 1]) {
-      if (this.flag) console.log(6, h, w + 1);
-      ++c;
-    }
+    if (currentCells[h - 0] && currentCells[h + 0][w - 1]) ++c;
+    if (currentCells[h - 0] && currentCells[h + 0][w + 1]) ++c;
 
-    if (h + 1 < this.height) {
-      if (currentCells[h + 1][w - 1]) {
-        if (this.flag) console.log(7, h + 1, w - 1);
-        ++c;
-      }
-      if (currentCells[h + 1][w - 0]) {
-        if (this.flag) console.log(8, h + 1, w);
-        ++c;
-      }
-      if (currentCells[h + 1][w + 1]) {
-        if (this.flag) console.log(9, h + 1, w + 1);
-        ++c;
-      }
-    }
+    if (currentCells[h + 1] && currentCells[h + 1][w - 1]) ++c;
+    if (currentCells[h + 1] && currentCells[h + 1][w - 0]) ++c;
+    if (currentCells[h + 1] && currentCells[h + 1][w + 1]) ++c;
 
     return c;
   }
