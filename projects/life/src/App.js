f0f6b44552ceacba86f194a56569e7fe9e084bfe
Erik Alonso <mr.alonsoea@protonmail.ch>
2018-06-14 17:21:36 -0500

    wip conwayGame

--- projects/life/src/App.js
+++ projects/life/src/App.js
@@ -1,67 +1,91 @@
-import React, { Component } from 'react';
-import Life from './life';
-import './App.css';
+import React, { Component } from "react";
+import CCA from "./cca";
+import "./App.css";
+
+let canvasWidth = 400;
+let canvasHeight = 300;
+
+const COLORS = [[0, 0, 0], [0x1f, 0, 0x7f]];
 
 /**
- * Life canvas
+ * CCA canvas
  */
-class LifeCanvas extends Component {
-
+class CCACanvas extends Component {
   /**
    * Constructor
    */
   constructor(props) {
     super(props);
 
-    this.life = new Life(props.width, props.height);
-    this.life.randomize();
+    this.cca = new CCA(canvasWidth, canvasHeight);
   }
 
   /**
    * Component did mount
    */
   componentDidMount() {
-    requestAnimationFrame(() => {this.animFrame()});
+    requestAnimationFrame(() => {
+      this.animFrame();
+    });
   }
 
   /**
    * Handle an animation frame
    */
   animFrame() {
-    //
-    // !!!! IMPLEMENT ME !!!!
-    //
+    let canvas = this.refs.canvas;
+    let ctx = canvas.getContext("2d");
 
-    // Request another animation frame
-    // Update life and get cells
-    // Get canvas framebuffer, a packed RGBA array
-    // Convert the cell values into white or black for the canvas
-    // Put the new image data back on the canvas
-    // Next generation of life
+    let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    let cells = this.cca.getCells();
+
+    let screenBuffer = imageData.data;
+
+    for (let height = 0; height < canvasHeight; height++) {
+      for (let width = 0; width < canvasWidth; width++) {
+        // convert xy to  index
+        let index = (height * canvasWidth + width) * 4;
+
+        let ccaStatus = cells[height][width];
+
+        // change pixels at index to match ccaStatus
+
+        screenBuffer[index + 0] = COLORS[ccaStatus][0];
+        screenBuffer[index + 1] = COLORS[ccaStatus][1];
+        screenBuffer[index + 2] = COLORS[ccaStatus][2];
+        screenBuffer[index + 3] = 255;
+      }
+    }
+
+    ctx.putImageData(imageData, 0, 0);
+
+    this.cca.step();
+    requestAnimationFrame(() => {
+      this.animFrame();
+    });
   }
 
   /**
    * Render
    */
   render() {
-    return <canvas ref="canvas" width={this.props.width} height={this.props.height} />
+    return <canvas ref="canvas" width={canvasWidth} height={canvasHeight} />;
   }
 }
 
 /**
- * Life holder component
+ * CCA holder component
  */
-class LifeApp extends Component {
-
+class CCAApp extends Component {
   /**
    * Render
    */
   render() {
     return (
       <div>
-        <LifeCanvas width={400} height={300} />
+        <CCACanvas width={canvasWidth} height={canvasHeight} />
       </div>
-    )
+    );
   }
 }
 
@@ -69,14 +93,13 @@ class LifeApp extends Component {
  * Outer App component
  */
 class App extends Component {
-
   /**
    * Render
    */
   render() {
     return (
       <div className="App">
-        <LifeApp />
+        <CCAApp />
       </div>
     );
   }
