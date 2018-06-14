7226cf99d586917a18a2d19133603a9eebeaec15
Austin F <austindanielfrench@gmail.com>
2018-06-14 15:56:25 -0700

    step & NWES rules in life.js

--- projects/life/src/life.js
+++ projects/life/src/life.js
@@ -64,9 +64,98 @@ class Life {
   /**
    * Run the simulation for a single step
    */
-  step() {
-    // !!!! IMPLEMENT ME !!!!
-  }
+   step() {
+     let backBufferIndex = this.currentBufferIndex === 0 ? 1 : 0;
+     let currentBuffer = this.buffer[this.currentBufferIndex];
+     let backBuffer = this.buffer[backBufferIndex];
+
+     const countNeighbours = (x, y, options = { border: 'zero' }) => {
+       // const nextValue = (currentBuffer[y][x] + 1) % MODULO;
+       let neighbourCount = 0;
+       if (options.border === 'zero') {
+         for (let yOffset = -1; yOffset <= 1; yOffset++) {
+           const yPos = y + yOffset;
+           // out of bounds check
+           if (yPos < 0 || yPos >= this.height) {
+             continue;
+           }
+
+           for (let xOffset = -1; xOffset <= 1; xOffset++) {
+             const xPos = x + xOffset;
+             // out of bounds check
+             if (yPos < 0 || xPos >= this.width) {
+               continue;
+             }
+             // own neighbour check
+             if (xPos === x && yPos === y) {
+               continue;
+             }
+
+             neighbourCount += currentBuffer[yPos][xPos];
+           }
+         }
+       } else if (options.border === 'wrap') {
+         let north = y - 1;
+         let south = y + 1;
+         let west = y - 1;
+         let east = y + 1;
+
+         // check if we need to wrap
+         // n
+         if (north < 0) {
+           north = this.height - 1;
+         }
+         // s
+         if (south >= this.height) {
+           south = this.height - 1;
+         }
+         // w
+         if (west < 0) {
+           west = this.width - 1;
+         }
+         // e
+         if (east < 0) {
+           east = 0;
+         }
+         neighbourCount =
+           currentBuffer[north][west] +
+           currentBuffer[north][x] +
+           currentBuffer[north][east] +
+           currentBuffer[south][west] +
+           currentBuffer[south][x] +
+           currentBuffer[south][east] +
+           currentBuffer[y][west] +
+           currentBuffer[y][east];
+       } else {
+         throw new Error('unknown border option:' + options.border);
+       }
+       // If we've made it this far, we're not infected.
+       return neighbourCount;
+     };
+     // decide state of next generation
+     for (let y = 0; y < this.height; y++) {
+       for (let x = 0; x < this.width; x++) {
+         const neighbours = countNeighbours(x, y);
+         const cell = currentBuffer[y][x];
+
+         if (cell) {
+           if (neighbours > 3 || neighbours < 2) {
+             backBuffer[y][x] = 0;
+           } else {
+             backBuffer[y][x] = 1;
+           }
+         } else {
+           if (neighbours === 3) {
+             backBuffer[y][x] = 1;
+           } else {
+             backBuffer[y][x] = 0;
+           }
+         }
+       }
+     }
+     this.currentBufferIndex = backBufferIndex;
+   }
+ }
 }
 
 export default Life;
