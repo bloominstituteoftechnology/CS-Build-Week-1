8e2120ca4ddb372338b8b554d387759dc2c49944
Dil Sombayeva <dilsombayeva@KevinsMacBook2.attlocal.net>
2018-10-29 15:21:22 -0700

    more css

--- conways/src/App.js
+++ conways/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import Display from './components/Display';
 import Home from './components/Home';
 
 class App extends Component {
@@ -8,7 +7,6 @@ class App extends Component {
         return (
             <div className="App">
                 <header className="App-header">
-                    <Display />
                     <Home />
                 </header>
             </div>
--- conways/src/components/Home.js
+++ conways/src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'reactstrap';
+import Display from './Display';
 
 class Home extends Component {
     constructor(props) {
@@ -16,32 +17,43 @@ class Home extends Component {
                     <Col xs="6" className='centerContent'><h3>Generation: #</h3></Col>
                     <Col xs="6" className='centerContent'><h3>Rules</h3></Col>
                 </Row>
-                <Row>
-                    <Col>.col</Col>
-                    <Col>.col</Col>
-                    <Col>.col</Col>
-                    <Col>.col</Col>
+                <Row className='displayFlex'>
+                    <Col xs="3">
+                        <Display /></Col>
+                    <Col xs="auto" className='dirColumn'>
+                        <button className='presetBut'>Preset 1</button>
+                        <button className='presetBut'>Preset 2</button>
+                        <button className='presetBut'>Preset 3</button>
+                        <button className='presetBut'>Preset 4</button>
+                    </Col>
+                    <Col xs="3">
+                        <ul>
+                            <li>some content</li>
+                            <li>some content</li>
+                            <li>some content</li>
+                            <li>some content</li>
+                            <li>some content</li>
+                            <li>some content</li>
+                            <li>some content</li>
+                            <li>some content</li>
+                        </ul>
+                    </Col>
                 </Row>
                 <Row>
-                    <Col xs="3">.col-3</Col>
-                    <Col xs="auto">.col-auto - variable width content</Col>
-                    <Col xs="3">.col-3</Col>
+                    <Col xs="3" className='thirtyPer displayFlex'>
+                        <button className='playB'>Play</button>
+                        <button className='playB'>Pause</button>
+                        <button className='playB'>Stop</button>
+                    </Col>
+                    <Col xs="9"></Col>
                 </Row>
                 <Row>
-                    <Col xs="6" sm="4">.col-6 .col-sm-4</Col>
-                    <Col xs="6" sm="4">.col-6 .col-sm-4</Col>
-                    <Col sm="4">.col-sm-4</Col>
+                    <Col><h3>About this Algorithm:</h3></Col>
                 </Row>
                 <Row>
-                    <Col sm={{ size: 6, order: 2, offset: 1 }}>.col-sm-6 .order-sm-2 .offset-sm-1</Col>
+                    <Col>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Col>
                 </Row>
-                <Row>
-                    <Col sm="12" md={{ size: 6, offset: 3 }}>.col-sm-12 .col-md-6 .offset-md-3</Col>
-                </Row>
-                <Row>
-                    <Col sm={{ size: 'auto', offset: 1 }}>.col-sm-auto .offset-sm-1</Col>
-                    <Col sm={{ size: 'auto', offset: 1 }}>.col-sm-auto .offset-sm-1</Col>
-                </Row>
+
             </Container>
         );
     }
