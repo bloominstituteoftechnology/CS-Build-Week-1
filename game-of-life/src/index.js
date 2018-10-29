0ceb2f843d032eecb67d0ae5b44d461f42efcb71
Lonnie Davis <ldavis0866@gmail.com>
2018-10-29 16:46:59 -0500

    Game grid rendered

--- game-of-life/src/index.js
+++ game-of-life/src/index.js
@@ -4,7 +4,84 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+class Main extends React.Component {
+    constructor() {
+        super();
+        this.speed = 100;
+        this.rows = 30;
+        this.cols = 50;
+        
+        this.state = {
+            generation: 0,
+            gridFull: Array(this.rows).fill().map(() => Array(this.cols).fill(false))
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>The Game of Life</h1>
+            <Grid
+                gridFull={this.state.gridFull}
+                rows={this.rows}
+                cols={this.cols} 
+                selectBox={this.selectBox}
+            />
+            <h2>Generations: {this.state.generation}</h2>
+            </div>
+        );
+    }
+}
+
+class Box extends React.Component {
+    
+    selectBox = () => {
+        this.props.selectBox(this.props.row, this.props.col);
+    }
+
+    render() {
+        return (
+            <div
+                className={this.props.boxClass}
+                id={this.props.id}
+                onClick={this.selectBox}
+            ></div>
+        );
+    }
+}
+class Grid extends React.Component {
+    render() {
+        const width = (this.props.cols * 16);
+        var rowsArr = [];
+
+        var boxClass ="";
+        for (var i = 0; i < this.props.rows; i++) {
+            for (var j = 0; j < this.props.cols; j++) {
+                let boxId = i + "_" + j;
+
+                boxClass = this.props.gridFull[i][j] ? "box on" : "box off";
+                rowsArr.push(
+                    <Box 
+                    boxClass={boxClass}
+                    key={boxId}
+                    boxId={boxId}
+                    row={i}
+                    col={j}
+                    selectBox={this.props.selectBox}
+                />
+                );
+            }
+        }
+        return (
+            <div className="grid" style={{width: width}}>
+                {rowsArr}
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<Main />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
