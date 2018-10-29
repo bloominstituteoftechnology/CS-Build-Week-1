ac6c17e15df46f6a8423a3039e7c2ecbfc788ae6
Jameson13B <jamesonbrown@neolivity.com>
2018-10-29 13:08:09 -0600

    Added Cell component and created a handleClick function and change colors

---
+++ client/src/Cell.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class Cell extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: false,
+      nextState: false
+    }
+  }
+
+  handleClick = (e) => {
+    this.setState({selected: !this.state.selected})
+  }
+
+  render() { 
+    return (
+      <div className={this.state.selected?"cell active":"cell"}
+        onClick={this.handleClick} >
+      </div>
+    );
+  }
+}
+ 
+export default Cell;
