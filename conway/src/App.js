c3aab7e19fccfe0e929f0d2451b5489d0836be79
noxasaxon <34722463+noxasaxon@users.noreply.github.com>
2018-10-29 10:50:39 -0700

    grid refactored into its own component, began initializing grid into a 2d array of nodes

--- conway/src/App.js
+++ conway/src/App.js
@@ -1,89 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Game from './Components/Game/Game';
 import './App.css';
-
-//start drawing a grid with 50px squares (starting at 0,0)
-
-window.addEventListener('load', eventWindowLoaded, false);
-
-function eventWindowLoaded() {
-  canvasApp(); //包含整个Canvas应用程序
-}
-function canvasSupport(e) {
-  return !!e.getContext;
-}
-function canvasApp() {
-  var myCanvas = document.getElementById('myCanvas');
-
-  if (!canvasSupport(myCanvas)) {
-    return;
-  }
-
-  var ctx = myCanvas.getContext('2d');
-
-  myCanvas.width = window.innerWidth;
-  myCanvas.height = window.innerHeight;
-
-  function drawScreen() {
-    // 横线与竖线的是距
-    var dx = 50;
-    var dy = 50;
-
-    // 初始坐标原点
-    var x = 0;
-    var y = 0;
-    var w = myCanvas.width;
-    var h = myCanvas.height;
-
-    // 单个步长所表示的长度
-
-    var xy = 10;
-
-    ctx.lineWidth = 1;
-
-    // draw horizontal lines
-    while (y < h) {
-      y = y + dy;
-      ctx.moveTo(x, y);
-      ctx.lineTo(w, y);
-
-      // ctx.stroke();
-
-      // 横坐标的数字  unsure what this is
-      // ctx.font = "1px Calibri";
-      // ctx.fillText(xy, x, y);
-      // xy += 10;
-    }
-
-    //draw vertical lines
-    y = 0;
-    xy = 10;
-    while (x < w) {
-      x = x + dx;
-      ctx.moveTo(x, y);
-      ctx.lineTo(x, h);
-      ctx.stroke();
-
-      //纵坐标的数字
-      // ctx.font = "1px Calibri";
-      // ctx.fillText(xy,x,10);
-      // xy+=10;
-    }
-  }
-
-  drawScreen();
-}
-
-// draw a box (each square is 50px wide and )
-// ctx.rect(50, 0, 50, 50);
-// ctx.fillStyle = "red";
-// ctx.fill();
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <canvas id="myCanvas" width="400" height="400" />
+        <Game />
       </div>
     );
   }
---
+++ conway/src/Components/Game/Game.jsx
@@ -0,0 +1,133 @@
+import React, { Component } from 'react';
+// import './Game.css';
+
+//start drawing a grid with 50px squares (starting at 0,0)
+
+window.addEventListener('load', eventWindowLoaded, false);
+
+function eventWindowLoaded() {
+  canvasApp(); 
+}
+function canvasSupport(e) {
+  return !!e.getContext;
+}
+
+let TotalNodesX = window.innerWidth;
+let TotalNodesY = window.innerHeight;
+
+TotalNodesX = Math.round(Math.floor(TotalNodesX / 50))
+TotalNodesY = Math.round(Math.floor(TotalNodesY / 50))
+
+// grid nodes are {x: 0, y:0, xz:50, yz:50}
+
+// const gridOdd = [TotalNodesX][TotalNodesY];
+const gridOdd = [];
+
+let curX = 0;
+let curY = 0;
+for (let i = 0; i < TotalNodesX; i++){
+   if(i != 0) curX += 50;
+   const newArr = [];
+
+   for(let j = 0; j < TotalNodesY; j++){
+      if(j != 0) curY += 50;
+      newArr.push({x: curX, y: curY, xz:50, yz:50})
+   }
+
+   gridOdd.push(newArr)
+}
+
+console.log(gridOdd)
+
+
+
+function canvasApp() {
+  var myCanvas = document.getElementById('myCanvas');
+
+  if (!canvasSupport(myCanvas)) {
+    return;
+  }
+
+  var ctx = myCanvas.getContext('2d');
+
+
+  const Width = 50 * TotalNodesX;
+  const Height = 50 * TotalNodesY;
+
+  myCanvas.width = Width;
+  myCanvas.height = Height;
+
+
+  function drawScreen() {
+
+    var dx = 50;
+    var dy = 50;
+
+   
+    var x = 0;
+    var y = 0;
+    var w = myCanvas.width;
+    var h = myCanvas.height;
+
+ 
+
+    var xy = 10;
+
+    ctx.lineWidth = 1;
+
+    ctx.moveTo(x, y);
+    ctx.lineTo(w, y);
+
+    ctx.stroke();
+    // draw horizontal lines
+    while (y < h) {
+      y = y + dy;
+      ctx.moveTo(x, y);
+      ctx.lineTo(w, y);
+
+      ctx.stroke();
+
+      // unsure what this is
+      // ctx.font = "1px Calibri";
+      // ctx.fillText(xy, x, y);
+      // xy += 10;
+    }
+
+    //draw vertical lines
+    y = 0;
+    xy = 10;
+    ctx.moveTo(x, y);
+    ctx.lineTo(x, h);
+    ctx.stroke();
+    while (x < w) {
+      x = x + dx;
+      ctx.moveTo(x, y);
+      ctx.lineTo(x, h);
+      ctx.stroke();
+
+
+      // ctx.font = "1px Calibri";
+      // ctx.fillText(xy,x,10);
+      // xy+=10;
+    }
+  }
+
+  drawScreen();
+}
+
+// draw a box (each square is 50px wide and )
+// ctx.rect(50, 0, 50, 50);
+// ctx.fillStyle = "red";
+// ctx.fill();
+
+
+
+class Game extends Component {
+  render() {
+    return (
+        <canvas id="myCanvas" width="400" height="400" />
+    );
+  }
+}
+
+export default Game;
