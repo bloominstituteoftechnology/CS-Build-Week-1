79d920565e1f9fea78d36d6898ada06b3a4487a4
Daniel Chamorro <dan@dcitllc.com>
2018-10-29 14:12:48 -0400

    Set generation state and created Grid component

--- gameoflife/src/index.js
+++ gameoflife/src/index.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Grid extends React.Component {
+  render() {
+    return <div>Grid</div>;
+  }
+}
+
+class Main extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      generations: 0
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>The Game of Life</h1>
+        <Grid />
+        <h2>Generations: {this.state.generations}</h2>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Main />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
