6696146c91aade9697d453c9c7860cd389bd6b84
Simon <Simon_Freelance@hotmail.com>
2018-10-29 16:03:42 -0400

    Basic gatsby config for metadata and styled-components

---
+++ conways-game/gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    siteMetadata: {
+        title: "Conway's Game of Life"
+    },
+    plugins: [
+      {
+        resolve: "gatsby-plugin-styled-components"
+      }
+    ]
+  };
