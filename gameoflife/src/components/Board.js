1cd56794e10ee039d30e91a64c340d00daba2687
Nicky Chen <nicky.chen0@gmail.com>
2018-10-29 12:28:29 -0700

    resizing board

--- gameoflife/src/components/Board.js
+++ gameoflife/src/components/Board.js
@@ -1,29 +1,41 @@
 import React from "react";
 import "./Board.css";
 
-const cell_size = 20;
-const board_width = 900;
-const board_height = 900;
+let cell_size = 20;
+let board_width = 900;
+let board_height = 900;
+let rows = board_height / cell_size; // This lets us manipulate each cell on the board.
+let columns = board_width / cell_size;
 
 class Board extends React.Component {
   constructor() {
     super();
-    this.rows = board_height / cell_size; // This lets us manipulate each cell on the board.
-    this.columns = board_width / cell_size;
     this.board = this.generateBoard(); // Create a board out of the rows and column sizes
     this.state = {
-      cells: []
+      cells: [],
+      gridSize: ""
     };
   }
   handleClick = event => {
     event.preventDefault();
   };
+  handleChange = event => {
+    this.setState({ gridSize: event.target.value });
+  };
+  handleResize = event => {
+    console.log("handleResize is firing");
+    event.preventDefault();
+    board_width = Number(this.state.gridSize);
+    board_height = Number(this.state.gridSize);
+    this.setState({});
+    this.generateBoard();
+  };
 
   generateBoard = () => {
     let cellGrid = [];
-    for (let x = 0; x < this.columns; x++) {
+    for (let x = 0; x < columns; x++) {
       cellGrid[x] = []; // This will create an array of all of x axis of the board.
-      for (let y = 0; y < this.rows; y++) {
+      for (let y = 0; y < rows; y++) {
         cellGrid[x][y] = false; // This will add to the cellGrid array, creating in each array, an array of y's.
       }
     }
@@ -33,6 +45,8 @@ class Board extends React.Component {
   render() {
     console.log("this.board is: ", this.board);
     console.log("this.state.cells is: ", this.state.cells);
+    console.log("this.state.gridSize is: ", this.state.gridSize);
+    console.log("typeof board_width is: ", typeof board_width);
     return (
       <div>
         <div
@@ -44,6 +58,10 @@ class Board extends React.Component {
           }}
           onClick={this.handleClick}
         />
+        <form>
+          <input placeholder="Change grid size to" onChange={this.handleChange} />
+          <button onClick={this.handleResize}>Change</button>
+        </form>
       </div>
     );
   }
