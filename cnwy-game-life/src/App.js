5e381b7e8d36e69fe5c587ea1474161269baa2a7
snowcoding <vimalshah77@gmail.com>
2018-10-29 13:13:44 -0700

    Added onClick Handler and a function to get the absolute (x,y) of canvas

--- cnwy-game-life/src/App.js
+++ cnwy-game-life/src/App.js
@@ -24,15 +24,65 @@ const GenDiv = styled.div`
   margin-top:50px;
 `
 class App extends Component {
+  constructor (){
+    super();
+  }
+  
+  canvasClickHandler = (e) =>{
+    console.log('You clicked on the canvas!');
+    
+    //Get the mouse click's (x,y) coordinate
+    let canvasCoord = this.getCanvasCoord(e);
+    console.log("X: ", canvasCoord.x);
+    console.log("Y: ", canvasCoord.y);
+
+
+
+  } 
+  /**
+   * Get the Canvas (x,y) coordinates
+   * 
+   * Takes in an event from a mouse click to the canvas element
+   * Returns an object with the absolute X and Y values
+   */
+  getCanvasCoord = (e) =>{
+    //Initialize object to hold the return values
+    let canvasCoord = {x:0, y:0};
+
+    //Get a reference to the main-container:
+    let mainContainer = document.getElementById('main-container');
+    
+    //Get it's computed style
+    let nodeStyle = window.getComputedStyle(mainContainer);
+    
+    //Get the marin Left as a string ,ie, 196.4px
+    let mainContXOffsetStr = nodeStyle.getPropertyValue('margin-left');
+    
+    //Drop the last two characters (for px) convert to a number and round up
+    let mainContXOffset = Math.round(Number(mainContXOffsetStr.substring(0, mainContXOffsetStr.length - 2)));
+
+    //Subtract the main-container's X offset and target's offset to get the actual canvas X value
+    canvasCoord.x = e.pageX - (e.target.offsetLeft + mainContXOffset);
+    if (canvasCoord.x < 0) canvasCoord.x = 0;
+    
+    //Subtract the target's Y offset to get the actual canvas Y value
+    canvasCoord.y = e.pageY - e.target.offsetTop;
+    if (canvasCoord.y < 0) canvasCoord.y = 0;
+    
+    return canvasCoord; 
+  }
+
+
   render() {
     return (
       <div className="App">
-        <MainContainer>
+        {/* <LifeCanvas height={500} width={500} clickHandler={this.canvasClickHandler}/> */}
+        <MainContainer id="main-container">
           <Row>
             <Col sm="6">
               <div><h2>Conway's Game of Life</h2></div>
               <GenDiv>Generation: 0</GenDiv>
-              <LifeCanvas className="border" height={500} width={500}/>
+              <LifeCanvas height={500} width={500} clickHandler={this.canvasClickHandler}/>
               <div>Update every <input type="text"/> ms</div>
               <div>Size of grid <input type="text"/> </div>
               <div>Play Button</div>
