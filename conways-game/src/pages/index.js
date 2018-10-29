d59a9cdd231dc994b79bdb5cb4dd2f794862f726
Simon <Simon_Freelance@hotmail.com>
2018-10-29 16:04:12 -0400

    Started stying index

--- conways-game/src/pages/index.js
+++ conways-game/src/pages/index.js
@@ -1,3 +1,26 @@
-import React from "react"
+import React, { Fragment } from "react";
+import { createGlobalStyle } from "styled-components";
 
-export default () => <div>Hello world!</div>
+const GlobalStyle = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+    html {
+        font-family: sans-serif;
+        font-size: 62.5%;
+    }
+    body {
+        font-size: 1.6rem;
+    }
+`;
+
+export default () => {
+  return (
+  <Fragment>
+    <GlobalStyle/>
+    <h2>hello world</h2>
+  </Fragment>
+  );
+};
