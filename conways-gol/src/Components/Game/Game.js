47afa40721da29012d328650e3effc0a88cfd70b
markdavidwilliams <markwilliams.aus@gmail.com>
2018-10-30 17:24:10 -0500

    implemented algorithm and run animation, set interval

--- conways-gol/src/Components/Game/Game.js
+++ conways-gol/src/Components/Game/Game.js
@@ -29,6 +29,8 @@ class Game extends Component {
 
   state = {
     cells: [],
+    interval: 100,
+    isRunning: false,
   }
 
   makeEmptyBoard() {
@@ -79,8 +81,75 @@ class Game extends Component {
     this.setState({ cells: this.makeCells() });
   }
 
+  runGame = () => {
+    this.setState({ isRunning: true });
+    this.runIteration();
+  }
+
+  stopGame = () => {
+    this.setState({ isRunning: false })
+    if (this.timeoutHandler) {
+      window.clearTimeout(this.timeoutHandler);
+      this.timeoutHandler = null;
+    }
+  }
+
+  runIteration() {
+    console.log('running iteration');
+    let newBoard = this.makeEmptyBoard();
+
+    for (let y = 0; y < this.rows; y++) {
+      for (let x = 0; x < this.cols; x++) {
+        let neighbors = this.calculateNeighbors(this.board, x, y);
+        if (this.board[y][x]) {
+          if (neighbors === 2 || neighbors === 3) {
+            newBoard[y][x] = true;
+          } else {
+            newBoard[y][x] = false;
+          }
+        } else {
+          if (!this.board[y][x] && neighbors === 3) {
+            newBoard[y][x] = true;
+          }
+        }
+      }
+    }
+
+    this.board = newBoard;
+    this.setState({ cells: this.makeCells() })
+
+    this.timeoutHandler = window.setTimeout(() => {
+      this.runIteration();
+    }, this.state.interval);
+  }
+
+    /**
+     * Calculate the number of neighbors at point (x, y)
+     * @param {Array} board 
+     * @param {int} x 
+     * @param {int} y 
+     */
+    calculateNeighbors(board, x, y) {
+      let neighbors = 0;
+      const dirs = [[-1, -1], [-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1]];
+      for (let i = 0; i < dirs.length; i++) {
+        const dir = dirs[i];
+        let y1 = y + dir[0];
+        let x1 = x + dir[1];
+
+        if (x1 >= 0 && x1 < this.cols && y1 >= 0 && y1 < this.rows && board[y1][x1]) {
+          neighbors++
+        }
+      }
+      return neighbors;
+    }
+
+  handleIntervalChange = event => {
+    this.setState({ interval: event.target.value })
+  }
+
   render() {
-    const { cells } = this.state;
+    const { cells, interval, isRunning } = this.state;
     return (
       <div>
         <div className='board'
@@ -90,6 +159,14 @@ class Game extends Component {
         {cells.map(cell => (
           <Cell x={cell.x} y={cell.y} key={`${cell.x}, ${cell.y}`} />
         ))}
+        </div>
+        <div className='controls'>
+          Update every <input value={this.state.interval} onChange={this.handleIntervalChange} /> msec
+          {isRunning ? (
+            <button className='button' onClick={this.stopGame}>Stop</button>
+          ) : (
+            <button className='button' onClick={this.runGame}>Run</button>
+          )}
         </div>
       </div>
     )
