cc0f98627d06d19810f3198ead9e9490876631d9
Simon <Simon_Freelance@hotmail.com>
2018-10-29 23:05:51 -0400

    Refactored index.js

---
+++ conways-game/src/components/App.js
@@ -0,0 +1,49 @@
+import React, { Fragment, Component } from "react";
+import { createGlobalStyle } from "styled-components";
+import Canvas from "../components/Canvas";
+import ControlsMenu from "../components/ControlsMenu/ControlsContainer";
+import HeaderPanel from "../components/PresetSection/HeaderPanel";
+import CarouselPanel from "../components/PresetSection/CarouselPanel";
+import { colors } from "../utils/variables";
+
+const GlobalStyle = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+    html, body {
+        height: 100%;
+    }
+    html {
+        font-family: sans-serif;
+        font-size: 62.5%;
+        color: ${colors.white};
+    }
+    body {
+        font-size: 1.6rem;
+    }
+    input[type="search"]::-webkit-search-decoration,
+    input[type="search"]::-webkit-search-cancel-button,
+    input[type="search"]::-webkit-search-results-button,
+    input[type="search"]::-webkit-search-results-decoration {
+        display: none;
+    }
+`;
+
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <Fragment>
+        <GlobalStyle />
+        <Canvas />
+        <ControlsMenu />
+        <HeaderPanel />
+        <CarouselPanel />
+      </Fragment>
+    );
+  }
+}
+
+export default App;
--- conways-game/src/pages/index.js
+++ conways-game/src/pages/index.js
@@ -1,43 +1,7 @@
-import React, { Fragment } from "react";
-import { createGlobalStyle } from "styled-components";
-import Canvas from "../components/Canvas";
-import ControlsMenu from "../components/ControlsMenu/ControlsContainer";
-import HeaderPanel from "../components/PresetSection/HeaderPanel";
-import CarouselPanel from "../components/PresetSection/CarouselPanel";
-import { colors } from "../utils/variables";
-const GlobalStyle = createGlobalStyle`
-    * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-    }
-    html, body {
-        height: 100%;
-    }
-    html {
-        font-family: sans-serif;
-        font-size: 62.5%;
-        color: ${colors.white};
-    }
-    body {
-        font-size: 1.6rem;
-    }
-    input[type="search"]::-webkit-search-decoration,
-    input[type="search"]::-webkit-search-cancel-button,
-    input[type="search"]::-webkit-search-results-button,
-    input[type="search"]::-webkit-search-results-decoration {
-        display: none;
-    }
-`;
-
+import React from 'react';
+import App from "../components/App";
 export default () => {
   return (
-    <Fragment>
-      <GlobalStyle />
-      <Canvas />
-      <ControlsMenu />
-      <HeaderPanel />
-      <CarouselPanel />
-    </Fragment>
+    <App />
   );
 };
