4cf3f139202f9bfb887327c31a80bde926b167f7
snowcoding <vimalshah77@gmail.com>
2018-10-30 10:33:00 -0700

    Refactor - Moved in all canvas related logic from App.js

--- cnwy-game-life/src/components/LifeCanvas.js
+++ cnwy-game-life/src/components/LifeCanvas.js
@@ -19,10 +19,16 @@ class LifeCanvas extends Component {
     // this.generation = 0;
     this.state = {
       generation : 0,
+      gridSize:0,
+      cellSize:0
       // continueAnimation : true
     }
   }
   
+  componentWillMount(){
+    this.setState({gridSize:500, cellSize:25})
+  }
+
   componentDidMount(){
     this.initializeCanvas();
   }
@@ -33,28 +39,27 @@ class LifeCanvas extends Component {
     // this.setState({continueAnimation:false})
   }
 
+  /**
+   * Initialize the canvas
+   * 
+   * @param void
+   * @return void
+   */
   initializeCanvas = ()=>{
     this.setState({generation:0})
-
-    // this.setState({
-    //   generation:0,
-    //   continueAnimation : true
-    // })
 
     let canvas = this.refs.canvas; 
     const ctx = canvas.getContext('2d');
     
     //Fill the ctx with a black box:
-    let gridHeight = this.props.height;
-    let gridWidth = this.props.width;
-    ctx.fillRect(0,0,gridHeight,gridWidth);
+    ctx.fillRect(0,0,this.state.gridSize,this.state.gridSize);
     
     //Fill a single Rect:
-    let sqEdgeLength = this.props.cellSize;
+    let sqEdgeLength = this.state.cellSize;
 
     //Fill Matrix with Squares:
-    for (let i = 0; i<gridHeight/sqEdgeLength; i++){
-      for (let j = 0; j<gridHeight/sqEdgeLength; j++){
+    for (let i = 0; i<this.state.gridSize/sqEdgeLength; i++){
+      for (let j = 0; j<this.state.gridSize/sqEdgeLength; j++){
       ctx.beginPath();
       ctx.rect(i*sqEdgeLength, j*sqEdgeLength, sqEdgeLength, sqEdgeLength);
       ctx.fillStyle = 'white';
@@ -66,6 +71,183 @@ class LifeCanvas extends Component {
     }
   }
 
+
+  /**
+   * Gets the absolute XY coord of the canvas at the location of the mouse click
+   * 
+   * @param e The event of the click on the canva
+   * @return object canvasCoord which will have the x,y coordinates of the click
+   */
+  getCanvasXYcoordFromMouseClick = (e) =>{
+    console.log('You clicked on the canvas!');
+    
+    //Get the mouse click's (x,y) coordinate
+    let canvasCoord = this.getCanvasCoord(e);
+    console.log("X: ", canvasCoord.x);
+    console.log("Y: ", canvasCoord.y);
+    
+    //Get the pixel's RGBA values:
+    let canvas = e.target; 
+    const ctx = canvas.getContext('2d');
+    let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+    // See the screenBuffer
+    // let screenBuffer = imageData.data;
+    // console.log("screen buffer: ", screenBuffer);
+
+    const pixelRGBA =  this.getPixel(imageData, canvasCoord.x, canvasCoord.y);
+    console.log("pixelRGBA: ", pixelRGBA);
+
+    this.toggleCell(canvasCoord, pixelRGBA);
+
+    //Set the pixel to black
+    // this.setPixel(canvas, canvasCoord.x, canvasCoord.y);
+
+  }
+  
+  /**
+   * Toggle cell's color based on the cell's corresponding to the Canvas Coord (x,y)
+   * 
+   * @param canvasCoord = an object that contains the user clicked (x,y) coord
+   * @param pixelRGBA = an array of [R, G, B, A] containing the color of the pixe
+   * @return void
+   */
+  toggleCell = (canvasCoord, pixelRGBA) => {
+    // Get Ref to canvas element
+    let canvas = document.getElementById("canvas");
+    // console.log("canvas in toggle:", canvas);
+    
+    // Get the Context of the canvas Elem
+    const ctx = canvas.getContext('2d');
+    // console.log("context in toggle:", ctx);
+    
+    // Test the imageData
+    // let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    
+    // Test the screenBuffer
+    // let screenBuffer = imageData.data;
+    // console.log("screen buffer: ", screenBuffer);
+
+    // Test the Cell Size
+    // console.log("Cell Size: ", this.state.cellSize);
+
+    // Get the multiplier from (x,y), dividing by the cellsize
+    // allows use to find which cell the user clicked on
+    let xMult = Math.floor(canvasCoord.x/this.state.cellSize);
+    let yMult = Math.floor(canvasCoord.y/this.state.cellSize);
+
+    // Test the pixel color coming in
+    // console.log("pixelRGBA: ", pixelRGBA);
+
+    ctx.beginPath();
+    ctx.rect(xMult*this.state.cellSize, yMult*this.state.cellSize, this.state.cellSize, this.state.cellSize);
+    ctx.fillStyle = (pixelRGBA[0] === 255 && pixelRGBA[1] === 255 && pixelRGBA[1] === 255) ? 'black' : 'white';
+    ctx.fill();
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = 'grey';
+    ctx.stroke();
+  }
+
+
+  /**
+   * Get the Canvas (x,y) coordinates
+   * 
+   * @param e The event of the click on the canva
+   * @return object canvasCoord which will have the x,y coordinates of the click
+   */
+  getCanvasCoord = (e) =>{
+    //Initialize object to hold the return values
+    let canvasCoord = {x:0, y:0};
+
+    //Get a reference to the main-container:
+    let mainContainer = document.getElementById('main-container');
+    
+    //Get it's computed style
+    let nodeStyle = window.getComputedStyle(mainContainer);
+    
+    //Get the margin Left as a string ,ie, 196.4px
+    let mainContXOffsetStr = nodeStyle.getPropertyValue('margin-left');
+    
+    //Drop the last two characters (for px) convert to a number and round up
+    let mainContXOffset = Math.round(Number(mainContXOffsetStr.substring(0, mainContXOffsetStr.length - 2)));
+
+    //Subtract the main-container's X offset and target's offset to get the actual canvas X value
+    canvasCoord.x = e.pageX - (e.target.offsetLeft + mainContXOffset);
+    if (canvasCoord.x < 0) canvasCoord.x = 0;
+    
+    //Subtract the target's Y offset to get the actual canvas Y value
+    canvasCoord.y = e.pageY - e.target.offsetTop;
+    if (canvasCoord.y < 0) canvasCoord.y = 0;
+    
+    return canvasCoord; 
+  }
+
+
+
+  /**
+   * Get a pixel value from imageData
+   *
+   * @param imageData HTML canvas imagedata from getImageData()
+   * @param x X coordinate to get pixels from
+   * @param y Y coordinate to get pixels from
+   * @return Array [R,G,B,A] for the pixel in question, or null if out of bounds
+   */
+  getPixel = (imageData, x, y) => {
+    const w = imageData.width; // Conveniently the width is here
+    const h = imageData.height;
+
+    if (x < 0 || x >= w || y < 0 || y >= h) {
+        // Out of bounds
+        return null;
+    }
+
+    // Compute index within the array
+    const index = (w * y + x) * 4;
+
+    // Return a copy of the R, G, B, and A elements
+    return imageData.data.slice(index, index + 4);
+  }
+
+
+  /**
+   * Put a pixel value from imageData
+   *
+   * @param canvas HTML canvas
+   * @param x X coordinate to get pixels from
+   * @param y Y coordinate to get pixels from
+   * @return void
+   */
+  setPixel = (canvas, x, y) => {
+    const ctx = canvas.getContext('2d');
+    let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+    const w = imageData.width; // Conveniently the width is here
+    const h = imageData.height;
+
+    if (x < 0 || x >= w || y < 0 || y >= h) {
+        // Out of bounds
+        return null;
+    }
+
+    // Compute index within the array
+    const index = (w * y + x) * 4;
+
+    imageData.data[index + 0] = 0x00; // Red: 0xff == 255, full intensity
+    imageData.data[index + 1] = 0x00; // Green: zero intensity
+    imageData.data[index + 2] = 0x00; // Blue: zero intensity
+    imageData.data[index + 3] = 0xff; // Alpha: 0xff == 255, fully opaque
+  
+    ctx.putImageData(imageData, 0, 0);
+  }
+
+
+
+  /**
+   * Animate the canvas 
+   *
+   * @param timestamp
+   * @return void
+   */
   onAnimFrame = (timestamp) => {
     let canvas = this.refs.canvas; 
     const ctx = canvas.getContext('2d');
@@ -93,7 +275,7 @@ class LifeCanvas extends Component {
     // TODO: Do animation stuff to the canvas
     ctx.beginPath();
     // ctx.rect(this.generation++ * this.props.cellSize, 7*this.props.cellSize, this.props.cellSize, this.props.cellSize);
-    ctx.rect(this.state.generation * this.props.cellSize, 7*this.props.cellSize, this.props.cellSize, this.props.cellSize);
+    ctx.rect(this.state.generation * this.state.cellSize, 7*this.state.cellSize, this.state.cellSize, this.state.cellSize);
     ctx.fillStyle = 'black';
     ctx.fill();
     ctx.lineWidth = 1;
@@ -103,6 +285,12 @@ class LifeCanvas extends Component {
     this.setState({generation: this.state.generation + 1} )
   }
 
+
+
+  /**
+   * Game Controllers
+   * 
+   */
   startGame = () => {
     requestAnimationFrame((timestamp) => { this.onAnimFrame(timestamp); });
     this.continueAnimation = true;
@@ -127,13 +315,16 @@ class LifeCanvas extends Component {
     // this.setState({continueAnimation:false})
   }
 
-  
+
+
+  /**
+   * Rendering
+   */
   render() {
     return (
       <div>
-        {/* <GenDiv>Generation: {this.generation}</GenDiv> */}
         <GenDiv>Generation: {this.state.generation}</GenDiv>
-        <canvas ref="canvas" id="canvas" width={this.props.width} height={this.props.height} onClick={this.props.clickHandler}/>
+        <canvas ref="canvas" id="canvas" width={this.state.gridSize} height={this.state.gridSize} onClick={this.getCanvasXYcoordFromMouseClick}/>
         <div>
           <GameBtns onClick={this.startGame}>Start</GameBtns>
           <GameBtns onClick={this.stopGame}>Stop</GameBtns>
