f70928e6e9fc87a00a73e1f44cacdac29763c361
markdavidwilliams <markwilliams.aus@gmail.com>
2018-10-30 15:47:51 -0500

    cells now clickable

--- conways-gol/src/Components/Game/Game.js
+++ conways-gol/src/Components/Game/Game.js
@@ -5,6 +5,20 @@ const cellSize = 20;
 const width = 800;
 const height = 600;
 
+class Cell extends Component {
+  render() {
+    const { x, y } = this.props;
+    return (
+      <div className='cell' style={{
+        left: `${cellSize * x + 1}px`,
+        top: `${cellSize * y + 1}px`,
+        width: `${cellSize - 1}px`,
+        height: `${cellSize - 1}px`,
+      }} />
+    )
+  }
+}
+
 class Game extends Component {
   constructor() {
     super();
@@ -22,7 +36,7 @@ class Game extends Component {
     for (let y = 0; y < this.rows; y++) {
       board[y] = [];
       for (let x = 0; x < this.cols; x++) {
-        board[y] [x] = false;
+        board[y][x] = false;
       }
     }
     return board;
@@ -32,7 +46,7 @@ class Game extends Component {
     let cells = [];
     for (let y = 0; y < this.rows; y++) {
       for (let x = 0; x < this.cols; x++) {
-        if (this.board[y] [x]) {
+        if (this.board[y][x]) {
           cells.push({ x, y });
         }
       }
@@ -40,10 +54,42 @@ class Game extends Component {
     return cells;
   }
 
+  getElementOffset() {
+    const rect = this.boardRef.getBoundingClientRect();
+    const doc = document.documentElement;
+
+    return {
+      x: (rect.left + window.pageXOffset) - doc.clientLeft,
+      y: (rect.top + window.pageYOffset) - doc.clientTop,
+    };
+  }
+
+  handleClick = event => {
+    const elemOffset = this.getElementOffset();
+    const offsetX = event.clientX - elemOffset.x;
+    const offsetY = event.clientY - elemOffset.y;
+
+    const x = Math.floor(offsetX / cellSize);
+    const y = Math.floor(offsetY / cellSize);
+
+    if (x >= 0 && x <= this.cols && y >= 0 && y <= this.rows) {
+      this.board[y][x] = !this.board[y][x];
+    }
+
+    this.setState({ cells: this.makeCells() });
+  }
+
   render() {
+    const { cells } = this.state;
     return (
       <div>
-        <div className='board' style={{ width: width, height: height, backgroundSize: `${cellSize}px ${cellSize}px` }}>
+        <div className='board'
+        style={{ width: width, height: height, backgroundSize: `${cellSize}px ${cellSize}px` }}
+        onClick={ this.handleClick }
+        ref={ (n) => { this.boardRef = n }}>
+        {cells.map(cell => (
+          <Cell x={cell.x} y={cell.y} key={`${cell.x}, ${cell.y}`} />
+        ))}
         </div>
       </div>
     )
