cdc1355749677017c62c1e1fa097e3699fea70de
emaquino44 <prexy44@yahoo.com>
2018-10-30 14:28:45 -0700

    Create Cell.js - cells are clickable

---
+++ project_gameoflife/src/Components/Cell.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../App.css';
+
+const CELL_SIZE = 20;
+
+
+
+class Cell extends React.Component {
+
+    render() {
+        const { x, y } = this.props;
+        return (
+            <div className="Cell" style={{
+                left: `${CELL_SIZE * x + 1}px`,
+                top: `${CELL_SIZE * y + 1}px`,
+                width: `${CELL_SIZE - 1}px`,
+                height: `${CELL_SIZE - 1}px`,
+            }} />
+        );
+    }
+}
+
+export default Cell;
--- project_gameoflife/src/Components/Grid.js
+++ project_gameoflife/src/Components/Grid.js
@@ -1,17 +1,47 @@
 import React from 'react';
+import Cell from './Cell';
 import '../App.css';
 
-const WIDTH = 800;
-const HEIGHT = 600;
+const WIDTH = 600;
+const HEIGHT = 400;
+const Square = 25;
 
 class Grid extends React.Component {
+
+constructor() {
+        super();
+        this.rows = HEIGHT / Square;
+        this.cols = WIDTH / Square;
+
+        this.board = this.newBoard();
+    }
+
+    state = {
+        cells: [],
+        isRunning: false,
+        interval: 100,
+    }
+
+    newBoard() {
+        let board = [];
+        for (let y = 0; y < this.rows; y++) {
+            board[y] = [];
+            for (let x = 0; x < this.cols; x++) {
+                board[y][x] = false;
+            }
+        }
+
+        return board;
+    }
 
     render() {
 
         return (
             <div>
                 <div className="grid"
-                    style={{ width: WIDTH, height: HEIGHT, }}>
+                    style={{ width: WIDTH, height: HEIGHT, backgroundSize: `${Square}px ${Square}px` }}
+                    onClick={this.handleClick}
+                    ref={(n) => { this.boardRef = n; }}>
                 </div>
                 <div className="controls">
 
