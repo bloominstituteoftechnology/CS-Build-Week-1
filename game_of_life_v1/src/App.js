4496589f3e47956ae9a5683f10f2eaebcc5cf29e
Adrian Adames <adrianadames@gmail.com>
2018-10-30 16:17:29 -0400

    Set up react app. Cleared away default app css styling.

---
+++ game_of_life_v1/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Game of Life - v1
+      </div>
+    );
+  }
+}
+
+export default App;
