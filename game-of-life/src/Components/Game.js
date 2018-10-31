d938cd6bb230ef465b834bf5127a1846d65cb9b1
sean chavez <seanchavez@seanchavezsMBP.home>
2018-10-31 13:36:03 -0700

    Implement addCells and clearBoard methods to Game

--- game-of-life/src/Components/Game.js
+++ game-of-life/src/Components/Game.js
@@ -6,6 +6,39 @@ const WIDTH = 800;
 const HEIGHT = 600;
 
 class Game extends React.Component {
+  constructor() {
+    super();
+    this.rows = HEIGHT / CELL_SIZE;
+    this.cols = WIDTH / CELL_SIZE;
+    this.board = this.clearBoard();
+  }
+  state = {
+    cells: [],
+  }
+
+  clearBoard = () => {
+    const board = [];
+    for (let y = 0; y < this.rows; y++) {
+      board[y] = [];
+      for (let x = 0; x < this.cols; x++) {
+        board[y][x] = false;
+      }
+    }
+    return board;
+  }
+
+  addCells = () => {
+    const cells = [];
+    for (let y = 0; y < this.rows; y++) {
+      for (let x = 0; x < this.cols; x++) {
+        if (this.board[x][y]) {
+          cells.push({ x, y });
+        }
+      }
+    }
+    return cells;  
+  }
+ 
   render() {
     return( 
       <div>
