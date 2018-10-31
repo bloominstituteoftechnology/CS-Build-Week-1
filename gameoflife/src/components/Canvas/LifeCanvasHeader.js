9c29eb1a885cba9af66599acd819b169ffee9776
“dirupt” <iamcooled@gmail.com>
2018-10-31 16:02:55 -0500

    added about / rules links

--- gameoflife/src/components/Canvas/LifeCanvasHeader.js
+++ gameoflife/src/components/Canvas/LifeCanvasHeader.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 const LifeCanvasHeader = () => {
     return (
-        <header className='canvas-header'></header>
+        <header className='canvas-header'>
+            <div className='canvas-header-links'>
+                <a className='canvas-header-rules' href="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life" target="_blank">Rules</a>
+                <a className='canvas-header-about' href="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life" target="_blank">About</a>
+            </div>
+        </header >
     );
 }
 
