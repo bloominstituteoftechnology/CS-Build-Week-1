5626a610eb42f334ceb5e19f93df830a879855a0
Jameson13B <jamesonbrown@neolivity.com>
2018-10-31 10:15:14 -0600

    Added rules and algorithm modals and connected to buttons

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -19,12 +19,18 @@ class App extends Component {
   handleClear = () => {this.setState({ clear: !this.state.clear, gens: 0, speed: 500 })};
   handleGen = () => {this.setState({ gens: this.state.gens+1 })}
   handleSpeed = (ms) => {this.setState({ speed: ms })}
+  handleRules = () => {document.getElementById('ruleModal').style.display = "block"};
+  handleAlgHist = () => {document.getElementById('algModal').style.display = "block"};
+  handleRuleClose = () => {document.getElementById('ruleModal').style.display = "none"};
+  handleAlgClose = () => {document.getElementById('algModal').style.display = "none"};
 
   render() {
     return (
       <div className="App">
         <Header
           handleSpeed={this.handleSpeed}
+          handleRules={this.handleRules}
+          handleAlgHist={this.handleAlgHist}
         />
         <Board
           clear={this.state.clear}
@@ -39,16 +45,25 @@ class App extends Component {
           toggleRun={this.toggleRun}
           handleClear={this.handleClear}
         />
-        <div id="ruleModal" class="modal">
-          <div class="modal-content">
-            <span class="close">&times;</span>
-            <p>Rules for the game...</p>
+        <div id="ruleModal" className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={this.handleRuleClose}>&times;</span>
+            <h1>Game of life Rules:</h1>
+            <ol>
+              <li>1. Any LIVE cell with fewer than two live neighbors dies, as if by underpopulation.</li>
+              <li>2. Any LIVE cell with two or three live neighbors lives on to the next generation.</li>
+              <li>3. Any LIVE cell with more than three live neighbors dies, as if by overpopulation.</li>
+              <li>4. Any DEAD cell with exactly three live neighbors becomes a live cell, as if by reproduction.</li>
+            </ol>
           </div>
         </div>
-        <div id="algModal" class="modal">
-          <div class="modal-content">
-            <span class="close">&times;</span>
-            <p>Algorithm info here...</p>
+        <div id="algModal" className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={this.handleAlgClose}>&times;</span>
+            <h1>Controls:</h1>
+            <p>Choose a figure from the templates below or make your own by click on a cell. The start/stop button 
+              advances the cells generation(based off an iteration of the rules). Game speed can be controlled from 
+              the buttons in the header.</p>
           </div>
         </div>
       </div>
