1618063df5e7b7345ba1278bd5f9eb6782162fed
Jameson13B <jamesonbrown@neolivity.com>
2018-10-30 20:16:52 -0600

    Added variable speed option

--- client/src/App.js
+++ client/src/App.js
@@ -9,21 +9,31 @@ class App extends Component {
     this.state = {
       running: false,
       clear: false,
-      gens: 0
+      gens: 0,
+      speed: 500
     }
   }
 
   toggleRun = () => {this.setState({ running: !this.state.running })};
   handleClear = () => {this.setState({ clear: !this.state.clear, gens: 0 })};
   handleGen = () => {this.setState({ gens: this.state.gens+1 })}
+  handleSpeed = (ms) => {this.setState({ speed: ms })}
 
   render() {
     return (
       <div className="App">
-        Main App
+        <div className='header'>
+          <h1 className='title'>Conway's Game of Life</h1>
+          <div className='icon' onClick={'Rules'}>Rules</div>
+          <div className='button' onClick={() => this.handleSpeed(250)}>250 ms</div>
+          <div className='button' onClick={() => this.handleSpeed(500)}>500 ms</div>
+          <div className='button' onClick={() => this.handleSpeed(1000)}>1000 ms</div>
+          <div className='icon' onClick={'Algorithm'}>Algorithm</div>
+        </div>
         <Board
           clear={this.state.clear}
           running={this.state.running}
+          speed={this.state.speed}
           handleClear={this.handleClear}
           handleGen={this.handleGen}
         />
