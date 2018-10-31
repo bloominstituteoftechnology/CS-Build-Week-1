e40059f62e1fdba2300130f7a04fdf4c17bc3542
snowcoding <vimalshah77@gmail.com>
2018-10-31 00:00:08 -0700

    Initial Commit of Patterns
    - Each pattern is just an array of (x,y) coordinate pairs

---
+++ cnwy-game-life/src/components/Patterns.js
@@ -0,0 +1,8 @@
+
+let patterns = {
+    Glider: [[1,0],[2,1],[0,2],[1,2],[2,2]],
+    Blinker: [[1,1], [2,1], [3,1]],
+    'R-pentomino': [[2,1], [3,1], [1,2], [2,2], [2,3]] 
+  }
+
+module.exports = patterns
