8659e44cd66a3f7d5db48fe373a40f1762bc2b22
sean chavez <seanchavez@seanchavezsMBP.home>
2018-10-31 11:16:25 -0700

    Create Board

---
+++ game-of-life/src/Components/Game.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Game.css'
+
+const CELL_SIZE = 20;
+const WIDTH = 800;
+const HEIGHT = 600;
+
+class Game extends React.Component {
+  render() {
+    return( 
+      <div>
+        <div className="Board" style={{ width: WIDTH, height: HEIGHT }}>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Game;
