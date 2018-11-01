32c98624ddeae7dbd1d8fdd840f5fce63b2320bd
JacobLayton <jlayjlay3@gmail.com>
2018-11-01 13:03:08 -0700

    Simulation is running, Logic appears to be correct, Comments added for clarification

--- gameoflife/src/App.js
+++ gameoflife/src/App.js
@@ -15,12 +15,14 @@ class App extends Component {
     }
   }
 
+  // Select Box Handler
   selectBox = (row, col) => {
     let gridCopy = arrayClone(this.state.gridFull);
     gridCopy[row][col] = !gridCopy[row][col];
     this.setState({gridFull: gridCopy});
   }
 
+  // Fills in random cells to the grid
   randomizer = () => {
     let gridCopy = arrayClone(this.state.gridFull);
     for (let i = 0; i < this.rows; i++) {
@@ -33,8 +35,58 @@ class App extends Component {
     this.setState({gridFull: gridCopy});
   }
 
+  // Starts the simulation
+  start = () => {
+    clearInterval(this.intervalId);
+    this.intervalId = setInterval(this.play, this.speed);
+  }
+
+
+  play = () => {
+    // Initialize grid and make a clone for changes made
+    let grid1 = this.state.gridFull;
+    let grid2 = arrayClone(this.state.gridFull);
+
+    // Nested for loop to run through each cell in the grid
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.cols; j++) {
+        // Initialize neighbors
+        let neighbors = 0;
+        // Check each neighboring box and increase count for each live cell
+        // Top
+        if (j < this.cols - 1) if (grid1[i][j + 1]) neighbors++;
+        // Right
+        if (i < this.rows - 1) if (grid1[i + 1][j]) neighbors++;
+        // Bottom
+        if (j > 0) if (grid1[i][j - 1]) neighbors++;
+        // Left
+        if (i > 0) if (grid1[i - 1][j]) neighbors++;
+        // Top-Left
+        if (i > 0 && j < this.cols - 1) if (grid1[i - 1][j + 1]) neighbors++;
+        // Top-Right
+        if (i < this.rows - 1 && j < this.cols - 1) if (grid1[i + 1][j + 1]) neighbors++;
+        // Bottom- Right
+        if (i < this.rows - 1 && j > 0) if (grid1[i + 1][j - 1]) neighbors++;
+        // Bottom-Left 
+        if (i > 0 && j > 0) if (grid1[i - 1][j - 1]) neighbors++;
+
+
+        // If less than 2 or more than 3 neighbors, kill cell
+        if (grid1[i][j] && (neighbors < 2 || neighbors > 3)) grid2[i][j] = false;
+        // If cell is dead and has 3 neighbors, cell goes live
+		    if (!grid1[i][j] && neighbors === 3) grid2[i][j] = true;
+      }
+    }
+    // Set state to grid2 and increment the generation counter
+    this.setState({
+		  gridFull: grid2,
+		  generation: this.state.generation + 1
+		});
+  }
+
   componentDidMount() {
     this.randomizer();
+    this.start();
   }
 
   render() {
