4cdf94a82a6d4ab1a3023780f218da0677316623
Vu Cao <cdhvu@yahoo.com>
2018-11-01 14:43:41 -0700

    Changed title

--- game/src/App.js
+++ game/src/App.js
@@ -6,8 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Game of Life</h1>
-        <h3>by Vu Cao</h3>
+        <h1>Conways's Game of Life</h1>
         <LifeCanvas />
       </div>
     );
