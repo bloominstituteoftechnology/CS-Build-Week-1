ff67afb037eb179e620124efcee5875296a3579a
Tyson Gushiken <tysonism@gmail.com>
2018-11-01 10:22:14 -0700

    Build for deploy to Netlify https://game.tysonism.com

---
+++ game/build/precache-manifest.dbcbdd577e586b91179a0560a7bb27bf.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6f637fc343eae0cb29fc5d54ed8a470",
+    "url": "/static/media/din2014-regular.e6f637fc.woff"
+  },
+  {
+    "revision": "04d29c2bfe7276d9a8bc94071b127652",
+    "url": "/static/media/din2014-light.04d29c2b.woff"
+  },
+  {
+    "revision": "c869fffd08f696b9b063c8bb31dc323d",
+    "url": "/static/media/din2014-demi.c869fffd.woff"
+  },
+  {
+    "revision": "7ed93392f382275d9729ee3ded942721",
+    "url": "/static/media/bg.7ed93392.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ba1f5e9a0f6e7c0a7eeb",
+    "url": "/static/js/main.ba1f5e9a.chunk.js"
+  },
+  {
+    "revision": "8de03db89959a0568f1f",
+    "url": "/static/js/1.8de03db8.chunk.js"
+  },
+  {
+    "revision": "ba1f5e9a0f6e7c0a7eeb",
+    "url": "/static/css/main.5e251d46.chunk.css"
+  },
+  {
+    "revision": "6e461cc5674c15639d744f87b7c4042b",
+    "url": "/index.html"
+  }
+];
