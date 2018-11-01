16302a0cb1f89a26bae3d4ba5d732e82b9bde1ee
Alec <afroenygam@gmail.com>
2018-11-01 00:48:45 -0400

    /build updated

---
+++ conways-life/build/precache-manifest.f787b780cebb1f9870beeefc0687389f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "556ff23a2ce6f9f136fafae7f7bd0d2a",
+    "url": "/static/media/gol-bkg.556ff23a.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "679cb031b87776d45a8a",
+    "url": "/static/js/main.679cb031.chunk.js"
+  },
+  {
+    "revision": "ee63573ca0ef041f0ada",
+    "url": "/static/js/1.ee63573c.chunk.js"
+  },
+  {
+    "revision": "679cb031b87776d45a8a",
+    "url": "/static/css/main.9dfec77a.chunk.css"
+  },
+  {
+    "revision": "35ee31c40d0ad58b3ddfe260924d0beb",
+    "url": "/index.html"
+  }
+];
