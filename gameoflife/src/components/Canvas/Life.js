739ba5e376f231e0cd3b8989c8a36cc63f932fec
“dirupt” <iamcooled@gmail.com>
2018-11-01 01:59:41 -0500

    implemented ability for cells to wrap around

--- gameoflife/src/components/Canvas/Life.js
+++ gameoflife/src/components/Canvas/Life.js
@@ -75,70 +75,51 @@ class Life {
         }
     }
 
+    findNeighbors(x, y) {
+        const index = (Math.ceil(this.cellHeight) * x) + y;
+        const cell = this.cells[this.buffer][index];
+        return cell && cell.alive ? 1 : 0;
+    }
+
     step() {
         let cells = this.cells[this.buffer];
         let cellsBuffer = this.cells[this.buffer === 0 ? 1 : 0];
         let cellWidth = Math.ceil(this.cellWidth);
         let cellHeight = Math.ceil(this.cellHeight);
 
-        for (let i = 0; i < cells.length; i++) {
-            let neighbors = 0;
-            let x = cells[i].coords[0];
-            let y = cells[i].coords[1];
+        for (let x = 0; x < cellWidth; x++) {
+            for (let y = 0; y < cellHeight; y++) {
+                let NORTH = (cellHeight + y - 1) % cellHeight;
+                let SOUTH = (cellHeight + y + 1) % cellHeight;
 
-            // North
-            if (y > 0 && cells[i - 1].alive) {
-                neighbors++;
-            }
-            // North-West
-            if (y > 0 && x > 0 && cells[i - cellHeight - 1].alive) {
-                neighbors++;
-            }
-            // North-East
-            if (y > 0 && x < cellWidth - 1 && cells[i + cellHeight - 1].alive) {
-                neighbors++;
-            }
-            // South
-            if (y < cellHeight - 1 && cells[i + 1].alive) {
-                neighbors++;
-            }
-            // South-West
-            if (y < cellHeight - 1 && x > 0 && cells[i + 1 - cellHeight].alive) {
-                neighbors++;
-            }
-            // South-East
-            if (y < cellHeight - 1 && x < cellWidth - 1 && cells[i + 1 + cellHeight].alive) {
-                neighbors++;
-            }
-            // West
-            if (x > 0 && cells[i - cellHeight].alive) {
-                neighbors++;
-            }
-            // East
-            if (x < cellWidth - 1 && cells[i + cellHeight].alive) {
-                neighbors++;
-            }
+                let EAST = (cellWidth + x + 1) % cellWidth;
+                let WEST = (cellWidth + x - 1) % cellWidth;
 
-            if (cells[i].alive) {
-                if (neighbors < 2) {
-                    cellsBuffer[i].alive = false;
-                } else if (neighbors === 2 || neighbors === 3) {
-                    cellsBuffer[i].alive = true;
+                let neighbors =
+                    this.findNeighbors(EAST, NORTH) + this.findNeighbors(WEST, NORTH) +
+                    this.findNeighbors(EAST, SOUTH) + this.findNeighbors(WEST, SOUTH) +
+                    this.findNeighbors(x, SOUTH) + this.findNeighbors(x, NORTH) +
+                    this.findNeighbors(EAST, y) + this.findNeighbors(WEST, y);
+
+                let i = (Math.ceil(this.cellHeight) * x) + y;
+
+                if (cells[i].alive) {
+                    if (neighbors < 2 || neighbors > 3) {
+                        cellsBuffer[i].alive = false;
+                    } else if (neighbors === 2 || neighbors === 3) {
+                        cellsBuffer[i].alive = true;
+                    }
                 } else {
-                    cellsBuffer[i].alive = false;
-                }
-            } else {
-                if (neighbors === 3) {
-                    cellsBuffer[i].alive = true;
-                } else {
-                    cellsBuffer[i].alive = false;
+                    if (neighbors === 3) {
+                        cellsBuffer[i].alive = true;
+                    } else {
+                        cellsBuffer[i].alive = false;
+                    }
                 }
             }
         }
         this.buffer = this.buffer === 0 ? 1 : 0;
     }
-
-
 }
 
 export default Life;
