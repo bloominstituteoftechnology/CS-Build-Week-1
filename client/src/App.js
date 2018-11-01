225c796ee10a12be4ff27852311414234ed8451a
Kevin Tena <kevten09@gmail.com>
2018-11-01 15:38:06 -0500

    Added rules and info sections

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,20 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className="rules-container">
+            <h1> Rules </h1>
+            <a> If a cell is alive and it has exactly 2 or 3 live neighbors, it stays alive.<br></br>
+              
+                If a cell is alive and it has less than 2 or 4+ live neighbors, it dies. <br></br>
+                
+                If a cell is dead and it has exactly 3 live neighbors, it comes to life. </a>
+          </div>
         <CellGrid/>
+        <div className="info-container">
+            <h1> Conway's Game of Life </h1>
+            <a href="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life"> Link to wiki</a>
+            <p> </p>
+          </div>
       </div>
     );
   }
