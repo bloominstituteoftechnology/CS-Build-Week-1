2ce963e7e2b17da7f84b895f961e262be682aeb2
Katie Gorbell <katiegorbell@gmail.com>
2018-11-01 15:52:51 -0500

    buttons added for presets

--- conway/src/App.js
+++ conway/src/App.js
@@ -17,33 +17,67 @@ const Content = styled.div`
   width: 1000px;
 `;
 
-const Rules = styled.div`
-  width: 360px;
-  font-weight: bold;
+const PresetsContainer = styled.div`
+  width: 300px;
+  border: 1px solid red;
 `;
 
-const Bullet = styled.ul`
+const Rules = styled.div`
+  width: 300px;
+  font-weight: bold;
+  border: 1px solid red;
+`;
+
+const Bullet = styled.li`
   margin-bottom: 10px;
+  text-align: left;
 `;
 
 const AboutSection = styled.div`
   width: 100%;
 `;
 
+const Button = styled.button`
+    height: 25px;
+    font-size: 20px;
+    border-radius: 15px;
+    border: 1px solid black
+`;
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      preset: null
+    }
+  }
+
+  initPreset = e => {
+    this.setState({preset: e.target.value});
+  }
+
   render() {
+
+
     return (
       <div className="App">
         <Content>
         <Title>Conway's Game of Life</Title>
           <Game/>
+          <PresetsContainer>
+            <Button onClick={this.initPreset} value='1'>Preset 1</Button>
+            <Button onClick={this.initPreset} value='2'>Preset 2</Button>
+            <Button onClick={this.initPreset} value='3'>Preset 3</Button>
+          </PresetsContainer>
           <Rules className="Rules">
             <h2>Rules:</h2>
-            <Bullet>1. Any live cell with fewer than two live neighbors dies, as if by underpopulation.</Bullet>
-            <Bullet>2. Any live cell with two or three live neighbors livess on to the next generation.</Bullet>
-            <Bullet>3. Any live cell with more than 3 neighbors dies as if by overpopulation.</Bullet>
-            <Bullet>4. Any dead cell with exactly threee live neighbors becomes a live cell, as if by reproduction.</Bullet>
-            <Bullet>5. Cells off the grid are either assumed to be dead, or wrap around to neighbor the next cell on the row below.</Bullet>
+            <ul>
+              <Bullet>Any live cell with fewer than two live neighbors dies, as if by underpopulation.</Bullet>
+              <Bullet>Any live cell with two or three live neighbors livess on to the next generation.</Bullet>
+              <Bullet>Any live cell with more than 3 neighbors dies as if by overpopulation.</Bullet>
+              <Bullet>Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.</Bullet>
+              <Bullet>Cells off the grid are either assumed to be dead, or wrap around to neighbor the next cell on the row below.</Bullet>
+            </ul>
           </Rules>
           <AboutSection>
             <h2>About this Algorithm:</h2>
