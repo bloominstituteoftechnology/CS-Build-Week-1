3e68f8c5ee7b1234cb0c7e9e2d1614ccfd10d618
Mark Hong <m4rkh0ng@gmail.com>
2018-11-01 20:17:17 -0400

    add rules and history components to app

--- game-of-life/src/App.js
+++ game-of-life/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Board from './components/board';
 import Control from './components/control';
 import Counter from './components/counter';
+import Rules from './components/rules';
+import History from './components/history';
 import Learn from './components/learn';
 import './App.css';
 
@@ -23,19 +25,27 @@ class App extends Component {
   render() {
     return (
       <div>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css"></link>
-        <h1>Game of Life</h1>
         <div>
-          <Learn/>
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css"></link>
+          <h1>Game of Life</h1>
+          <div>
+            <Learn/>
+          </div>
+          <Board 
+            running={this.state.running}
+          />
+          <Counter />
+          <Control 
+            running={this.state.running}
+            isRunning={this.isRunning}
+          />
         </div>
-        <Board 
-          running={this.state.running}
-        />
-        <Counter />
-        <Control 
-          running={this.state.running}
-          isRunning={this.isRunning}
-        />
+        <div className="whitespace"/>
+        <div className="info">
+          <Rules />
+          <History />
+        </div>
+        <div className="footer-whitespace"/>
       </div>
     );
   }
--- game-of-life/src/components/cell.js
+++ game-of-life/src/components/cell.js
@@ -6,6 +6,6 @@ export default ({alive, newBorn, running, handleClick}) =>
         onClick={running ? '' : handleClick}
         className={`${alive ? 'alive' : ''} ${newBorn ? 'new-born': ''}`}
       // TODO: Write ternary or other conditional statement to prevent cell from being clicked during the playing of the game.
-      >
+    >
     </td>
   )
