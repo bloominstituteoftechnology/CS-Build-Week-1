4f89f6f52376377963bec9c4420aed94f50214aa
“Kennedykid1995” <34872885+Kennedykid1995@users.noreply.github.com>
2018-12-10 11:45:01 -0600

    Organizing
    Placing all of the different containers, buttons, and inputs. organizing them so that basic styling is finished before the logic.

--- game_of_life/src/App.js
+++ game_of_life/src/App.js
@@ -1,26 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components'; 
 
+const App_Holder = styled.div`
+border: 1px solid black; 
+display: flex; 
+flex-direction: column; 
+flex:1;
+`
+const Header_container = styled.header`
+border: 1px solid red; 
+display: flex; 
+flex-direction: column; 
+flex: 1; 
+`
+const Btn = styled.button`
+width: 100px; 
+padding: 10px; 
+margin: 5px; 
+`
+const Inner_Header = styled.header`
+border: 1px solid green; 
+display: flex; 
+width: 200px; 
+height: 100xp;
+`
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <App_Holder>
+        <Header_container>
+          <Btn>
+            Submit
+            </Btn>
+            <Inner_Header>
+            </Inner_Header>
+        </Header_container>
+      </App_Holder>
     );
   }
 }
