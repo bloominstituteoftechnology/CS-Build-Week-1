8eef9f4b0e910146483ff649ed0a7895c1e30698
david-pok <david.pok@gmail.com>
2018-12-10 14:14:20 -0800

    attempting to setup canvas

--- gameoflife/src/App.js
+++ gameoflife/src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import LifeCanvas from "./components/LifeCanvas";
 
 class App extends Component {
   render() {
@@ -8,17 +9,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <LifeCanvas />
         </header>
       </div>
     );
