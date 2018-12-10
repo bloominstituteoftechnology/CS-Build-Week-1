83fdc4e1b3f11ac8303ff86dd8b9d495f0abb538
William <kwontaewoongah@gmail.com>
2018-12-10 14:43:58 -0800

    made use of array data structure for the grid made an init function to initialize values in grid to 0

--- game-of-life/src/gameComponents/grid.js
+++ game-of-life/src/gameComponents/grid.js
@@ -1,1 +1,27 @@
 import React, { Component } from 'react';
+
+class Grid extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            grid:[]
+        }
+    }
+    componentDidMount() {
+        this.init();
+    }
+    init=()=>{
+        const grid=[];
+        for(let i=0; i<15; i++) {
+            grid.push([]);
+            for (let j=0; j<15; j++) {
+                grid[i].push(0);
+            }
+        }
+        this.setState({grid:grid});
+    } 
+    render(){
+        
+    }
+}
+export default Grid;
