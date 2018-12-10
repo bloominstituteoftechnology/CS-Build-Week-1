62e71cb7da348681e150bee991944ea17c7b6614
adamcglee <adamcglee@gmail.com>
2018-12-10 14:51:20 -0800

    added base code and logic for the grid_init and new_grid functions

---
+++ game/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+import Grid from "./components/grid";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      game_running: false
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Grid/>
+      </div>
+    );
+  }
+}
+
+export default App;
