6eef269a918d14194234a9a2ea2c6bcbc6a264ca
Rachel DiCesare <racheldicesare@missrachelthedeveloper.com>
2018-12-10 12:48:12 -0500

    created grid

---
+++ gameoflife/src/Game.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Game.css';
+
+const CELL_SIZE = 20;
+const WIDTH = 800;
+const HEIGHT = 600;
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div>
+                <div className="Board"
+                style={{width: WIDTH, height: HEIGHT,backgroundSize: `${CELL_SIZE}px ${CELL_SIZE}px`}}>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Game;
