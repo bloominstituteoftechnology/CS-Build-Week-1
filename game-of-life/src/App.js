f3c8ad7cd8b99cdbc37f71c8281af071333cf65f
Luis Fernando Salazar <lfsalazarcruz@gmail.com>
2018-12-10 13:24:46 -0800

    Added title, generation (and generation state), and grid component.
    Currently working on displaying the grid.

--- game-of-life/src/App.js
+++ game-of-life/src/App.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import Grid from './components/Grid/Grid';
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      generation: 0,
+    }
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Game of Life</h1>
+        <Grid/>
+        <h1>Generation: {this.state.generation} </h1>
       </div>
     );
   }
---
+++ game-of-life/src/components/Grid/Grid.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Grid extends Component {
+	render() {
+		return (
+			<div>
+				Grid
+			</div>
+		);
+	}
+}
+
+export default Grid;
