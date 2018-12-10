849b9053025b83291dcaea2ed822403585ac4141
Fred <sohnf90@yahoo.com>
2018-12-10 14:54:03 -0800

    Added rules component

--- conways-life/src/App.js
+++ conways-life/src/App.js
@@ -1,25 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import Grid from './components/Grid';
+import Rules from './components/Rules'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>Conway's Game of Life</h1>
+        {/* <Grid /> */}
+        <Rules />
+        {/* <History /> */}
       </div>
     );
   }
---
+++ conways-life/src/components/Rules.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rules = () => {
+    return(
+        <div>
+            <h2>Survival:</h2>
+            <p>If a cell is alive and has 2 or 3 living neighors, it survives and moves on to the next generation.</p>
+            <h2>Birth:</h2>
+            <p>If a cell is dead and has exactly 3 living neighbors, it comes to life in the next generation.</p>
+            <h2>Death:</h2>
+            <p>If a cell is alive and has less than 2 living neighbors or more than 3 living neighbors, it dies and is shown as a dead cell in the next generation.</p>
+        </div>
+    );
+}
+
+export default Rules;
