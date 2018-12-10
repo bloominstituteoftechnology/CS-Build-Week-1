ffb51809b8d466e78023ed97bebab6e75f616751
LizBaker <lizbaker.pdx@gmail.com>
2018-12-10 15:35:11 -0800

    Added run/stop/clear buttons -run and stop toggle a running boolean on state -when running is true, cells cannot be clicked -clear resets all cells and clears the canvas

--- game/src/App.js
+++ game/src/App.js
@@ -5,21 +5,24 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
+      running: false
     }
   }
   componentDidMount() {
-    const gridStateInit = {}
-    for(let i=0; i<=300; i+=10){
-      for(let j=0; j<=300; j+=10){
+    this.gridStateInit()
+    this.drawGrid()
+  }
+  gridStateInit = () => {
+    for (let i=0; i<=300; i+=20){
+      for (let j=0; j<=300; j+=20){
         this.setState({[`${i}_${j}`]: "dead"})
       }
     }
-    this.drawGrid()
   }
   drawGrid = () => {
     const c = this.refs.grid
     const ctx = c.getContext("2d");
-    for(let i=0; i<= 300; i+=10) {
+    for(let i=0; i<= 300; i+=20) {
       ctx.beginPath()
       //vertical
       ctx.moveTo((i+.5), 0);
@@ -30,19 +33,39 @@ class App extends Component {
       ctx.stroke();
    }
   }
-  handleClick = (e) => {
-    const c = this.refs.grid
-    const ctx = c.getContext("2d");
-    const boxSize = 10
-    //offset squares to make up for where the grid is on the page
-    //minus 1 px for border
-    const x = Math.floor(e.clientX / boxSize) * boxSize - 49
-    const y = Math.floor(e.clientY / boxSize) * boxSize - 79
-    ctx.fillStyle = this.state[`${x}_${y}`] === "alive" ? "#FFFFFF" : "#DB7093";
-    //minus 1 px for border
-    ctx.fillRect(x, y, 9, 9);
-    const cellStatus = this.state[`${x}_${y}`] === "alive" ? "dead" : "alive"
-    this.setState({[`${x}_${y}`]: cellStatus});
+  buttonClick = (type) => {
+    switch(type){
+      case "run":
+        this.setState({running: true})
+        break;
+      case "stop":
+        this.setState({running: false})
+        break;
+      case "clear":
+        const c = this.refs.grid
+        const ctx = c.getContext("2d");
+        ctx.clearRect(0,0,c.width,c.height)
+        this.drawGrid()
+        this.gridStateInit()
+        break;
+    }
+  }
+  cellClick = (e) => {
+    if (this.state.running === false){
+      const c = this.refs.grid
+      const ctx = c.getContext("2d");
+      const boxSize = 20
+      //offset squares to make up for where the grid is on the page
+      const x = Math.floor(e.clientX / boxSize) * boxSize -39
+      const y = Math.floor(e.clientY / boxSize) * boxSize -79
+      ctx.fillStyle = this.state[`${x-1}_${y-1}`] === "alive" ? "#FFFFFF" : "#DB7093";
+      //minus 1 px for border
+      ctx.fillRect(x, y, 19, 19);
+      const cellStatus = this.state[`${x-1}_${y-1}`] === "alive" ? "dead" : "alive"
+      this.setState({[`${x-1}_${y-1}`]: cellStatus});
+    } else {
+      return null;
+    }
   }
 
   render() {
@@ -50,11 +73,11 @@ class App extends Component {
       <div className="App">
         <h1 className="title">Conway's Game of Life</h1>
         <div className="board">
-          <canvas onClick={this.handleClick} ref="grid" width="301" height="301"/>
+          <canvas onClick={this.cellClick} ref="grid" width="301" height="301"/>
           <div className="presets">
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
+            <button onClick={()=>this.buttonClick("run")}>Run</button>
+            <button onClick={()=>this.buttonClick("stop")}>Stop</button>
+            <button onClick={()=>this.buttonClick("clear")}>Clear</button>
           </div>
           <div>
             <h2>Rules</h2>
