b65ed6b02cb49c8fdf5ec47c015a92dc347dc4ea
vanceleon <vanceleon44@gmail.com>
2018-12-10 17:42:26 -0500

    CSS styling

--- gameoflife/src/components/details.js
+++ gameoflife/src/components/details.js
@@ -3,24 +3,24 @@ import React from "react";
 
 const Details = () => {
   return (
-    <React.Fragment>
+    <div className="details-container">
       <div className="rules-container">
         <h1>Game of Life's Four Rules</h1>
         <ol>
           <li>
-            Any live cell with fewer than two live neighbors dies, as if by
+            1. Any live cell with fewer than two live neighbors dies, as if by
             underpopulation
           </li>
           <li>
-            Any live cell with two or three live neighbors lives on to the
+            2. Any live cell with two or three live neighbors lives on to the
             next generation.{" "}
           </li>
           <li>
-            Any live cell with more than three live neighbors dies, as if by
+            3. Any live cell with more than three live neighbors dies, as if by
             overpopulation{" "}
           </li>
           <li>
-            Any dead cell with exactly three live neighbors becomes a live
+            4. Any dead cell with exactly three live neighbors becomes a live
             cell, as if by reproduction.
           </li>
         </ol>
@@ -35,7 +35,7 @@ const Details = () => {
           properties can be created.
         </div>
       </div>
-    </React.Fragment>
+    </div>
   );
 };
 
