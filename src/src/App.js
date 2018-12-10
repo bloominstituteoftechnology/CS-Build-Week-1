deec36be3ac469724019b42b2d3a84c5025da369
Noah Amar <ne.amar@outlook.com>
2018-12-10 15:27:37 -0600

    react set-up

---
+++ src/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Grid from './Grid'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       <Grid />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/src/Grid.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Grid extends Component {
+    state = { 
+        //state for toggling here(alive/dead), ternary operator?
+     }
+    render() { 
+        return ( 
+            <div>
+            Grid
+            // div squares or canvas here
+            </div>
+         );
+    }
+}
+ 
+export default Grid;
