04fd6ef30fd65c1e3489d901599ff6689e140308
Grant Reighard <grant.reighard@gmail.com>
2018-12-10 17:24:22 -0500

    Working with Ronald to figure out some issues.

--- game/src/components/Game.js
+++ game/src/components/Game.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import {matrix1} from './matrix1';
+import {matrix2} from './matrix2';
 
 class Game extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            array1: matrix2
         }
     }
 
     componentDidMount = () => {
         this.drawBoard();
+    }
+
+    componentDidUpdate = () => {
+        this.toggleSquares();
     }
 
     drawBoard = () => {
@@ -33,20 +37,64 @@ class Game extends React.Component {
         canvas.addEventListener('click', function(event) {
             // context.fillStyle = "yellow";
             // context.fillRect(event.pageX - (event.pageX % 10) - 9, event.pageY - (event.pageY % 10) - 79, 9, 9);
-            matrix1[event.pageX % 10] = matrix1[event.pageX % 10] === 1 ? 0 : 1;
-        })
+            let yCoord = Number((Math.ceil(event.pageY / 10))) - 1;
+            let xCoord = Number((Math.ceil(event.pageX / 10))) - 1;
+            console.log("xCoord: ", xCoord);
+            console.log("pageX:", event.pageX);
+            console.log("yCoord: ", yCoord);
+            console.log("pageY:", event.pageY);
+            matrix2[yCoord][xCoord] = matrix2[yCoord][xCoord] === 1 ? 0 : 1;
+            this.setState({array1: matrix2});
+        }.bind(this));
 
-        for (let i = 0; i < matrix1.length; i++) {
-            if (matrix1[i]) {
-                context.fillStyle = "yellow";
-                context.fillRect(i * 10 + 1, 1, 9, 9);
-            } else {
-                context.fillStyle = "dodgerblue";
-                context.fillRect(i * 10 + 1, 1, 9, 9);
+        // for (let i = 0; i < this.state.array1.length; i++) {
+        //     if (this.state.array1[i]) {
+        //         context.fillStyle = "yellow";
+        //         context.fillRect(i * 10 + 1, 1, 9, 9);
+        //     } else {
+        //         context.fillStyle = "dodgerblue";
+        //         context.fillRect(i * 10 + 1, 1, 9, 9);
+        //     }
+        // }
+
+        for (let i = 0; i < 80; i++) {
+            for (let j = 0; j < 190; j++) {
+                if (this.state.array1[i][j]) {
+                    context.fillStyle = "yellow";
+                    context.fillRect(i * 10 + 1, j * 10 + 1, 9, 9);
+                } else {
+                    context.fillStyle = "dodgerblue";
+                    context.fillRect(i * 10 + 1, j * 10 + 1, 9, 9);
+                }
             }
         }
     }
 
+    toggleSquares = () => {
+        const canvas = this.refs.canvas;
+        const context = canvas.getContext('2d');
+        // for (let i = 0; i < this.state.array1.length; i++) {
+        //     if (this.state.array1[i]) {
+        //         context.fillStyle = "yellow";
+        //         context.fillRect(i * 10 + 1, 1, 9, 9);
+        //     } else {
+        //         context.fillStyle = "dodgerblue";
+        //         context.fillRect(i * 10 + 1, 1, 9, 9);
+        //     }
+        // }
+        for (let i = 0; i < 80; i++) {
+            for (let j = 0; j < 190; j++) {
+                if (this.state.array1[i][j]) {
+                    context.fillStyle = "yellow";
+                    context.fillRect(i * 10 + 1, j * 10 + 1, 9, 9);
+                } else {
+                    context.fillStyle = "dodgerblue";
+                    context.fillRect(i * 10 + 1, j * 10 + 1, 9, 9);
+                }
+            }
+        }
+    }
+ 
     render() {
         return (
             <div>
