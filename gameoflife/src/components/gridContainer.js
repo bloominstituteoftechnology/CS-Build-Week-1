399d5b7c7526bd57d7e8a8fb56878ffdf90d2d6e
vanceleon <vanceleon44@gmail.com>
2018-12-11 16:27:48 -0500

    hard coded grid

--- gameoflife/src/components/gridContainer.js
+++ gameoflife/src/components/gridContainer.js
@@ -6,10 +6,30 @@ class GridContainer extends Component {
     this.state = {
       alive: { isToggleOn: true },
       square: false,
-      grid: [...Array(15)].map(e => Array(15).fill(0))
+      grid:[ 
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        ],
+      
+      // [...Array(15)].map(e => Array(15).fill(0))
     };
   }
-  //  [ This is a visual of the data structure above
+  // This is a visual of the data structure above
+  //  [ 
   // [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
   // [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
   // [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
@@ -27,15 +47,17 @@ class GridContainer extends Component {
   // [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
   // [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
   // ]
-
-  componentDidMount() {
+  
+  
+  
+  componentDidMount(){
     const w = 450;
     const h = 450;
     const canvas = this.refs.canvas;
     const ctx = canvas.getContext("2d");
     ctx.canvas.width = w;
     ctx.canvas.height = h;
-
+    
     for (let j = 0; j <= w; j += 15) {
       for (let k = 0; k <= h; k += 15) {
         ctx.moveTo(j, 0);
@@ -46,18 +68,18 @@ class GridContainer extends Component {
         ctx.stroke();
       }
     }
-  for (let y = 0; y < this.grid[y].length; y++) {
-    console.log(`This is each array: ${y}`);
-    for (let x = 0; x < this.grid[y][x].length; x++) {
-      console.log(`This is the x value in the array: ${x}`);
+    
+    for (let y = 0; y < this.state.grid[y].length; y++) {
+      console.log(`This is the y value in the array: ${y}`)
+      for (let x = 0; x < this.state.grid[y][x].length; x++) {
+        console.log(`This is the x value in the array: ${x}`);
+      }
     }
+    // for(y)iterate over each array and see what is alive
+    // for(x) iterate over each element
   }
-
-  // for(y)iterate over each array and see what is alive
-  // for(x) iterate over each element
-}
-
-
+  
+  
   handleClick(event) {
     event.preventDefault();
     this.setState({
