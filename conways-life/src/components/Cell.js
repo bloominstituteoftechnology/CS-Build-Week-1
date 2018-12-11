db7e230081abdfcbccdfd1ad9d59fb26ba72842b
sca4cs <shannon.atkinson.ca@gmail.com>
2018-12-11 15:59:34 -0500

    working on algorithm

--- conways-life/src/components/Cell.js
+++ conways-life/src/components/Cell.js
@@ -5,24 +5,29 @@ class Cell extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            value: this.props.cell,
+            value: this.props.value,
+            x: this.props.x,
+            y: this.props.y
         }
     }
 
     componentWillReceiveProps(nextProps) {
-        this.setState({ value: nextProps.cell });
+        this.setState({ value: nextProps.value });
       }
+      
 
-    handleToggle() {
+    handleToggle = () => {
         if (this.state.value === 1) {
-            this.setState({value: 0});
+            this.setState({value: 0}); 
+            this.props.updateGrid(0, this.state.x, this.state.y);
         } else {
             this.setState({value: 1});
+            this.props.updateGrid(1, this.state.x, this.state.y); 
         }
     }
 
     render() {
-        return (
+            return (
             <div className="cell"
                 onClick={() => this.handleToggle()}
                 style={ this.state.value === 1 ? { background: "blue" } : null }>
--- conways-life/src/components/GridDisplay.js
+++ conways-life/src/components/GridDisplay.js
@@ -23,22 +23,19 @@ class GridDisplay extends Component {
         }
         for (let i = 0; i < this.state.cols; i++) {
             for (let j = 0; j < this.state.rows; j++) {
-                arr[i][j] = 0;
+                arr[i][j] = { value: 0, x: i, y: j };
             }
         } 
         this.setState({ grid: arr });
     }
 
-    seeGrid = () => {
-        console.log(this.state.grid);
-    }
 
     randomGrid = () => {
         let arr = this.state.grid.slice();
 
         for (let i = 0; i < this.state.cols; i++) {
             for (let j = 0; j < this.state.rows; j++) {
-                arr[i][j] = Math.round(Math.random()); //random 0 or 1
+                arr[i][j].value = Math.round(Math.random()); //random 0 or 1
             }
         } 
 
@@ -50,26 +47,78 @@ class GridDisplay extends Component {
 
         for (let i = 0; i < this.state.cols; i++) {
             for (let j = 0; j < this.state.rows; j++) {
-                arr[i][j] = 0;
+                arr[i][j].value = 0;
             }
         } 
 
         this.setState({ grid: arr });
     }
 
+    computeNextGrid = () => {
+        const prevGrid = this.state.grid.slice();
+        let nextGrid = prevGrid.slice();
+        
+        for (let i = 0; i < this.state.cols; i++) {
+            for (let j = 0; j < this.state.rows; j++) {
+              let cellState = prevGrid[i][j].value;
+              // Count live neighbors
+              let neighbors = this.countNeighbors(prevGrid, i, j);
+        
+              if (cellState === 0 && neighbors === 3) {
+                nextGrid[i][j].value = 1;
+              } else if (cellState === 1 && (neighbors < 2 || neighbors >= 4)) {
+                nextGrid[i][j].value = 0;
+              } else if (cellState === 1 && (neighbors === 3 || neighbors === 2)){
+                nextGrid[i][j].value = 1;
+              } else {
+                nextGrid[i][j].value = cellState;
+              }
+        
+            }
+          }
+          this.setState({ grid: nextGrid });
+    }
+
+    countNeighbors = (grid, x, y) => {
+        let sum = 0;
+        for (let i = -1; i < 2; i++) {
+            for (let j = -1; j < 2; j++) {
+            // edges wrap around
+            let col = (x + i + this.state.cols) % this.state.cols;
+            let row = (y + j + this.state.rows) % this.state.rows;
+            sum += grid[col][row].value;
+            }
+        }
+        sum -= grid[x][y].value;
+        return sum;
+    }
+
+    updateGrid = (value, x, y) => {
+        let arr = this.state.grid.slice();
+  
+        arr[x][y].value = value;
+
+        this.setState({ grid: arr });
+    }
+
     render() {
-        return (
+            return (
             <div className="mainContainer">
                  <div className="gridContainer">
                     {this.state.grid.map(row => 
                         row.map((cell, index) => 
-                        <Cell key={index} cell={cell}/>
+                        <Cell
+                            key={index}
+                            value={cell.value}
+                            updateGrid={this.updateGrid}
+                            x={cell.x}
+                            y={cell.y}/>
                     ))}
                 </div>
                 <Buttons
                     randomGrid={this.randomGrid}
                     clearGrid={this.clearGrid}
-                    seeGrid={this.seeGrid}/>
+                    computeNextGrid={this.computeNextGrid}/>
             </div>
           );
         }
