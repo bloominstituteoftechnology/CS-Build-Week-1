5ea56eae4a0d0486f3f95cff1ac6a2fc6104d8c5
adamcglee <adamcglee@gmail.com>
2018-12-10 16:24:47 -0800

    added more code for the grid logic and trying to get the visuals up and running

--- game/src/App.js
+++ game/src/App.js
@@ -10,10 +10,28 @@ class App extends Component {
     };
   }
 
+  start_game() {
+    if(!this.state.game_running) {
+      this.setState({
+        game_running: true,
+      });
+    }
+  }
+
+  stop_game() {
+    this.setState({
+      game_running: false,
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <Grid/>
+        <div className="headerButtons">
+            <button className="submit" onClick={this.startGame}>Start</button>
+            <button className="submit" onClick={this.stopGame}>Stop</button>
+          </div>
       </div>
     );
   }
--- game/src/components/grid.js
+++ game/src/components/grid.js
@@ -1,71 +1,108 @@
 import React, { Component } from "react";
+import "./grid.css";
 
 class Grid extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-            cell_size: 20,
-            grid_width: 15,
-            grid_height: 15,
-            grid: []
+  constructor(props) {
+    super(props);
+    this.state = {
+      cell_size: 20,
+      grid_width: 15,
+      grid_height: 15,
+      grid: []
+    };
+  }
+
+  componentDidMount() {
+    this.grid_init(this.state.grid_width, this.state.grid_height);
+  }
+
+  //initialize the grid with the proper height and width and all false values.
+  grid_init(width, height) {
+    const grid = [];
+    for (let y = 0; y < height; y++) {
+      grid[y] = [];
+      for (let x = 0; x < width; x++) {
+        grid[y][x] = false;
+      }
+    }
+    this.setState({
+      grid: grid
+    });
+  }
+
+  //create a new grid based on the game rules being applied to the current grid, then setting the new grid to the current grid in state.
+  new_grid() {
+    const grid = this.state.grid;
+    const neighbors = [
+      [-1, -1],
+      [-1, 0],
+      [-1, 1],
+      [0, 1],
+      [1, 1],
+      [1, 0],
+      [1, -1],
+      [0, -1]
+    ];
+    let counter = 0;
+    for (let y = 0; y < this.state.grid_height; y++) {
+      for (let x = 0; x < this.state.grid_widt; x++) {
+        for (let i = 0; i < neighbors.length; i++) {
+          const neighbor = neighbors[i];
+          let y1 = y + neighbor[0];
+          let x1 = x + neighbor[1];
+          if (
+            x1 >= 0 &&
+            this.state.grid_width &&
+            y1 >= 0 &&
+            y1 < this.state.grid_height &&
+            this.state.grid[y1][x1]
+          ) {
+            counter++;
+          }
         }
-    }
-
-    componentDidMount() {
-        this.grid_init(this.state.grid_width, this.state.grid_height);
-    }
-
-    //initialize the grid with the proper height and width and all false values.
-    grid_init(width, height) {
-        const grid = [];
-        for(let y = 0; y < height; y++) {
-            grid[y] = [];
-            for(let x = 0; x < width; x++) {
-                grid[y][x] = false;
-            }
-         }
-        this.setState({ 
-            grid: grid
-        });
-    }
-
-    //create a new grid based on the game rules being applied to the current grid, then setting the new grid to the current grid in state.
-    new_grid() {
-        const grid = this.state.grid;
-        const neighbors = [[-1, -1], [-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1]];
-        let counter = 0;
-        for(let y = 0; y < this.state.grid_height; y++) {
-            for(let x = 0; x < this.state.grid_widt; x++) {
-                for(let i = 0; i < neighbors.length; i++) {
-                    const neighbor = neighbors[i];
-                    let y1 = y + neighbor[0];
-                    let x1 = x + neighbor[1];
-                    if(x1 >= 0 && this.state.grid_width && y1 >= 0 && y1 < this.state.grid_height && this.state.grid[y1][x1]) {
-                        counter++;
-                    }
-                }
-                if(this.state.grid[y][x]) {
-                    if(counter === 2 || counter === 2) {
-                        grid[y][x] = true;
-                    }else {
-                        grid[y][x] = true;
-                    }
-                }else {
-                    if(!this.state.grid[y][x] && counter ===3) {
-                        grid[y][x] = true;
-                    }
-                }
-            }
+        if (this.state.grid[y][x]) {
+          if (counter === 2 || counter === 2) {
+            grid[y][x] = true;
+          } else {
+            grid[y][x] = true;
+          }
+        } else {
+          if (!this.state.grid[y][x] && counter === 3) {
+            grid[y][x] = true;
+          }
         }
+      }
     }
+  }
 
+  render_grid() {
+    const newGrid = [];
+    let cellRow = [];
+    for (let y = 0; y < this.state.grid_height; y++) {
+      for (let x = 0; x < this.state.grid_widt; x++) {
+        cellRow.push(<Cell key={[y, x]} />);
+      }
+      newGrid.push(
+        <div className="row" key={y}>
+          {cellRow}
+        </div>
+      );
+      cellRow = [];
+    }
+    return newGrid;
+  }
+
+  render() {
+    return <div className="gridContainer">{this.render_grid()}</div>;
+  }
+}
+
+export default Grid;
+
+class Cell extends Component {
     render() { 
         return ( 
-            <div className="Grid" 
-                style={{width: (this.state.cell_size * this.state.grid_width), height:(this.state.cell_size * this.state.grid_height)}}>
-            </div>
-         );
+            <div className="cellContainer"></div>
+        );
     }
 }
- 
-export default Grid;
