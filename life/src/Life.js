df0c6521a33c9755f39f52451de335b031c1f7ad
Munsu <dark_specineff2002@yahoo.com>
2018-12-11 09:36:15 -0800

    methods to generate board and cells

--- life/src/Life.js
+++ life/src/Life.js
@@ -6,6 +6,39 @@ const WIDTH = 800;
 const HEIGHT = 600;
 
 class Life extends React.Component {
+  constructor() {
+    super();
+    this.rows = HEIGHT / CELL_SIZE;
+    this.cols = WIDTH / CELL_SIZE;
+    this.board = this.generateEmptyBoard();
+  }
+  state = {
+    cells: []
+  };
+
+  generateEmptyBoard() {
+    let board = [];
+    for (let y = 0; y < this.rows; y++) {
+      board[y] = [];
+      for (let x = 0; x < this.cols; x++) {
+        board[y][x] = false;
+      }
+    }
+    return board;
+  }
+
+  generateCells() {
+    let cells = [];
+    for (let y = 0; y < this.rows; y++) {
+      for (let x = 0; x < this.cols; x++) {
+        if (this.board[y][x]) {
+          cells.push({ x, y });
+        }
+      }
+    }
+    return cells;
+  }
+
   render() {
     return (
       <div>
