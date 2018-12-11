ea1cab7fcebe07a34577cee2df4125f458f4744d
Carey <careycbaldwin@gmail.com>
2018-12-11 11:30:32 -0500

    box, grid, and main components
    also worked on box on/off functionality

--- game-of-life/src/App.js
+++ game-of-life/src/App.js
@@ -1,28 +1,102 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
+class Box extends React.Component {
+  selectBox = () => {
+    this.props.selectBox(this.props.row, this.props.col);
+  }
+
+  render(){
+    return(
+      <div
+          className = {this.props.boxClass}
+          id = {this.props.id}
+          onClick = {this.selectBox}
+      />
+
+    );
+  }
+}
+
+class Grid extends React.Component{
+
+  render(){
+    //set a constant width equal to the number columns
+    const width = this.props.cols * 31;
+    let rowArray = [];
+
+    let boxClass = "";
+    for(let i = 0; i < this.props.rows; i++) {
+      for (let j = 0; j < this.props.cols; j++) {
+        let boxId = i + "_" + j; // the first box id would be 1_1
+
+        boxClass = this.props.gridFull[i][j] ? "box on" : "box off";
+        rowArray.push(
+          <Box
+            boxClass = {boxClass}
+            key = {boxId}
+            boxId = {boxId}
+            row = {i}
+            col = {j}
+            selectBox = {this.props.selectBox}
+            />
+        )
+      }
+    }
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className = "grid" style = {{width: width}}>
+        {rowArray}
       </div>
     );
   }
+}
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.rows = 15;
+    this.cols = 15;
+
+    this.state = {
+      generation: 0,
+      gridFull: Array(this.rows).fill().map(() => Array(this.cols).fill(false))
+    }
+  }
+
+  selectBox = (row, col) => {
+    let gridCopy = arrayClone(this.state.gridFull);
+    gridCopy[row][col] = !gridCopy[row][col];
+    this.setState({
+      gridFull: gridCopy
+    });
+  }
+
+  render(){
+
+    return (
+      <div className="App">
+        <h1>
+          Conway's Game Of Life
+        </h1>
+        <h4>
+          Generations: {this.state.generation}
+        </h4>
+        <Grid 
+          gridFull = {this.state.gridFull}
+          rows = {this.rows}
+          cols = {this.cols}
+          selectBox = {this.selectBox}
+        />
+
+      </div>
+    );
+  }
+}
+
+function arrayClone(arr){
+  return JSON.parse(JSON.stringify(arr));
 }
 
 export default App;
