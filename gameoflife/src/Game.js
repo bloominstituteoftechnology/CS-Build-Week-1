9976fdbd5e096e58c44add5ce2262d1cd9587678
Rachel DiCesare <racheldicesare@missrachelthedeveloper.com>
2018-12-11 16:35:47 -0500

    adding rules and about info to app

--- gameoflife/src/Game.js
+++ gameoflife/src/Game.js
@@ -206,6 +206,15 @@ class Game extends React.Component {
             <button className="button" onClick={this.stepthroughGame}>Step</button>
             <button className="button" onClick={this.handleClear}>Clear</button>
             </div>
+            <div className = "Rules">
+            <h4>Game Rules</h4>
+            <ol>
+            <li>Any live cell with fewer than two live neighbors dies, as if caused by under population.</li>
+            <li>Any live cell with two or three live neighbors lives on to the next generation.</li>
+            <li>Any live cell with more than three live neighbors dies, as if by overpopulation.</li>
+            <li>Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.</li>
+            </ol>
+            </div>
         </div>
         );
     }
