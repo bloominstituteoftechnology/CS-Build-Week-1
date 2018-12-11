243f203f718f994c32a39a432347b5f7e1912425
Erik Kimsey <erik@erikkimsey.com>
2018-12-11 12:49:57 -0500

    Cell change fill color dynamically

--- gol/src/GOL/Cell.js
+++ gol/src/GOL/Cell.js
@@ -10,24 +10,37 @@ dark gray: #363636
 
 class Cell {
   constructor(p, i, j, fill){
-    this.active = 0;
+    this.active = 1;
     this.id = null;
     this.x = i;
     this.y = j;
     this.width = 20;
     this.height = 20;
     this.p = p;
-    this.fill = fill;
+    this.activeFill = "#cc527a";
+    this.inactiveFill = "#d11554";
   }
   
   setXAndY = () => {
     
   };
 
+  setFill = () => {
+    if(this.active == 0){
+      this.fill = this.activeFill;
+    } else {
+      this.fill = this.inactiveFill;
+    }
+    return this.fill;
+  }
+
   createRect = () => {
+    let fillV = this.setFill();
+    console.log(fillV);
+    
     this.p.noStroke();
     this.p.rect(this.x*this.width, this.y*this.width, this.width-1, this.width-1);
-    this.p.fill(this.fill);
+    this.p.fill(fillV);
   };
 }
 
