f393f9c0f4a191a58f240e79ce0f88193623e312
Arthur Bates <aabatesj@gmail.com>
2018-12-11 18:30:43 -0500

    Main components
    added Grid and Main app component
    
    added some prelimary styling, will change later

--- gameoflife/src/index.js
+++ gameoflife/src/index.js
@@ -2,4 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Grid extends React.Component {
+    render() {
+        return (
+            <div>
+                Grid
+            </div>
+        );
+    }
+}
+
+class Main extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            generation: 0,
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>The Game of Life</h1>
+                <Grid
+                />
+                <h2>Generations: {this.state.generation}</h2>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<Main />, document.getElementById('root'));
