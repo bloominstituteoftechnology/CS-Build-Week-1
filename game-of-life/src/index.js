a0963d9c86431dd5d9bf449f0fd5c178755a5946
your <jurgenkela@gmail.com>
2018-12-11 16:59:22 -0500

    grid displaying with appropriate labeling

--- game-of-life/src/index.js
+++ game-of-life/src/index.js
@@ -1,12 +1,80 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Box extends React.Component {
+  selectBox = () => {
+    this.props.selectBox(this.props.rows, this.props.col);
+  };
+  render() {
+    return (
+      <div
+        className={this.props.boxClass}
+        id={this.props.id}
+        onClick={this.selectBox}
+      />
+    );
+  }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Grid extends React.Component {
+  render() {
+    const width = this.props.cols * 14;
+    var rowsArr = [];
+
+    var boxClass = "";
+    for (var i = 0; i < this.props.rows; i++) {
+      for (var j = 0; j < this.props.cols; j++) {
+        let boxId = i + "_" + j;
+
+        boxClass = this.props.gridFull[i][j] ? "box on" : "box off";
+        rowsArr.push(
+          <Box
+            boxClass={boxClass}
+            key={boxId}
+            boxId={boxId}
+            row={i}
+            col={j}
+            selectBox={this.props.selectBox}
+          />
+        );
+      }
+    }
+
+    return (
+      <div className="grid" style={{ width: width }}>
+        {rowsArr}
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  speed = 100;
+  rows = 30;
+  cols = 50;
+
+  state = {
+    generation: 0,
+    gridFull: Array(this.rows)
+      .fill()
+      .map(() => Array(this.cols).fill(false))
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Conway's Game of Life</h1>
+        <h2>Number of Generations: {this.state.generation}</h2>
+        <Grid
+          gridFull={this.state.gridFull}
+          rows={this.rows}
+          cols={this.cols}
+          selectBox={this.selectBox}
+        />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
