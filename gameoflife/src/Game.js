61fa427ed24138fe381ea2edf4d609e15f1a3ef3
Rachel DiCesare <racheldicesare@missrachelthedeveloper.com>
2018-12-11 17:19:58 -0500

    added about , may make another file to put it in but onto important tasks

--- gameoflife/src/Game.js
+++ gameoflife/src/Game.js
@@ -215,6 +215,14 @@ class Game extends React.Component {
             <li>Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.</li>
             </ol>
             </div>
+            <div className = "About">
+             <h4>About</h4>
+             <p>Conway's Game of Life is an imaginary cellular automation made by the british mathematician
+            John Horton Conway in 1970.This is a zero-player game, meaning its evolution is determined by its initial state
+            ,requiring no further input.The reason it is called a game is because people who play the game can set
+            it up in different ways to make it do different things.It is on the of the first ever simulation games that
+            represent things that happen in real life.</p>
+            </div>
         </div>
         );
     }
