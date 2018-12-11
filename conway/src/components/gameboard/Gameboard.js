2f9fde17b60c56c5e23ea9f60d45e12efc5d3f00
Joseph Stossmeister <joseph.stossmeister@gmail.com>
2018-12-11 16:36:47 -0500

    clicking changes state

--- conway/src/components/gameboard/Gameboard.js
+++ conway/src/components/gameboard/Gameboard.js
@@ -12,11 +12,11 @@ class Gameboard extends Component {
     canvas.fillStyle = "white";
     canvas.fillRect(0, 0, this.state.canvasSize, this.state.canvasSize);
     const startState = new Array(this.state.squareSize)
-      .fill(0)
+      .fill(false)
       .map(() =>
         new Array(this.state.squareSize)
-          .fill(0)
-          .map(() => (Math.floor(Math.random() * 2) === 0 ? 1 : 0))
+          .fill(false)
+          .map(() => (Math.floor(Math.random() * 2) === 0 ? true : false))
       );
     this.setState({ gameState: startState }, () => this.drawBoard());
   }
@@ -26,7 +26,7 @@ class Gameboard extends Component {
     const tileSize = this.state.canvasSize / this.state.squareSize;
     for (let i = 0; i < this.state.squareSize; i++) {
       for (let j = 0; j < this.state.squareSize; j++) {
-        if (this.state.gameState[j][i] === 1) {
+        if (this.state.gameState[j][i]) {
           canvas.fillStyle = "black";
           canvas.fillRect(i * tileSize, j * tileSize, tileSize, tileSize);
         } else {
@@ -37,8 +37,15 @@ class Gameboard extends Component {
     }
   };
 
-  handleClick = (canvas, event) => {
-
+  handleClick = event => {
+    const canvas = this.refs.canvas;
+    const rect = canvas.getBoundingClientRect();
+    const tileSize = this.state.canvasSize / this.state.squareSize;
+    const x = Math.floor((event.clientX - rect.left) / tileSize);
+    const y = Math.floor((event.clientY - rect.top) / tileSize);
+    const gameState = this.state.gameState.slice();
+    gameState[y][x] = !gameState[y][x];
+    this.setState({ gameState }, () => this.drawBoard());
   }
 
   render() {
@@ -47,6 +54,7 @@ class Gameboard extends Component {
         ref="canvas"
         width={this.state.canvasSize}
         height={this.state.canvasSize}
+        onClick={this.handleClick}
       />
     );
   }
