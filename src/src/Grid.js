4c8d1f54b2973b960c8a51888c15bb09c59da844
Noah Amar <ne.amar@outlook.com>
2018-12-11 17:11:54 -0600

    animation is clickable, can fill in grid cells using mouse or click

--- src/src/Grid.js
+++ src/src/Grid.js
@@ -22,6 +22,41 @@ class Grid extends Component {
             }
         }
      }
+
+     componentDidMount(){
+         const canvas = this.refs.canvas;
+         const ctx = canvas.getContext('2d');
+         const boxSize = 40;
+         const boxes = Math.floor(200/boxSize);
+         canvas.addEventListener('click', handleClick);
+         canvas.addEventListener('mousemove', handleClick);
+
+         function drawBox(){
+             ctx.beginPath();
+             ctx.fillStyle = "white";
+             ctx.lineWidth = 3;
+             ctx.strokeStyle = 'black';
+             for (let row = 0; row < boxes; row++){
+                for (let column = 0; column < boxes; column++){
+                    let x = column * boxSize;
+                    let y = row * boxSize;
+                    ctx.rect(x, y, boxSize, boxSize);
+                    ctx.fill();
+                    ctx.stroke();
+                }
+             }
+             ctx.closePath();
+         }
+
+         function handleClick(e){
+            ctx.fillStyle = 'black';
+
+            ctx.fillRect(Math.floor(e.offsetX / boxSize)* boxSize,
+            Math.floor(e.offsetY / boxSize) * boxSize,
+            boxSize, boxSize);
+         }
+         drawBox();
+     }
     render() { 
         return ( 
             <div>
