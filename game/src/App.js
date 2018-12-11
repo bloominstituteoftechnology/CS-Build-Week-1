3af32af60e46d35e34f28cc49594f2dfa8282b52
LizBaker <lizbaker.pdx@gmail.com>
2018-12-11 12:12:42 -0800

    Added main algo to give the cells life! -uses a buffer for proper transition

--- game/src/App.js
+++ game/src/App.js
@@ -22,6 +22,7 @@ class App extends Component {
   drawGrid = () => {
     const c = this.refs.grid
     const ctx = c.getContext("2d");
+    ctx.strokeStyle = "blue"
     for(let i=0; i<= 300; i+=20) {
       ctx.beginPath()
       //vertical
@@ -33,10 +34,75 @@ class App extends Component {
       ctx.stroke();
    }
   }
+
+  moveCells = () => {
+    const c = this.refs.grid
+    const ctx = c.getContext("2d");
+    if(this.state.running){
+      let stateBuffer = {...this.state};
+      for (let i=0; i<=300; i+=20){
+        for (let j=0; j<=300; j+=20){
+          let livingNeighbors = 0
+          let neighborTop = `${i}_${j-20}`
+          let neighborBot = `${i}_${j+20}`
+          let neighborL = `${i-20}_${j}`
+          let neighborR = `${i+20}_${j}`
+          let neighborTopL = `${i-20}_${j-20}`
+          let neighborTopR = `${i+20}_${j-20}`
+          let neighborBotL = `${i-20}_${j+20}`
+          let neighborBotR = `${i+20}_${j+20}`
+          if (this.state[neighborTop] === "alive"){
+            ++livingNeighbors
+          }
+          if (this.state[neighborBot] === "alive"){
+            ++livingNeighbors
+          }
+          if (this.state[neighborL] === "alive"){
+            ++livingNeighbors
+          }
+          if (this.state[neighborR] === "alive"){
+            ++livingNeighbors
+          }
+          if (this.state[neighborTopL] === "alive"){
+            ++livingNeighbors
+          }
+          if (this.state[neighborTopR] === "alive"){
+            ++livingNeighbors
+          }
+          if (this.state[neighborBotL] === "alive"){
+            ++livingNeighbors
+          }
+          if (this.state[neighborBotR] === "alive"){
+            ++livingNeighbors
+          }
+          console.log("neighbors", livingNeighbors)
+          if (this.state[`${i}_${j}`] === "alive" && livingNeighbors > 3) {
+            console.log("o no cell die")
+            stateBuffer[`${i}_${j}`] = "dead"
+            ctx.fillStyle = "#FFFFFF"
+            ctx.fillRect(i+1, j+1, 19, 19);
+          } else if (this.state[`${i}_${j}`] === "alive" && livingNeighbors < 2) {
+            console.log("o no cell die")
+            stateBuffer[`${i}_${j}`] = "dead"
+            ctx.fillStyle = "#FFFFFF"
+            ctx.fillRect(i+1, j+1, 19, 19);
+          } else if (this.state[`${i}_${j}`] === "dead" && livingNeighbors === 3) {
+            console.log("come to liiiife")
+            stateBuffer[`${i}_${j}`] = "alive"
+            ctx.fillStyle = "#DB7093"
+            ctx.fillRect(i+1, j+1, 19, 19);
+          }
+        }
+      }
+      this.setState(stateBuffer);
+      requestAnimationFrame(this.moveCells);
+    }
+  }
   buttonClick = (type) => {
     switch(type){
       case "run":
         this.setState({running: true})
+        requestAnimationFrame(this.moveCells)
         break;
       case "stop":
         this.setState({running: false})
@@ -44,9 +110,9 @@ class App extends Component {
       case "clear":
         const c = this.refs.grid
         const ctx = c.getContext("2d");
-        ctx.clearRect(0,0,c.width,c.height)
-        this.drawGrid()
-        this.gridStateInit()
+        ctx.clearRect(0,0,c.width,c.height) //this clears the entire canvas
+        this.drawGrid() // so i redraw it here
+        this.gridStateInit() //reset state with all dead cells
         break;
     }
   }
@@ -56,13 +122,15 @@ class App extends Component {
       const ctx = c.getContext("2d");
       const boxSize = 20
       //offset squares to make up for where the grid is on the page
-      const x = Math.floor(e.clientX / boxSize) * boxSize -39
-      const y = Math.floor(e.clientY / boxSize) * boxSize -79
-      ctx.fillStyle = this.state[`${x-1}_${y-1}`] === "alive" ? "#FFFFFF" : "#DB7093";
-      //minus 1 px for border
-      ctx.fillRect(x, y, 19, 19);
-      const cellStatus = this.state[`${x-1}_${y-1}`] === "alive" ? "dead" : "alive"
-      this.setState({[`${x-1}_${y-1}`]: cellStatus});
+      //minus 9 on clientX because I had issues with it reporting a larger number than
+      //I expected, can't figure out for now so using brute force
+      const x = Math.floor((e.clientX - 9) / boxSize) * boxSize -40
+      const y = Math.floor(e.clientY / boxSize) * boxSize -80
+      ctx.fillStyle = this.state[`${x}_${y}`] === "alive" ? "#FFFFFF" : "#DB7093";
+      //plus 1 px for border
+      ctx.fillRect(x+1, y+1, 19, 19);
+      const cellStatus = this.state[`${x}_${y}`] === "alive" ? "dead" : "alive"
+      this.setState({[`${x}_${y}`]: cellStatus});
     } else {
       return null;
     }
