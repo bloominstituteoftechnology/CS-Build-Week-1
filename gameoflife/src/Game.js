0906e0c71081bbb713ba38f11ae587ea08650c3d
JoCatherine Griggs <jocafrin@gmail.com>
2018-12-11 13:03:45 -0500

    added methods to game calss

--- gameoflife/src/Game.js
+++ gameoflife/src/Game.js
@@ -7,6 +7,39 @@ const WIDTH = 800;
 const HEIGHT = 600;
 
 class Game extends React.Component {
+    constructor () {
+        super();
+        this.rows = HEIGHT / CELL_SIZE;
+        this.cols = WIDTH / CELL_SIZE;
+        this.board = this.createEmptyBoard();
+    }
+    state = {
+        cells: [],
+    }
+
+    //create the empty board
+    createEmptyBoard() {
+        let board= [];
+        for (let y = 0; y < this.rows; y++) {
+            board[y] = [];
+            for (let x = 0; x < this.cols; x++) {
+                board[y][x] = false;
+            }
+        }
+        return board;
+    }
+    // now, let's create cells from this.board
+    makeCells() {
+        let cells = [];
+        for (let y = 0; y < this.rows; y++) {
+            for (let x = 0; x < this.cols; x++) {
+                if (this.board[y][x]) {
+                    cells.push({ x,y });
+                }
+            }
+        }
+        return cells;
+    }
 
     render() {
         return (
