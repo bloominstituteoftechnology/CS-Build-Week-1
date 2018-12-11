7b1dab8ee67ac21bfa5d5ae51998941057b69fee
vanceleon <vanceleon44@gmail.com>
2018-12-10 23:23:39 -0500

    gitignore

--- gameoflife/src/components/gridContainer.js
+++ gameoflife/src/components/gridContainer.js
@@ -1,13 +1,27 @@
 import React, { Component } from "react";
 import Square from "./square";
 
-const GridContainer = () => {
-  return (
-    <div className="grid-container">
-      <Square />
-      
-    </div>
-  );
-};
+class GridContainer extends Component {
+  state = {
+    alive: "",
+  }
+
+
+  componentDidMount() {
+    const canvas = this.refs.canvas;
+    const ctx = canvas.getContent('2d');
+    ctx.canvas.width = 500;
+    
+  }
+
+
+  render() {
+    return (
+      <div className="grid-container">
+        <canvas className="grid" />
+      </div>
+    );
+  }
+}
 
 export default GridContainer;
