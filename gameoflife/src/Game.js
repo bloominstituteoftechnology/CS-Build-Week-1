a88fe514907dfbc9c8cf6d9adb3d3faef01e70d6
JoCatherine Griggs <jocafrin@gmail.com>
2018-12-11 16:17:35 -0500

    added css to cells

--- gameoflife/src/Game.js
+++ gameoflife/src/Game.js
@@ -14,7 +14,7 @@ class Cell extends React.Component{
         return (
             <div className="Cell" style={{
                 left:  `${CELL_SIZE * x +1}px`,
-                top: `${CELL_SIZE * x + 1}px`,
+                top: `${CELL_SIZE * y + 1}px`,
                 width: `${CELL_SIZE - 1}px`,
                 height: `${CELL_SIZE - 1}px`,
             }} />
