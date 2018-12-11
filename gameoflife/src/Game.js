73ad6ea0835406e588eff7d8cf5f8d28c8cf6c49
JoCatherine Griggs <jocafrin@gmail.com>
2018-12-11 16:40:37 -0500

    added helper functions

--- gameoflife/src/Game.js
+++ gameoflife/src/Game.js
@@ -1,106 +1,130 @@
-import React from 'react';
-import './Game.css';
+import React from "react";
+import "./Game.css";
 
-
-const CELL_SIZE = 20;
+const CELL_SIZE = 35;
 const WIDTH = 800;
 const HEIGHT = 600;
 
 //rendering cells to the board
-class Cell extends React.Component{
-
-    render() {
-        const { x, y } = this.props;
-        return (
-            <div className="Cell" style={{
-                left:  `${CELL_SIZE * x +1}px`,
-                top: `${CELL_SIZE * y + 1}px`,
-                width: `${CELL_SIZE - 1}px`,
-                height: `${CELL_SIZE - 1}px`,
-            }} />
-        );
-    }
+class Cell extends React.Component {
+  render() {
+    const { x, y } = this.props;
+    return (
+      <div
+        className="Cell"
+        style={{
+          left: `${CELL_SIZE * x + 1}px`,
+          top: `${CELL_SIZE * y + 1}px`,
+          width: `${CELL_SIZE - 1}px`,
+          height: `${CELL_SIZE - 1}px`
+        }}
+      />
+    );
+  }
 }
 
 class Game extends React.Component {
-    constructor () {
-        super();
-        this.rows = HEIGHT / CELL_SIZE;
-        this.cols = WIDTH / CELL_SIZE;
-        this.board = this.createEmptyBoard();
-    }
-    state = {
-        cells: [],
-    }
+  constructor() {
+    super();
+    this.rows = HEIGHT / CELL_SIZE;
+    this.cols = WIDTH / CELL_SIZE;
+    this.board = this.createEmptyBoard();
+  }
+  state = {
+    cells: [],
+    interval: 100,
+    isRunning: false
+  };
 
-    //create the empty board
-    createEmptyBoard() {
-        let board= [];
-        for (let y = 0; y < this.rows; y++) {
-            board[y] = [];
-            for (let x = 0; x < this.cols; x++) {
-                board[y][x] = false;
-            }
+  //create the empty board
+  createEmptyBoard() {
+    let board = [];
+    for (let y = 0; y < this.rows; y++) {
+      board[y] = [];
+      for (let x = 0; x < this.cols; x++) {
+        board[y][x] = false;
+      }
+    }
+    return board;
+  }
+
+  //calculates position of our board element
+  getElementOffset() {
+    const rect = this.boardRef.getBoundingClientRect();
+    const doc = document.documentElement;
+
+    return {
+      x: rect.left + window.pageXOffset - doc.clientLeft,
+      y: rect.top + window.pageYOffset - doc.clientTop
+    };
+  }
+
+  // now, let's create cells from this.board
+  makeCells() {
+    let cells = [];
+    for (let y = 0; y < this.rows; y++) {
+      for (let x = 0; x < this.cols; x++) {
+        if (this.board[y][x]) {
+          cells.push({ x, y });
         }
-        return board;
+      }
     }
+    return cells;
+  }
 
-    getElementOffset() {
-        ///calculates position of our board element
-        const rect = this.boardRef.getBoundingClientRect();
-        const doc = document.documentElement;
+  //   retrieves click position, makes it
+  //   relative and calculates columns and rows being clicked.
+  //   state is then reversed.
+  clickHandler = e => {
+    const elemOffset = this.getElementOffset();
+    const offsetX = e.clientX - elemOffset.x;
+    const offsetY = e.clientY - elemOffset.y;
 
-        return {
-            x: (rect.left + window.pageXOffset) - doc.clientLeft,
-            y: (rect.top + window.pageYOffset) - doc.clientTop,
-        };
+    const x = Math.floor(offsetX / CELL_SIZE);
+    const y = Math.floor(offsetY / CELL_SIZE);
+
+    if (x >= 0 && x <= this.cols && y >= 0 && y <= this.rows) {
+      this.board[y][x] = !this.board[y][x];
     }
+    this.setState({ cells: this.makeCells() });
+  };
 
-    clickHandler = (e) => {
-        //retrieves click position, makes it relative and calculates columns and rows being clicked.
-        //state is then reversed.
-        const elemOffset = this.getElementOffset();
-        const offsetX = e.clientX - elemOffset.x;
-        const offsetY = e.clientY - elemOffset.y;
+  //HELPER FUNCTIONS
+  runGame = () => {
+    this.setState({ isRunning: true });
+  }
 
-        const x = Math.floor(offsetX / CELL_SIZE);
-        const y = Math.floor(offsetY /CELL_SIZE);
+  stopGame = () => {
+      this.setState({ isRunning: false});
+  }
 
-        if (x >= 0 && x <= this.cols && y >= 0 && y <= this.rows) {
-            this.board[y][x] = !this.board[y][x];
-        }
-        this.setState({ cells: this.makeCells() });
-    }
+  handleIntervalChange = (e) => {
+      this.setState({interval: e.target.value});
+  }
 
-    // now, let's create cells from this.board
-    makeCells() {
-        let cells = [];
-        for (let y = 0; y < this.rows; y++) {
-            for (let x = 0; x < this.cols; x++) {
-                if (this.board[y][x]) {
-                    cells.push({ x,y });
-                }
-            }
-        }
-        return cells;
-    }
-
-    render() {
-        const { cells } = this.state;
-        return (
-            <div>
-                <div className="gameBoard"
-                style ={{ width: WIDTH, height: HEIGHT, backgroundSize: `${CELL_SIZE}px ${CELL_SIZE}px`}}
-                onClick={this.clickHandler}
-                ref={(n) => { this.boardRef = n; }}>
-                {cells.map(cell => (
-                    <Cell x={cell.x} y={cell.y}
-                        key={`${cell.x}, ${cell.y}`}/>
-                ))}
-                </div>
-            </div>
-        );
-    }
+  render() {
+    const { cells } = this.state;
+    return (
+      <div>
+        <div
+          className="gameBoard"
+          style={{
+            width: WIDTH,
+            height: HEIGHT,
+            backgroundSize: `${CELL_SIZE}px ${CELL_SIZE}px`
+          }}
+          onClick={this.clickHandler}
+          ref={n => {
+            this.boardRef = n;
+          }}
+        >
+          {cells.map(cell => (
+            <Cell x={cell.x} y={cell.y} key={`${cell.x}, ${cell.y}`} />
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Game;
