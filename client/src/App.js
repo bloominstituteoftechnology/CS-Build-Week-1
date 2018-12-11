abdacd4c1e913d0d7e72360092c63e5fb8cf263d
codejoncode <jonathanjholloway@gmail.com>
2018-12-10 18:24:02 -0600

    I was using only one object and changing the data which meant i was using one pointer to an address so when i changed the hash table and added it to the array I was adding  one address thus each item in the array was the exact same.  I fixed this by bringing the creation of the object and then updating inside of the loop. I also had to move the count variable outside of the loop to keep the count incrementing and not reseting.

--- client/src/App.js
+++ client/src/App.js
@@ -21,47 +21,46 @@ class App extends Component {
       13: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
       14: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     },
-    count : 0, 
+    
     matrixUsing : [], 
   };
 
   componentWillMount () {
     this.setMatrixUp(); 
   }
-  increaseCount () {
-    this.setState({count : this.state.count + 1}); 
-  }
 
   setMatrixUp () {
     const matrixUsing = []; 
-    const temp_hash = {row: 0, position_in_row: 0, actual_number : 0, value: 0};
-
+    let count = 0
     Object.entries(this.state.matrix).forEach(entry => {
+      
       for (let x of entry[1]){
-        temp_hash.row = entry[0]; 
-        temp_hash.position_in_row = this.state.count % 14;
-
-        temp_hash.actual_number = this.state.count; 
-        temp_hash.value = x; 
+        const temp_hash = {row: 0, position_in_row: 0, actual_number : 0};
+        temp_hash.row = Number(entry[0]); 
+        // console.log(entry[0])
+        temp_hash.position_in_row = count % 15;
+        // console.log(count % 15); 
+        temp_hash.actual_number = count; 
         matrixUsing.push(temp_hash); 
-        
+        count++;
+        console.log(temp_hash, count);  
       }
     });
+    console.log(matrixUsing.length); 
+    console.log(matrixUsing);
     this.setState({matrixUsing}); 
-    this.setState({count : 0 }); //reset count 
+    
   }
 
-  turnOnOrOff = (row, position_in_row, value) => {
+  turnOnOrOff = (row, position_in_row) => {
       //Just add 1 to the % of 2  it will provide 0 or 1. The conditional is already set up on the div to set the div to the correct class based off the value
       console.log("Getting into the function.");
-      console.log(row, position_in_row, value); 
+      console.log(row, position_in_row); 
       console.log("above is the paramaters for the function"); 
       const matrix = this.state.matrix; 
       console.log(matrix[row][position_in_row]); 
       matrix[row][position_in_row] = matrix[row][position_in_row] === 0 ? 1 : 0; 
       this.setState({matrix}); 
-      
-      
   }
 
   render() {
@@ -70,7 +69,7 @@ class App extends Component {
 
     return (<div className="container"> 
       
-      {matrix.map(hash => <div key = {hash.actual_number} onClick = {() => this.turnOnOrOff(hash.row, hash.position_in_row, hash.value)} className = {this.state.matrix[hash.row][hash.position_in_row] === 0 ? "offDiv" : "onDiv"} >ADD</div> )}
+      {matrix.map((hash, id) => <div key = {id} onClick = {() => this.turnOnOrOff(hash.row, hash.position_in_row)} className = {this.state.matrix[hash.row][hash.position_in_row] === 0 ? "offDiv gridDiv" : "onDiv gridDiv"} >{hash.position_in_row}</div> )}
       
     
     </div>);
