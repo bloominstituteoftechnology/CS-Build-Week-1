541a237133d3f5c0e588661fc33c9158f9e4343a
“Kennedykid1995” <34872885+Kennedykid1995@users.noreply.github.com>
2018-12-11 12:58:09 -0600

    Stages
    working on the Stages.js logic for the process to work.

--- game_of_life/src/Stages.js
+++ game_of_life/src/Stages.js
@@ -1,1 +1,28 @@
 import React, { Component } from 'react';
+
+//Rules:
+
+// A live cell with less than two live neighbours dies.
+// A live cell with two or three neighbours lives on to the next generation.
+// A live cell with more than tree neighbours dies.
+// A dead cell with exactly three neighbours is reborn and becomes a live cell.
+
+//to start you will need to initiate the states of several things.
+//stage = how many stages of change the cell goes through
+//living = will give us a map of the living cells
+//dead =  will give a map of the dead cells
+//nextStage = will give a map of the new stage in the process.
+
+class Stages extends Component{
+    constructor(){
+        super();
+        this.state ={
+            stage = 0,
+            living = new Map(),
+            nextStage = new Map(),
+            dead = new Map(),
+        }
+    }
+    
+}
+export default Stages
