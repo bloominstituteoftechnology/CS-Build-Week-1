b0b93113b07dc106dc961a9a8a3384801a6428e5
Fred <sohnf90@yahoo.com>
2018-12-11 15:04:12 -0800

    clear button working but takes a couple seconds. erasing whole grid and remaking original all dead cell grid

--- conways-life/src/components/Grid.js
+++ conways-life/src/components/Grid.js
@@ -16,12 +16,29 @@ export default class Grid extends Component {
         ctx.fillRect(Math.floor(e.offsetX / 20) * 20, Math.floor(e.offsetY / 20) *20, 20, 20);
     }
 
+    clearGrid = (e) => {
+        const canvas = this.refs.canvas;
+        const ctx = canvas.getContext('2d');
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        for(let x = 0; x <= 400; x+=20){
+            for(let y = 0; y<= 400; y+=20){
+                ctx.moveTo(x, 0);
+                ctx.lineTo(x, 400);
+                ctx.stroke();
+                ctx.moveTo(0, y);
+                ctx.lineTo(400, y);
+                ctx.stroke();
+            }
+        }
+    }
+
     componentDidMount() {
         const canvas = this.refs.canvas;
         const ctx = canvas.getContext('2d');
         ctx.canvas.width = 400;
         ctx.canvas.height = 400;
         canvas.addEventListener('click', this.handleClick);
+        canvas.addEventListener('clear', this.clearGrid);
 
         for(let x = 0; x <= 400; x+=20){
             for(let y = 0; y<= 400; y+=20){
@@ -37,7 +54,12 @@ export default class Grid extends Component {
 
     render() {
         return(
-            <canvas class="canvas" ref="canvas" />
+            <div>
+                <canvas class="canvas" ref="canvas" />
+                <button onClick={this.play}>Play</button>
+                <button onClick={this.pause}>Pause</button>
+                <button onClick={this.clearGrid}>Clear</button>
+            </div>
         );
     }
 }
