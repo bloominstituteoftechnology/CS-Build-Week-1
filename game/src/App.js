a689b9c24a029c6690a8cded317ebf5721e11365
Grant Reighard <grant.reighard@gmail.com>
2018-12-11 22:16:27 -0500

    Laid groundwork for 3D version.

--- game/src/App.js
+++ game/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Game from './components/Game.js';
+import Game3D from './components/Game3D.js';
 import About from './components/About';
 import {Link, Route} from 'react-router-dom';
 
@@ -10,7 +11,10 @@ class App extends Component {
       <div className="App">
         <h1>Welcome to Conway's Game of Life - by Grant Reighard</h1>
         <Route exact path="/" render={props => <Link to="/about">About this game</Link>} />
+        <Route exact path="/" render={props => <Link to="/3d">3D Version</Link>} />
+        <Route exact path="/3d" render={props => <Link to="/">2D Version</Link>} />
         <Route exact path="/" render={props => <Game />} />
+        <Route path="/3d" render={props => <Game3D />} />
         <Route path="/about" render={props => <About />} />
       </div>
     );
