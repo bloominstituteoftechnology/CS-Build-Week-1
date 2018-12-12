3aec2060c63524f49490754dde1eee9bd4fd9736
Grant Reighard <grant.reighard@gmail.com>
2018-12-12 13:38:22 -0500

    Added link to blog post.

--- game/src/App.js
+++ game/src/App.js
@@ -12,6 +12,7 @@ class App extends Component {
         <h1>Welcome to Conway's Game of Life - by Grant Reighard</h1>
         <Route exact path="/" render={props => <Link to="/about">About this game</Link>} />
         <Route exact path="/" render={props => <Link to="/3d">3D Version</Link>} />
+        <Route exact path="/" render={props => <a href="https://medium.com/@grant.reighard/how-i-programmed-conways-game-of-life-in-react-lambda-school-a851dfbfb3b4">Blog about this app's development</a>} />
         <Route exact path="/3d" render={props => <Link to="/">2D Version</Link>} />
         <Route exact path="/" render={props => <Game />} />
         <Route path="/3d" render={props => <Game3D />} />
