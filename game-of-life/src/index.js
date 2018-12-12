37d9dd580f422b7a165cf45c27ab9b991db73bf7
your <jurgenkela@gmail.com>
2018-12-12 16:50:11 -0500

    MVP completed with 3 custom features

--- game-of-life/src/index.js
+++ game-of-life/src/index.js
@@ -23,6 +23,68 @@ class Box extends React.Component {
   }
 }
 
+const Intructions = () => {
+  return (
+    <div className="instructions">
+      <h3>Game Rules</h3>
+      <ol>
+        <li>
+          Any live cell with fewer than two live neighbors dies, as if by
+          underpopulation.
+        </li>
+        <li>
+          Any live cell with two or three live neighbors lives on to the next
+          generation.
+        </li>
+        <li>
+          Any live cell with more than three live neighbors dies, as if by
+          overpopulation.
+        </li>
+        <li>
+          Any dead cell with exactly three live neighbors becomes a live cell,
+          as if by reproduction.
+        </li>
+      </ol>
+    </div>
+  );
+};
+
+const History = () => {
+  return (
+    <div className="history">
+      <h3>History - Technical Details</h3>
+      <h5>1940</h5>
+      <p>
+        John von Neumann defined life as a creation (as a being or organism)
+        which can reproduce itself and simulate a Turing machine.
+      </p>
+      <h5>1968</h5>
+      <p>
+        John Conway began doing experiments with a variety of different 2D
+        cellular automaton rules. Conway's initial goal was to define an
+        interesting and unpredictable cell automaton.
+      </p>
+      <h5>1970</h5>
+      <p>
+        The game made its first public appearance in the October issue of
+        Scientific American, in Martin Gardner's "Mathematical Games" column. In
+        this respect, it foreshadowed the later popularity of computer-generated
+        fractals. For many, Life was simply a programming challenge: a fun way
+        to use otherwise wasted CPU cycles
+      </p>
+      <h5>1970 and beyond</h5>
+      <p>
+        Conway originally conjectured that no pattern can grow
+        indefinitely—i.e., that for any initial configuration with a finite
+        number of living cells, the population cannot grow beyond some finite
+        upper limit. In the game's original appearance in "Mathematical Games",
+        Conway offered a prize of fifty dollars to the first person who could
+        prove or disprove the conjecture before the end of 1970
+      </p>
+    </div>
+  );
+};
+
 const Grid = props => {
   const width = props.cols * 14;
   let boxClass = "";
@@ -62,10 +124,10 @@ class Buttons extends React.Component {
       <div className="center">
         <ButtonToolbar>
           <button className="btn btn-default" onClick={this.props.playButton}>
-            Play
+            Start
           </button>
           <button className="btn btn-default" onClick={this.props.pauseButton}>
-            Pause
+            Stop
           </button>
           <button className="btn btn-default" onClick={this.props.clear}>
             Clear
@@ -223,7 +285,8 @@ class Main extends React.Component {
           seed={this.seed}
           gridSize={this.gridSize}
         />
-        ;
+        <Intructions />
+        <History />
       </div>
     );
   }
