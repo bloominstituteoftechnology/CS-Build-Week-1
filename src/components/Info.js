6d2d96cb08e2a2804578dc00e6ecdbd5b673020a
Charles Cortinas <crcortinas@gmail.com>
2018-12-12 17:02:54 -0600

    Filled in info

--- src/components/Info.js
+++ src/components/Info.js
@@ -5,24 +5,25 @@ class Info extends React.Component {
     return (
       <div className='info'>
         <section className='textbox'>
-          <h2>Rules</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-             aliquip ex ea commodo consequat. Duis aute irure dolor in
-             reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-              culpa qui officia deserunt mollit anim id est laborum.</p>
+          <a href='https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life#Rules' rel="noopener noreferrer" target='_blank'>Rules</a>
+          <ul>
+            <li>Any live cell with fewer than two live neighbors dies, as if by underpopulation.</li>
+            <li>Any live cell with two or three live neighbors lives on to the next generation.</li>
+            <li>Any live cell with more than three live neighbors dies, as if by overpopulation.</li>
+            <li>Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.</li>
+          </ul>
         </section>
         <section className='textbox'>
-          <a href='https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life'>About The Game of Life</a>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-               ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-               aliquip ex ea commodo consequat. Duis aute irure dolor in
-               reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                culpa qui officia deserunt mollit anim id est laborum.</p>
+          <a href='https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life' rel="noopener noreferrer" target='_blank'>About The Game of Life</a>
+            <p>The Game of Life is not your typical computer game, it is a 'cellular automaton',
+              and was invented by Cambridge mathematician John Conway.This game
+              became widely known when it was mentioned in an article published
+               by Scientific American in 1970. It consists of a collection of
+               cells which, based on a few mathematical rules, can live, die or
+                multiply. Depending on the initial conditions, the cells form
+                 various patterns throughout the course of the game. The Game of
+               Life is Turing complete as, given enough resources, it can solve
+             any algorithm.</p>
         </section>
       </div>
   )}
