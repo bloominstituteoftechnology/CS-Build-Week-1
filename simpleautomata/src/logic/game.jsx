c35b6ffb9c5046a4d437f65974c4cc4ad3efd8ee
tulf <timzulf@gmail.com>
2018-12-11 20:12:27 -0500

    can finally toggle individual cells, state changes implenented.
    needed to use cols vs columns perhaps a reserved word?

--- simpleautomata/src/logic/game.jsx
+++ simpleautomata/src/logic/game.jsx
@@ -5,11 +5,96 @@ const CELL_SIZE = 20;
 const WIDTH = 800;
 const HEIGHT = 600;
 
-class Game extends React.Component {
+class Cell extends React.Component {
   render() {
+    const { x, y } = this.props;
+    return (
+      <div className="Cell" style={{
+        left: `${CELL_SIZE * x + 1}px`,
+        top: `${CELL_SIZE * y + 1}px`,
+        width: `${CELL_SIZE - 1}px`,
+        height: `${CELL_SIZE - 1}px`,
+      }} />
+    );
+  }
+}
+
+class Game extends React.Component {
+  constructor(){
+    super();
+    this.rows = HEIGHT / CELL_SIZE;
+    this.cols = WIDTH / CELL_SIZE;
+    this.board = this.makeEmptyBoard();
+  }
+  state = {
+    //array of cells
+    cells: []
+  }
+  //method to create an empty board:
+  makeEmptyBoard(){
+    let board = [];
+    for(let y = 0; y < this.rows; y++){
+      //make y axis of arrays
+      board[y] = [];
+      for (let x = 0; x < this.cols; x++){
+        // so we have a "2D" array with an array of arrays for entries.
+        board[y][x] = false;
+      }
+    }
+    //return an empty board, with all false values for now.
+    return board;
+  }
+  //Create cells from this.board:
+  makeCells(){
+    let cells = [];
+    for (let y = 0; y < this.rows; y++){
+      for (let x = 0; x < this.cols; x++){
+        if(this.board[y][x]){
+          cells.push({ x, y});
+        }
+      }
+    }
+    return cells;
+  }
+  //getElementOffset calculates the  position of a board element (obv will be mappped on the whole array eventually)
+  //getBoundingClientRect return shte size of an element and its position relative to the viewport
+  //The returned value is a DOMRect object which is the union of the rectangles returned by getClientRects() for the element, i.e., the CSS border-boxes associated with the element. The result is the smallest rectangle which contains the entire element, with read-only left, top, right, bottom, x, y, width, and height properties describing the overall border-box in pixels. Properties other than width and height are relative to the top-left of the viewport.
+  // sicne we are in a grid we can use this with some simple calculations.
+  getElementOffset() {
+    const rect = this.boardRef.getBoundingClientRect();
+    const doc = document.documentElement;
+
+    return {
+      x: (rect.left + window.pageXOffset) - doc.clientLeft,
+      y: (rect.top + window.pageYOffset) - doc.clientTop,
+    };
+  }
+
+  handleClick = (event) => {
+    const elemOffset = this.getElementOffset();
+    const offsetX = event.clientX - elemOffset.x;
+    const offsetY = event.clientY - elemOffset.y;
+
+    const x = Math.floor(offsetX / CELL_SIZE);
+    const y = Math.floor(offsetY / CELL_SIZE);
+
+    if(x>= 0 && x <= this.cols && y>= 0 && y<= this.rows){
+      this.board[y][x] = !this.board[y][x];
+    }
+
+    this.setState({ cells: this.makeCells() });
+  }
+  //event handler with render method to allow user to toggle cells to true(on) onClick.
+  render() {
+    const { cells } = this.state;
     return (
       <div>
-        <div className="Board" style={{ width: WIDTH, height: HEIGHT, backgroundSize: `${CELL_SIZE}px ${CELL_SIZE}px`}}>
+        <div className="Board"
+          style = {{ width: WIDTH, height: HEIGHT,
+             backgroundSize: `${CELL_SIZE}px ${CELL_SIZE}px`}} onClick={this.handleClick}
+          ref={(n) => { this.boardRef = n; }}>
+          {cells.map(cell => (<Cell x={cell.x} y={cell.y} key={`${cell.x},${cell.y}`}/>
+          ))}
         </div>
       </div>
     );
