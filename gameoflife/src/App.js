99d24d1c6dba32f6d41986083afeff0e4c3dd8c2
vanceleon <vanceleon44@gmail.com>
2018-12-11 19:40:40 -0500

    code for grid

--- gameoflife/src/App.js
+++ gameoflife/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import GridContainer from "./components/gridContainer";
 import Details from "./components/details";
--- gameoflife/src/components/gridContainer.js
+++ gameoflife/src/components/gridContainer.js
@@ -4,8 +4,6 @@ class GridContainer extends Component {
   constructor() {
     super();
     this.state = {
-      alive: { isToggleOn: true },
-      square: false,
       grid: [...Array(15)].map(e => Array(15).fill(0))
     };
   }
@@ -29,13 +27,56 @@ class GridContainer extends Component {
   // [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
   // ]
 
+  // Dummy Live Cells
+  // this.state.grid[0][0] = 1;
+  // this.state.grid[5][5] = 1;
+  // this.state.grid[10][3] = 1;
+  // End Dummy Data
+
   componentDidMount() {
-    const w = 450;
-    const h = 450;
+    // drawing the grid
     const canvas = this.refs.canvas;
     const ctx = canvas.getContext("2d");
+    const w = 450;
+    const h = 450;
     ctx.canvas.width = w;
     ctx.canvas.height = h;
+
+    this.drawingGrid(w, h, ctx);
+
+    this.aliveCheck();
+  }
+
+  getMousePos = (canvas, event) => {
+    event.preventDefault();
+    let rect = canvas.getBoundingClientRect();
+    return { x: event.clientX - rect.left, y: event.clientY - rect.top };
+  };
+
+  // function for determining live or dead
+  aliveCheck = () => {
+    console.log("Current Grid in State:", this.state.grid);
+
+    const canvas = this.refs.canvas;
+    const ctx = canvas.getContext("2d");
+    const w = 15;
+    const h = 15;
+    for (let y = 0; y < this.state.grid.length; y++) {
+      for (let x = 0; x < this.state.grid[y].length; x++) {
+        if (this.state.grid[y][x]) {
+          // this.getMousePos()
+          // console.log(`x: ${x} y: ${y}`);
+          ctx.fillStyle = "black";
+          ctx.fillRect(x * w, y * h, w, h);
+        } else {
+          ctx.clearRect(x * w, y * h, w, h);
+        }
+      }
+    }
+    this.drawingGrid(450, 450, ctx);
+  };
+
+  drawingGrid = (w, h, ctx) => {
     // drawing the grid
     for (let j = 0; j <= w; j += 15) {
       for (let k = 0; k <= h; k += 15) {
@@ -47,49 +88,31 @@ class GridContainer extends Component {
         ctx.stroke();
       }
     }
-    canvas.onmousemove = function(event) {
-      let pos = this.getMousePos(this, event),
-      x = pos.x,
-      y = pos.y;
-      ctx.fillRect(j, k, w, h);
-    };
-    
-    // for(y)iterate over each array and see what is alive
-    // for(x) iterate over each element
-  }
-  
-  getMousePos = (canvas, event) => {
-    event.preventDefault();
-    let rect = canvas.getBoundingClientRect();
-    return { x: event.clientX - rect.left, y: event.clientY - rect.top };
-  };
-
-  // function for determining live or dead
-  aliveCheck = () => {
-    for (let y = 0; y < this.state.grid.length; y++) {
-      // console.log(`This is the y value in the array: ${y}`)
-
-      for (let x = 0; x < this.state.grid[y].length; x++) {
-        if (this.state.grid[x]) {
-          
-        }
-      }
-    }
   };
 
   handleClick(event) {
     event.preventDefault();
-    this.setState({
-      square: !this.state.square
-    });
+    const w = 15;
+    const h = 15;
+    const canvas = this.refs.canvas;
+    let pos = this.getMousePos(canvas, event),
+      x = Math.floor(pos.x / w),
+      y = Math.floor(pos.y / h);
+    // console.log(`x: ${x} y  : ${y}`);
   }
+
+  // Run function
+
+  // stop function
 
   render() {
     return (
       <div className="grid-container">
-        <canvas className="grid" ref="canvas" onClick={this.getMousePos}>
-          
-        </canvas>
+        <canvas
+          className="grid"
+          ref="canvas"
+          onClick={event => this.handleClick(event)}
+        />
         <div className="run-button">RUN</div>
         <div className="stop-button">STOP</div>
       </div>
