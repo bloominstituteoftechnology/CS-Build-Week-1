9d0a250d6d4da050e7dcc1a70738b73db539a751
LizBaker <lizbaker.pdx@gmail.com>
2018-12-12 10:34:29 -0800

    Added color selection ability and step functionality -colors change starting with the next step but not on initial click -working on an immediate change

--- game/src/App.js
+++ game/src/App.js
@@ -6,7 +6,8 @@ class App extends Component {
     super();
     this.state = {
       running: false,
-      cycles: 0
+      cycles: 0,
+      color: "#DB7093"
     }
   }
   componentDidMount() {
@@ -36,72 +37,74 @@ class App extends Component {
    }
   }
 
-  moveCells = () => {
+  nextBoard = () => {
+    let stateBuffer = {...this.state};
     const c = this.refs.grid
     const ctx = c.getContext("2d");
-    if(this.state.running){
-      setTimeout(() => {
-      let stateBuffer = {...this.state};
-      for (let i=0; i<300; i+=20){
-        for (let j=0; j<300; j+=20){
-          let livingNeighbors = 0
-          let neighborTop = `${i}_${j-20}`
-          let neighborBot = `${i}_${j+20}`
-          let neighborL = `${i-20}_${j}`
-          let neighborR = `${i+20}_${j}`
-          let neighborTopL = `${i-20}_${j-20}`
-          let neighborTopR = `${i+20}_${j-20}`
-          let neighborBotL = `${i-20}_${j+20}`
-          let neighborBotR = `${i+20}_${j+20}`
-          if (this.state[neighborTop] === "alive"){
-            ++livingNeighbors
-          }
-          if (this.state[neighborBot] === "alive"){
-            ++livingNeighbors
-          }
-          if (this.state[neighborL] === "alive"){
-            ++livingNeighbors
-          }
-          if (this.state[neighborR] === "alive"){
-            ++livingNeighbors
-          }
-          if (this.state[neighborTopL] === "alive"){
-            ++livingNeighbors
-          }
-          if (this.state[neighborTopR] === "alive"){
-            ++livingNeighbors
-          }
-          if (this.state[neighborBotL] === "alive"){
-            ++livingNeighbors
-          }
-          if (this.state[neighborBotR] === "alive"){
-            ++livingNeighbors
-          }
-          if (this.state[`${i}_${j}`] === "alive" && livingNeighbors > 3) {
-            console.log("o no cell die")
-            stateBuffer[`${i}_${j}`] = "dead"
-            ctx.fillStyle = "#FFFFFF"
-            ctx.fillRect(i+1, j+1, 19, 19);
-          } else if (this.state[`${i}_${j}`] === "alive" && livingNeighbors < 2) {
-            console.log("o no cell die")
-            stateBuffer[`${i}_${j}`] = "dead"
-            ctx.fillStyle = "#FFFFFF"
-            ctx.fillRect(i+1, j+1, 19, 19);
-          } else if (this.state[`${i}_${j}`] === "dead" && livingNeighbors === 3) {
-            console.log("come to liiiife")
-            stateBuffer[`${i}_${j}`] = "alive"
-            ctx.fillStyle = "#DB7093"
-            ctx.fillRect(i+1, j+1, 19, 19);
-          }
-          console.log("neighbors:", livingNeighbors, "current cell:", i,j, "alive or dead?", stateBuffer[`${i}_${j}`])
+    for (let i=0; i<300; i+=20){
+      for (let j=0; j<300; j+=20){
+        let livingNeighbors = 0
+        let neighborTop = `${i}_${j-20}`
+        let neighborBot = `${i}_${j+20}`
+        let neighborL = `${i-20}_${j}`
+        let neighborR = `${i+20}_${j}`
+        let neighborTopL = `${i-20}_${j-20}`
+        let neighborTopR = `${i+20}_${j-20}`
+        let neighborBotL = `${i-20}_${j+20}`
+        let neighborBotR = `${i+20}_${j+20}`
+        if (this.state[neighborTop] === "alive"){
+          ++livingNeighbors
+        }
+        if (this.state[neighborBot] === "alive"){
+          ++livingNeighbors
+        }
+        if (this.state[neighborL] === "alive"){
+          ++livingNeighbors
+        }
+        if (this.state[neighborR] === "alive"){
+          ++livingNeighbors
+        }
+        if (this.state[neighborTopL] === "alive"){
+          ++livingNeighbors
+        }
+        if (this.state[neighborTopR] === "alive"){
+          ++livingNeighbors
+        }
+        if (this.state[neighborBotL] === "alive"){
+          ++livingNeighbors
+        }
+        if (this.state[neighborBotR] === "alive"){
+          ++livingNeighbors
+        }
+        if (this.state[`${i}_${j}`] === "alive" && livingNeighbors > 3) {
+          stateBuffer[`${i}_${j}`] = "dead"
+          ctx.fillStyle = "#FFFFFF"
+          ctx.fillRect(i+1, j+1, 19, 19);
+        } else if (this.state[`${i}_${j}`] === "alive" && livingNeighbors < 2) {
+          stateBuffer[`${i}_${j}`] = "dead"
+          ctx.fillStyle = "#FFFFFF"
+          ctx.fillRect(i+1, j+1, 19, 19);
+        } else if (this.state[`${i}_${j}`] === "dead" && livingNeighbors === 3) {
+          stateBuffer[`${i}_${j}`] = "alive"
+          ctx.fillStyle = this.state.color
+          ctx.fillRect(i+1, j+1, 19, 19);
         }
       }
+    }
+    return stateBuffer;
+  }
 
-      let cyclesCopy = this.state.cycles
-      ++cyclesCopy;
-      console.log(stateBuffer)
-      this.setState ({...stateBuffer, cycles: cyclesCopy }, () => requestAnimationFrame(this.moveCells));
-    }, 100)}
+  updateCells = () => {
+    let cyclesCopy = this.state.cycles
+    ++cyclesCopy;
+    if(this.state.running){
+      setTimeout(() => {
+      let stateBuffer = this.nextBoard();
+      this.setState ({...stateBuffer, cycles: cyclesCopy }, () => requestAnimationFrame(this.updateCells));
+    }, 100)} else {
+      let stateBuffer = this.nextBoard();
+      this.setState({...stateBuffer, cycles: cyclesCopy })
+    }
   }
   buttonClick = (type) => {
     const c = this.refs.grid
@@ -109,7 +112,10 @@ class App extends Component {
     switch(type){
       case "run":
         this.setState({running: true})
-        requestAnimationFrame(this.moveCells)
+        requestAnimationFrame(this.updateCells)
+        break;
+      case "step":
+        requestAnimationFrame(this.updateCells)
         break;
       case "stop":
         this.setState({running: false})
@@ -129,7 +135,7 @@ class App extends Component {
           "100_120": "alive",
           "80_100": "alive"
         });
-        ctx.fillStyle = "#DB7093"
+        ctx.fillStyle = this.state.color
         ctx.fillRect(121, 81, 19, 19);
         ctx.fillRect(121, 101, 19, 19);
         ctx.fillRect(121, 121, 19, 19);
@@ -145,7 +151,7 @@ class App extends Component {
           "120_120": "alive",
           "120_140": "alive"
         });
-        ctx.fillStyle = "#DB7093"
+        ctx.fillStyle = this.state.color
         ctx.fillRect(121, 81, 19, 19);
         ctx.fillRect(121, 101, 19, 19);
         ctx.fillRect(121, 121, 19, 19);
@@ -161,7 +167,7 @@ class App extends Component {
           "140_160": "alive",
           "120_180": "alive",
         });
-        ctx.fillStyle = "#DB7093"
+        ctx.fillStyle = this.state.color
         ctx.fillRect(121, 101, 19, 19);
         ctx.fillRect(141, 121, 19, 19);
         ctx.fillRect(161, 141, 19, 19);
@@ -191,6 +197,31 @@ class App extends Component {
     }
   }
 
+  colorClick = (color) => {
+    switch(color) {
+      case "r":
+        this.setState({color: "red"})
+        break;
+      case "o":
+        this.setState({color: "#FF7F50"})
+        break;
+      case "y":
+        this.setState({color: "#FEFA8A"})
+        break;
+      case "g":
+        this.setState({color: "#00FA9A"})
+        break;
+      case "b":
+        this.setState({color: "#1E90FF"})
+        break;
+      case "pu":
+        this.setState({color: "#8A2BE2"})
+        break;
+      case "pi":
+        this.setState({color: "#DB7093"})
+    }
+  }
+
   render() {
     return (
       <div className="App">
@@ -199,21 +230,41 @@ class App extends Component {
           <canvas onClick={this.cellClick} ref="grid" width="301" height="301"/>
           <div className="controls">
             <button onClick={()=>this.buttonClick("run")}>Run</button>
+            <button onClick={()=>this.buttonClick("step")}>Step</button>
             <button onClick={()=>this.buttonClick("stop")}>Stop</button>
             <button onClick={()=>this.buttonClick("clear")}>Clear</button>
           </div>
           <div>
             <h2>Rules</h2>
-            <p>some stuff goes here later</p>
+            <p className="rules">
+              In the game of life each cell can be alive or dead. 
+              Live cells will remain alive as long as they have at least 2 neighbors.
+              Any less, and the cells will die of loneliness. 
+              <br/><br/>However, more than 3 neighbors and the cells will feel totally overburdened 
+              and just want to be left alone, so they feign death. But for your purposes, 
+              we'll consider them truly dead.
+              <br/><br/>If a cell is dead and lucky enough to be surrounded by 3 loving and supportive 
+              neighbors, it will come to life, and thus, the Game of Life continues. 
+            </p>
           </div>
         </div>
         <p className="belowText">Number of cycles: {this.state.cycles}</p>
         <p className="belowText">Presets</p>
         <div className="presets">
-            <button onClick={()=>this.buttonClick("glider")}>Glider</button>
-            <button onClick={()=>this.buttonClick("t")}>T</button>
-            <button onClick={()=>this.buttonClick("arrow")}>Arrow</button>
-          </div>
+          <button onClick={()=>this.buttonClick("glider")}>Glider</button>
+          <button onClick={()=>this.buttonClick("t")}>T</button>
+          <button onClick={()=>this.buttonClick("arrow")}>Arrow</button>
+        </div>
+        <p className="belowText">Cell Color</p>
+        <div className="colors">
+          <button className="r" onClick={()=>this.colorClick("r")}>Red</button>
+          <button className="o" onClick={()=>this.colorClick("o")}>Orange</button>
+          <button className="y" onClick={()=>this.colorClick("y")}>Yellow</button>
+          <button className="g" onClick={()=>this.colorClick("g")}>Green</button>
+          <button className="b" onClick={()=>this.colorClick("b")}>Blue</button>
+          <button className="pu" onClick={()=>this.colorClick("pu")}>Purple</button>
+          <button className="pi" onClick={()=>this.colorClick("pi")}>Pink</button>
+        </div>
       </div>
     );
   }
