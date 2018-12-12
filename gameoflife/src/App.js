c73255f8e7b83766eee6a816c8200e06784a02e5
Rachel DiCesare <racheldicesare@missrachelthedeveloper.com>
2018-12-12 11:18:28 -0500

    MVP Reached

--- gameoflife/src/App.js
+++ gameoflife/src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
      <div className="App">
-        <h1>Conway's Game of Life</h1>
+        <h1>Conway's Game of Life- Rachel DiCesare</h1>
           <Game />
       </div>
     );
