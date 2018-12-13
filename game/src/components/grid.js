92d6a8ed6854b232df847ced4596fccb0ddd6a7d
adamcglee <adamcglee@gmail.com>
2018-12-13 13:25:52 -0800

    got everything working properly and sort of looking ok, could definitely use some more styling love

--- game/src/components/grid.js
+++ game/src/components/grid.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-// import Cell from "./cell";
+import { Modal } from "reactstrap";
 import "./grid.css";
 
 let gen_count = 0;
@@ -14,15 +14,15 @@ class Grid extends Component {
       grid: [],
       grid_empty: true,
       game_running: false,
+      speed: 300
     };
   }
 
   componentDidMount() {
     requestAnimationFrame(this.tick);
-    
+
     //set grid array size and set all values to false
     this.grid_init(this.state.grid_width_cells, this.state.grid_height_cells);
-    
   }
 
   draw_grid() {
@@ -37,12 +37,12 @@ class Grid extends Component {
 
     for (let y = size; y < height; y += size) {
       canvas.moveTo(0, y);
-      canvas.lineTo(height, y);
+      canvas.lineTo(width, y);
     }
 
     for (let x = size; x < width; x += size) {
       canvas.moveTo(x, 0);
-      canvas.lineTo(x, width);
+      canvas.lineTo(x, height);
     }
 
     canvas.strokeStyle = "black";
@@ -51,12 +51,15 @@ class Grid extends Component {
   }
 
   tick = () => {
-    if(this.state.game_running) {
+    if (this.state.game_running) {
       this.grid_update();
       this.grid_update_draw();
-      requestAnimationFrame(this.tick);
-    }        
-    console.log(this.state.game_running);
+      requestAnimationFrame(this.step);
+    }
+  };
+
+  step = () => {
+    setTimeout(this.tick, this.state.speed);
   };
 
   //initialize the grid with the proper height and width and all false values.
@@ -68,14 +71,14 @@ class Grid extends Component {
         grid[y][x] = false;
       }
     }
-    this.setState({ grid });
-    this.draw_grid();
-    console.log(this.state.grid_height_cells, this.state.grid_width_cells);
+    this.setState({ grid }, () => this.draw_grid());
   }
 
   //create a new grid based on the game rules being applied to the current grid, then setting the new grid to the current grid in state.
-  grid_update = () =>{
-    const grid = new Array(this.state.grid_height_cells).fill(0).map(x => new Array(this.state.grid_width_cells).fill(false));
+  grid_update = () => {
+    const grid = new Array(this.state.grid_height_cells)
+      .fill(0)
+      .map(x => new Array(this.state.grid_width_cells).fill(false));
     const neighbors = [
       [-1, -1],
       [-1, 0],
@@ -86,7 +89,6 @@ class Grid extends Component {
       [1, -1],
       [0, -1]
     ];
-    console.log(grid);
     let counter = 0;
     for (let y = 0; y < this.state.grid_height_cells; y++) {
       for (let x = 0; x < this.state.grid_width_cells; x++) {
@@ -103,13 +105,12 @@ class Grid extends Component {
           ) {
             counter++;
           }
-          console.log(y, x, counter);
         }
         if (this.state.grid[y][x]) {
           if (counter === 2 || counter === 3) {
             grid[y][x] = true;
           }
-        }else {
+        } else {
           if (counter === 3) {
             grid[y][x] = true;
           }
@@ -117,10 +118,9 @@ class Grid extends Component {
         counter = 0;
       }
     }
-    this.setState({grid});
+    this.setState({ grid });
     gen_count++;
-    console.log(this.state.grid);
-  }
+  };
 
   get_cursor = event => {
     if (this.props.game_running) {
@@ -140,9 +140,7 @@ class Grid extends Component {
     } else {
       grid[currentsquareY][currentsquareX] = true;
     }
-    this.setState({ grid }, () =>
-      this.grid_update_draw(currentsquareY, currentsquareX, cellSize)
-    );
+    this.setState({ grid }, () => this.grid_update_draw());
   };
 
   grid_update_draw = () => {
@@ -167,13 +165,14 @@ class Grid extends Component {
         }
       }
     }
-  }
+  };
 
   start_game = () => {
     if (!this.state.game_running) {
       this.setState({
         game_running: true
       });
+      gen_count++;
     }
     requestAnimationFrame(this.tick);
   };
@@ -184,7 +183,7 @@ class Grid extends Component {
     });
   };
 
-  grid_reset =() => {
+  grid_reset = () => {
     // this.setState({ game_running: false });
     this.stop_game();
     const grid = this.state.grid;
@@ -196,19 +195,20 @@ class Grid extends Component {
     this.setState({ grid });
     this.grid_update_draw();
     gen_count = 0;
-  }
+  };
 
   next_step = () => {
+    this.stop_game();
     this.grid_update();
     this.grid_update_draw();
     requestAnimationFrame(this.tick);
-  }
+  };
 
   random_board = () => {
     const grid = this.state.grid;
     for (let y = 0; y < this.state.grid_height_cells; y++) {
       for (let x = 0; x < this.state.grid_width_cells; x++) {
-        if((Math.floor(Math.random() * 10) + 1) <= 5) {
+        if (Math.floor(Math.random() * 10) + 1 <= 5) {
           grid[y][x] = false;
         } else {
           grid[y][x] = true;
@@ -217,7 +217,7 @@ class Grid extends Component {
     }
     this.setState({ grid });
     this.grid_update_draw();
-  }
+  };
 
   resize_grid(width, height) {
     const grid = [];
@@ -232,49 +232,125 @@ class Grid extends Component {
   }
 
   handleDimensionChange = event => {
-    this.setState({ [event.target.name]: event.target.value });
+    this.setState({ [event.target.name]: Number(event.target.value) });
   };
 
   render() {
     return (
-      <div className="gridContainer">
-        <div>
-          <h3>Generation: {gen_count}</h3>
+      <div className="game-container">
+        <div className="grid-container">
+          <div className="gen-counter">
+            <h3>Generation: {gen_count}</h3>
+          </div>
+          <div className="control-buttons">
+            <button className="submit" onClick={this.random_board}>
+              Randomize
+            </button>
+            <button className="submit" onClick={this.next_step}>
+              Next
+            </button>
+            <button className="submit" onClick={this.start_game}>
+              Start
+            </button>
+            <button className="submit" onClick={this.stop_game}>
+              Stop
+            </button>
+            <button className="submit" onClick={this.grid_reset}>
+              Clear
+            </button>
+          </div>
+          <div className="canvas">
+            <canvas
+              id="canvas"
+              ref="canvas"
+              width={this.state.grid_width_cells * this.state.cell_size}
+              height={this.state.grid_height_cells * this.state.cell_size}
+              onClick={this.get_cursor}
+            />
+          </div>
+          <div className="grid-change">
+            <h4>Change Grid Dimensions: </h4>
+            Width:
+            <input
+              type="number"
+              min="3"
+              max="40"
+              placeholder="Width"
+              onChange={this.handleDimensionChange}
+              name="grid_width_cells"
+              value={this.state.grid_width_cells}
+            />
+            Height:
+            <input
+              type="number"
+              min="3"
+              max="40"
+              placeholder="Height"
+              onChange={this.handleDimensionChange}
+              name="grid_height_cells"
+              value={this.state.grid_height_cells}
+            />
+            <button
+              className="submit"
+              onClick={() =>
+                this.grid_init(
+                  this.state.grid_width_cells,
+                  this.state.grid_height_cells
+                )
+              }
+            >
+              Set
+            </button>
+          </div>
         </div>
-        <div className="headerButtons">
-          <button className="submit" onClick={this.random_board}>
-            Randomize
-          </button>
-          <button className="submit" onClick={this.next_step}>
-            Next
-          </button>
-          <button className="submit" onClick={this.start_game}>
-            Start
-          </button>
-          <button className="submit" onClick={this.stop_game}>
-            Stop
-          </button>
-          <button className="submit" onClick={this.grid_reset}>
-            Clear
-          </button>
+        <div className="extras-container">
+          <div className="rules">
+            <h3>Game Rules</h3>
+            <div className="rules-body">
+              <p>
+                If the cell is alive **and** has 2 or 3 neighbors, then it
+                remains alive. Else it dies.
+              </p>
+              <p>
+                If the cell is dead **and** has exactly 3 neighbors, then it
+                comes to life. Else if remains dead.
+              </p>
+            </div>
+          </div>
+          <div className="about">
+            <h3>About Conway's Game of Life</h3>
+            <div className="about-body">
+              <p>
+                In this learning project we explore Conway's Game of Life. The
+                game involves an (infinite) two-dimensional grid with black
+                and white squares, which may be represented as 1 and 0. One
+                may think of them as "live cells" or "dead cells". The grid
+                evolves. The evolution rule is as follows:
+              </p>
+              <p>All cells evolve simultaneously</p>
+              <p>Each cell has eight neighbours</p>
+              <p>
+                An alive cell with two or three neighbours continues to live.
+                Otherwise it dies.
+              </p>
+              <p>
+                A dead cell with exactly three neighbours will become alive.
+              </p>
+              <p>
+                From these simple forms it is possible to create stable and
+                recursive patterns, such as the Gosper Glider Gun
+                (illustrated).
+              </p>
+              <p>
+                The Game of Life is a prototypical example of a cellular
+                automaton, an automatic machine of cells. It has attracted the
+                interest of researchers in diverse fields. Patterns in
+                Conway's Game of Life have been shown to be capable of
+                emulating a universal Turing machine.
+              </p>
+            </div>
+          </div>
         </div>
-        <div>
-          <canvas
-            id="canvas"
-            ref="canvas"
-            width={this.state.grid_width_cells * this.state.cell_size}
-            height={this.state.grid_height_cells * this.state.cell_size}
-            onClick={this.get_cursor}
-          />
-        </div>        
-          <h4>Change Grid Dimensions: </h4>
-          Width:
-          <input type="number" min="3" max="40"placeholder="Width" onChange={this.handleDimensionChange} name="grid_width_cells" value={this.state.grid_width_cells}/>
-          Height:
-          <input type="number" min="3" max="40"placeholder="Height" onChange={this.handleDimensionChange} name="grid_height_cells" value={this.state.grid_height_cells}/> 
-          <button className="submit" onClick={() => this.grid_init(this.state.grid_width_cells,this.state.grid_height_cells)}>
-            Set
-          </button>
       </div>
     );
   }
