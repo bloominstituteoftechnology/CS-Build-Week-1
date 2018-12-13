b25fe5de1f2a6ca440174ab2400b36622d01ccac
Erik Kimsey <erik@erikkimsey.com>
2018-12-13 00:07:35 -0500

    Created logo and added it to hheader w some style

--- gol/src/Header/Header.js
+++ gol/src/Header/Header.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './header.scss';
+import GOL from '../GoL_googly.svg';
 
 const Header = (props) => {
 
 
   return (
-    <div className="header-container">Header</div>
+    <div className="header-container">
+      <img src={require("../gol.svg")} />
+    </div>
   )
 };
 
