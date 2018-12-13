9bf05670405c8374ed2f0443fb82ee3e2c8bda8f
Erik Kimsey <erik@erikkimsey.com>
2018-12-13 09:24:35 -0500

    Added rules icon and styled it

--- gol/src/Rules/Rules.js
+++ gol/src/Rules/Rules.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
 import './rules.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faInfo } from '@fortawesome/free-solid-svg-icons'
 
 const Rules = (props) => {
+
+  window.setTimeout(()=>{
+    let hdr = document.querySelector('.rules-container');
+    hdr.classList.add('show-container')
+  }, 100);
+
   return (
-    <div className="rules-container">Rules</div>
+    <div className="rules-container" onClick={()=>{}}>
+      <a href="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life" target="_blank" title="Learn more on mathetmatician John Conway's 'Game of Life'">
+        <FontAwesomeIcon icon={faInfo} />
+      </a>
+    </div>
   )
 };
 
