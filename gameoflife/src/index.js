2752fd5ee2a0761ccf97e1420ecb54780dd3d0b4
Arthur Bates <aabatesj@gmail.com>
2018-12-13 06:33:58 -0500

    Update index.js
    play method implemented with game rules.
    
    Grid updates with new generation

--- gameoflife/src/index.js
+++ gameoflife/src/index.js
@@ -48,6 +48,10 @@ class Grid extends React.Component {
     }
 }
 
+function arrayClone(arry) {
+    arry.slice(0);
+}
+
 class Main extends React.Component {
     constructor() {
         super();
@@ -85,11 +89,41 @@ class Main extends React.Component {
 
     playButton = () => {
         clearInterval(this.intervalId)
-        this.intervalId = setInterval(this.playButton, this, props.speed);
+        this.intervalId = setInterval(this.playButton, this.speed);
+    }
+
+    pauseButton = () => {
+        clearInterval(this.intervalId);
+    }
+
+    play = () => {
+        let g = this.state.gridFull;
+        let g2 = arrayClone(this.state.gridFull);
+
+        for (let i = 0; i < this.rows; i++) {
+            for (let j = 0; j < this.cols; j++) {
+                let count = 0;
+                if (i > 0) if (g[i - 1][j]) count++;
+                if (i > 0 && j > 0) if (g[i - 1][j - 1]) count++;
+                if (i > 0 && j < this.cols - 1) if (g[i - 1][j + 1]) count++;
+                if (j < this.cols - 1) if (g[i][j + 1]) count++;
+                if (j > 0) if (g[i][j - 1]) count++;
+                if (i < this.rows - 1) if (g[i + 1][j]) count++;
+                if (i < this.rows - 1 && j > 0) if (g[i + 1][j - 1]) count++;
+                if (i < this.rows - 1 && this.cols - 1) if (g[i + 1][j + 1]) count++;
+                if (g[i][j] && (count < 2 || count > 3)) g2[i][j] = false;
+                if (!g[i][j] && count === 3) g2[i][j] = true;
+            }
+        }
+        this.setState({
+            gridFull: g2,
+            generation: this.state.generation + 1
+        });
     }
 
     componentDidMount() {
         this.seed();
+        this.playButton();
     }
 
     render() {
