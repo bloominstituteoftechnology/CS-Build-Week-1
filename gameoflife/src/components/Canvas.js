6bd3525cd9cdf353a06fe71f29854c097af6e0da
MichaelBoegner <cmikeb@gmail.com>
2018-12-13 00:24:17 -0500

    deployed on github pages. css mod

--- gameoflife/src/components/Canvas.js
+++ gameoflife/src/components/Canvas.js
@@ -105,12 +105,19 @@ export default class Canvas extends Component {
 
   onClickStart = () => {
     this.setState({ start: 1 });
-    let speed = this.state.speed;
+    this.timer();
+  }
 
-    setInterval(() => {
-      this.startFunction();
-    }, speed);
-  };
+  timer = () => {
+    let timer = setInterval( () => {
+      if(this.state.start === 0) {
+        return clearInterval(timer); 
+      } else if(this.state.start === 1) {
+          this.startFunction(this.state.start);
+          }
+      }, this.state.speed);
+  }
+  
 
   onClickStop = () => {
     this.setState({ start: 0 });
@@ -479,12 +486,14 @@ export default class Canvas extends Component {
         <div className="about">
             <h2>About this Algorithm</h2>
             <p>A bunch of stuff about the game of life. A bunch of stuff about the game of life. A bunch of stuff about the game of life. A bunch of stuff about the game of life. A bunch of stuff about the game of life. </p>
+            <p>To save a preset, color the cells you want and click preset, which will assign to the next free preset slot. If no presets are free, your preset will not be saved.</p>
         </div>
         <div>
             <h2>Rules:</h2>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
+            <li>Any live cell with fewer than two live neighbors dies, as if by underpopulation.</li>
+            <li>Any live cell with two or three live neighbors lives on to the next generation.</li>
+            <li>Any live cell with more than three live neighbors dies, as if by overpopulation.</li>
+            <li>Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.</li>
         </div>
       </div>
     );
