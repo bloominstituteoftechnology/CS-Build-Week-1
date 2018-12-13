863437c62d579b0d9cb556d113f5e476f14007c3
Erik Kimsey <erik@erikkimsey.com>
2018-12-13 14:34:06 -0500

    Instructions created and styled

--- src/Instructions/Instructions.js
+++ src/Instructions/Instructions.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './instructions.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faInfo } from '@fortawesome/free-solid-svg-icons'
+import { faInfo, faTimes } from '@fortawesome/free-solid-svg-icons'
 
 
 
@@ -12,16 +12,22 @@ const Instructions = (props) => {
     hdr.classList.add('show-container')
   }, 100);
 
+  const closeInstructs = () => {
+    let modal = document.querySelector('.instructions-container');
+    modal.style.display = "none";
+  }
+
   return (
     <div className="instructions-container">
-      <div>Playing with lives.</div>
-      <div>Conway's "Game of Life" is a simulation created by a mathematician named John Conway.  The simulation is to display, in real-time, the ebb and flow of life and death. "Cells" represent living or dead entities.  And those entities live or die accord to the number of "neighbors they have.  In this instance of the <i>Game of Life</i>, I cell dies if:</div>
-      <ul>
-        <li>a cell's qty of neighbors is <i>&gt;3</i> or <i>&lt;two</i></li>
-      </ul>
-      <ol>
-        <li></li>
-      </ol>
+      <div className="instr-header">
+        <h2>Playing with lives.</h2>
+        <FontAwesomeIcon className="exit" icon={faTimes} onClick={()=>{closeInstructs()}}/>
+      </div>
+      <div>Conway's "Game of Life" is a simulation created by a mathematician named John Conway.  The simulation is to display, in real-time, the ebb and flow of life and death. "Cells" represent living or dead entities.  And those entities live or die accord to the number of "neighbors" they have.  In this instance of the <i>Game of Life</i>, a cell dies if a cell's qty of neighbors is <i>&gt;3</i> or <i>&lt;2</i>  In the inverse of that qty, a cell remains alive or comes to life.</div>
+      <br></br>
+      <div>
+        So, click on this grid, bringing some cells to life, and hit play.  <i>Or,</i> click the "randomize" button to create a grid of life automatically.
+      </div>
     </div>
   )
 };
