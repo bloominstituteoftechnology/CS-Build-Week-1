c033016d3d35da8b95bc425b15a37072175307d6
mkerbleski <mkerbles@gmail.com>
2018-12-13 10:01:34 -0700

    added more styling.  restricted clicking while timer is running. added about paragraphs

--- life/src/App.js
+++ life/src/App.js
@@ -30,10 +30,15 @@ class App extends Component {
             color={this.state.color}
             onChangeComplete={ this.selectColor}/>
         </div>
-        <div className="instructions">
+        <div className="rules">
+            <h4>Rules</h4>
             <p>Click to toggle squares. Press 'next' to advance one frame at at time or hit start to progress automatically</p>
             <p>If the cell is alive **and** has 2 or 3 neighbors, then it remains alive. Else it dies.</p>
             <p>If the cell is dead **and** has exactly 3 neighbors, then it comes to life. Else if remains dead.</p>
+        </div>
+        <div className="rules">
+            <h4>About this Algorithm</h4>
+            <p>The Game of Life, also known simply as Life, is a cellular automaton devised by the British mathematician John Horton Conway in 1970. The game is a zero-player game, meaning that its evolution is determined by its initial state, requiring no further input. One interacts with the Game of Life by creating an initial configuration and observing how it evolves, or, for advanced players, by creating patterns with particular properties.</p>From: <a href="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life">wikipedia</a>
         </div>
       </AppDiv>
     );
@@ -54,9 +59,13 @@ const AppDiv = styled.div`
     border: 1px solid blue;
     display: flex;
     flex-direction: row;
-    padding: 10px;
+    justify-content: space-around;
+    width: 80%;
+    padding: 15px;
   }
-  .instructions{
+  .rules{
     border: 1px solid green;
+    width: 80%;
+    padding: 15px;
   }
 `;
