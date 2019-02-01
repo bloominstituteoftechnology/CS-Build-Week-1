9e8680269360e2a2f6d2f00da68145decbeae7a8
mkerbleski <mkerbles@gmail.com>
2019-02-01 10:43:41 -0700

    moved toml file to root

---
+++ life/build/precache-manifest.cdcab2595c2c3f5c8e682d720ab6469c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7d533ab09956ceb10fb",
+    "url": "/Conways-Life/static/js/runtime~main.a7d533ab.js"
+  },
+  {
+    "revision": "7eb39bf0136a43fe91f5",
+    "url": "/Conways-Life/static/js/main.7eb39bf0.chunk.js"
+  },
+  {
+    "revision": "961a4f427a449b4e8161",
+    "url": "/Conways-Life/static/js/1.961a4f42.chunk.js"
+  },
+  {
+    "revision": "7eb39bf0136a43fe91f5",
+    "url": "/Conways-Life/static/css/main.a3eece9f.chunk.css"
+  },
+  {
+    "revision": "7267df73db393928a7a7f867b9548491",
+    "url": "/Conways-Life/index.html"
+  }
+];
