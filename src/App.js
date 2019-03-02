ec00c996d74c6260ee8866d54e38568f6336b0bc
francistse23 <wtse.ucsd@gmail.com>
2019-03-01 17:42:42 -0800

    Added react router. removed homepage reference in package.json

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import { Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import './App.css';
 import LifeCanvas from './components/LifeCanvas';
 
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <LifeCanvas />
+        <Route exact path="/" component={LifeCanvas} />
       </div>
     );
   }
