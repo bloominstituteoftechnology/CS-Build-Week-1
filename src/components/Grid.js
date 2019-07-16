a2069f9f91c8818fd8efbad0946c2c6c3b4f0321
Christopher Foster <cjfoster@gmail.com>
2019-07-16 07:25:27 -0400

    Grid display now responds properly to grid dimensions.

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,14 +1,36 @@
 import React from 'react';
+import makeStyles from '@material-ui/styles/makeStyles';
+// import useTheme from '@material-ui/styles/useTheme';
 
 import Cell from './Cell';
 
+const useStyles = makeStyles((theme, gridX, gridY) => ({
+	root: {
+		width: '100%',
+		height: '100%'
+	},
+	grid: {
+		display: 'grid'
+		// gridTemplateColumns: `repeat(${gridX}, 40px)`,
+		// gridTemplateRows: 'repeat(gridY, 40px)'
+	}
+}));
+
 export default function Grid({ cellData, gridX, gridY }) {
-  console.log(cellData);
-  return (
-    <div>
-      {cellData.map((cell, index) => {
-        return <Cell key={cell.index} status={cell} />;
-      })}
-    </div>
-  );
+	const classes = useStyles(gridX, gridY);
+	// const theme = useTheme();
+
+	console.log(cellData);
+	return (
+		<div
+			className={classes.grid}
+			style={{
+				gridTemplate: `repeat(${gridY}, 40px) / repeat(${gridX}, 40px)`
+			}}
+		>
+			{cellData.map((cell, index) => {
+				return <Cell key={cell.index} status={cell} />;
+			})}
+		</div>
+	);
 }
