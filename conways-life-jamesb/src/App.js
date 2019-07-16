bc32a012b74e193b6440b7d8f23e01c7835bdcc9
James Basile <jbasile145@gmail.com>
2019-07-16 15:20:28 -0400

    create react app

---
+++ conways-life-jamesb/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>JAMES BASILE: Conway's Life</h1>
+        <p>
+          Cellular Automata
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
