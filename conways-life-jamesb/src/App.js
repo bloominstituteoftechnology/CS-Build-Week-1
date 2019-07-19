c5797f7ba5ae29b143834d7b7f4637111d4ecb9d
James Basile <jbasile145@gmail.com>
2019-07-18 22:30:04 -0400

    counting neighbors complete

--- conways-life-jamesb/src/App.js
+++ conways-life-jamesb/src/App.js
@@ -6,120 +6,224 @@ import ControlBar from '../src/components/ControlBar/ControlBar'
 
 
 class App extends React.Component {
-    // on load, state should be a 15x15 grid with all cells set to dead
-    state = {
-        currentGrid: [],
-        gridDimensions: 15,
-        generation: 0
-    };
+  // on load, state should be a 15x15 grid with all cells set to dead
+  state = {
+    currentGrid: [],
+    gridDimensions: 15,
+    generation: 0
+  };
 
-    createGrid = (newGridSize) => {
-      let newGrid = []
-      // create a (x-value) by (y-value) square grid using a nested loop and the new grid size,
-          // store x-val, y-val, and isAlive(boolean) for each cell
-      for (let x = 0; x < newGridSize; x++) {
-          let rowOfCells = []
-          // once a row of cells is created at the desired length, push it into the currentGrid array to be stored in state
-          for (let y = 0; y < newGridSize; y++) {
-              rowOfCells.push({
-                  xVal: y,
-                  yVal: x,
-                  isAlive: false,
-              });
-          }
-          // push the full row of cells into the allCells array
-          newGrid.push(rowOfCells);
-      }
-      // set the currentGrid state equal to the cell rows array, containing all rows, x/y vals, and isAlive
-      this.setState({
-          currentGrid: newGrid
-      })
-    }
-//createGrid^^^^^^^^------------------------------------------------------------------------------------------------
-
-    setNewDimensions = (newGridDim) => {
-        this.setState({
-          gridDimensions: newGridDim
+  createGrid = (newGridSize) => {
+    let newGrid = []
+    // create a (x-value) by (y-value) square grid using a nested loop and the new grid size,
+    // store x-val, y-val, and isAlive(boolean) for each cell
+    for (let x = 0; x < newGridSize; x++) {
+      let rowOfCells = []
+      // once a row of cells is created at the desired length, push it into the currentGrid array to be stored in state
+      for (let y = 0; y < newGridSize; y++) {
+        rowOfCells.push({
+          // x and y swapped so rows are stored rather than columns
+          xVal: y,
+          yVal: x,
+          isAlive: false,
         });
-        this.createGrid(newGridDim)
-    }
-//setNewDimensions^^^^^^-------------------------------------------------------------------------------------------
-
-// on load, create a new grid with default 15x15
-    componentDidMount() {
-        this.setNewDimensions(15)
-    };
-
-    componentDidUpdate() {
-      console.log('Current Grid:', this.state.currentGrid)
-      console.log('Grid Dimensions:', this.state.gridDimensions)
-    }
-
-    randomizeGrid = () => {
-      let newGrid = []
-      for (let x = 0; x < this.state.gridDimensions; x++) {
-          let rowOfCells = []
-          for (let y = 0; y < this.state.gridDimensions; y++) {
-            const random = !!Math.round(Math.random());
-              rowOfCells.push({
-                  xVal: y,
-                  yVal: x,
-                  isAlive: random,
-              });
-          }
-          newGrid.push(rowOfCells);
       }
-      this.setState({
-          currentGrid: newGrid
-      })
+      // push the full row of cells into the allCells array
+      newGrid.push(rowOfCells);
     }
+    // set the currentGrid state equal to the cell rows array, containing all rows, x/y vals, and isAlive
+    this.setState({
+      currentGrid: newGrid
+    })
+  }
+  //createGrid^^^^^^^^------------------------------------------------------------------------------------------------
 
-    clearGrid = () => {
-      let newGrid = []
-      for (let x = 0; x < this.state.gridDimensions; x++) {
-          let rowOfCells = []
-          for (let y = 0; y < this.state.gridDimensions; y++) {
-              rowOfCells.push({
-                  xVal: y,
-                  yVal: x,
-                  isAlive: false,
-              });
-          }
-          newGrid.push(rowOfCells);
+  setNewDimensions = (newGridDim) => {
+    this.setState({
+      gridDimensions: newGridDim
+    });
+    this.createGrid(newGridDim)
+  }
+  //setNewDimensions^^^^^^-------------------------------------------------------------------------------------------
+
+  // on load, create a new grid with default 15x15
+  componentDidMount() {
+    this.setNewDimensions(15)
+  };
+
+  // componentDidMount/Update^^^^^^^------------------------------------------------------------------------------------------
+
+  randomizeGrid = () => {
+    let newGrid = []
+    for (let x = 0; x < this.state.gridDimensions; x++) {
+      let rowOfCells = []
+      for (let y = 0; y < this.state.gridDimensions; y++) {
+        const random = !!Math.round(Math.random());
+        rowOfCells.push({
+          xVal: y,
+          yVal: x,
+          isAlive: random,
+        });
       }
-      this.setState({
-          currentGrid: newGrid
-      })
+      newGrid.push(rowOfCells);
+    }
+    this.setState({
+      currentGrid: newGrid
+    })
+  }
+  // randomize^^^^^^^------------------------------------------------------------------------------------------
+  clearGrid = () => {
+    let newGrid = []
+    for (let x = 0; x < this.state.gridDimensions; x++) {
+      let rowOfCells = []
+      for (let y = 0; y < this.state.gridDimensions; y++) {
+        rowOfCells.push({
+          xVal: y,
+          yVal: x,
+          isAlive: false,
+        });
+      }
+      newGrid.push(rowOfCells);
+    }
+    this.setState({
+      currentGrid: newGrid
+    })
+  }
+  // clear grid^^^^^^^------------------------------------------------------------------------------------------
+
+  toggleCell = cell => {
+    let curGrid = this.state.currentGrid;
+
+    curGrid[cell.yVal][cell.xVal].isAlive = !cell.isAlive
+    this.setState({
+      currentGrid: curGrid
+    });
+  };
+  // toggle dead/alive^^^^^^^------------------------------------------------------------------------------------------
+
+
+  gameAlgo = () => {
+    //start from neighbor on top and work clockwise around each cell
+    //first count up the neighbors
+    let nextGenGrid = this.state.currentGrid;
+
+    const size = this.state.gridDimensions;
+    const curGrid = this.state.currentGrid;
+    // for (let x = 0; x < this.state.gridDimensions; x++) {
+    //   let gridRow = [];
+    //   for (let y = 0; y < this.state.gridDimensions; y++) {
+    //     gridRow.push({...this.state.currentGrid[x][y]})
+    //   }
+    //   nextGenGrid.push(gridRow)
+    // }
+
+
+    //this loops down each column, starting at the top left, first if will be right, then move clockwise
+    //the first if statement is to avoid errors for when cell does not have a neighbor to a specific direction
+    for (let x = 0; x < size; x++) {
+      for (let y = 0; y < size; y++) {
+        let liveNeighbors = 0;
+
+
+        //right neighbor
+        if (x < size - 1) {
+          if (curGrid[y][x + 1].isAlive) {
+            liveNeighbors++
+            console.log('right works!!!!', liveNeighbors, x, y)
+          }
+        }
+
+        //bottom-right neighbor
+        if (y < size - 1 && x < size - 1) {
+          if (curGrid[y + 1][x + 1].isAlive) {
+            liveNeighbors++
+            console.log('bottom-right works!!!!', liveNeighbors, '     X,Y:', x, y)
+          }
+        }
+
+        //bottom neighbor
+        if (y < size - 1) {
+          if (curGrid[y + 1][x].isAlive) {
+            liveNeighbors++
+            console.log('bottom works!!!!', liveNeighbors, '     X,Y:', x, y)
+          }
+        }
+
+        //bottom-left
+        if (y < size - 1 && x > 0) {
+          if (curGrid[y + 1][x - 1].isAlive) {
+            liveNeighbors++
+            console.log('bottom-left works!!!!', liveNeighbors, '     X,Y:', x, y)
+          }
+        }
+
+        //left
+        if (x > 0) {
+          if (curGrid[y][x - 1].isAlive) {
+            liveNeighbors++
+            console.log('left works!!!!', liveNeighbors, '     X,Y:', x, y)
+          }
+        }
+
+        //top-left
+        if(x > 0 && y > 0) {
+          if(curGrid[y-1][x-1].isAlive) {
+            liveNeighbors++
+            console.log('top-left works!!!!', liveNeighbors, '     X,Y:', x, y)
+
+          }
+
+        }
+
+        // above neighbor
+        if (y > 0) {
+          if (curGrid[y - 1][x].isAlive) {
+            liveNeighbors++
+            console.log('top works!!!!', liveNeighbors, '     X,Y:', x, y)
+          }
+        }
+
+        // top right neighbor
+        if (y > 0 && x < size - 1) {
+          if (curGrid[y - 1][x + 1].isAlive) {
+            liveNeighbors++
+            console.log('top-right works!!!!', liveNeighbors, '     X,Y:', x, y)
+          }
+        }
+      }
     }
 
-    toggleCell = cell => {
-      let curGrid = this.state.currentGrid;
-
-      curGrid[cell.yVal][cell.xVal].isAlive = !cell.isAlive
-      this.setState({
-        currentGrid: curGrid
-      });
-    };
 
 
-    render() {
-        return (
-            <div className="App">
-                <p>James Basile: Conway's Game of Life</p>
-                <p>Generation: {this.state.generation}</p>
-                <Grid
-                currentGrid={this.state.currentGrid}
-                size={this.state.gridDimensions}
-                toggleCell={this.toggleCell}
-                />
-                <ControlBar
-                gridReset={this.setNewDimensions}
-                randomizeGrid={this.randomizeGrid}
-                clearGrid={this.clearGrid}
-                />
-            </div>
-        )
-    }
+
+    // apply life/death rules based on neighbor count
+
+    // increse generation by 1
+    this.setState({
+      generation: this.state.generation + 1,
+    })
+  }
+  //Game algo^^^-----------------------------------------------------------------------------------------------
+
+  render() {
+    return (
+      <div className="App">
+        <p>James Basile: Conway's Game of Life</p>
+        <p>Generation: {this.state.generation}</p>
+        <Grid
+          currentGrid={this.state.currentGrid}
+          size={this.state.gridDimensions}
+          toggleCell={this.toggleCell}
+        />
+        <ControlBar
+          gridReset={this.setNewDimensions}
+          randomizeGrid={this.randomizeGrid}
+          clearGrid={this.clearGrid}
+          nextGen={this.gameAlgo}
+        />
+      </div>
+    )
+  }
 }
 
 
--- conways-life-jamesb/src/components/ControlBar/ControlBar.js
+++ conways-life-jamesb/src/components/ControlBar/ControlBar.js
@@ -3,6 +3,7 @@ import React from 'react';
 export default function Grid(props) {
     return (
         <>
+            <button onClick={() => props.nextGen()}>Start Game</button>
             <button onClick={() => props.gridReset(15)}>15x15</button>
             <button onClick={() => props.gridReset(30)}>30x30</button>
             <button onClick={() => props.gridReset(45)}>45x45</button>
