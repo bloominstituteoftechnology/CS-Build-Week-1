a60340fae55b222e5316aec2e33bba9a8d383cde
Chad Rutherford <chad_rutherford@outlook.com>
2020-05-27 20:51:26 -0400

    Added UIView (GameView) to project

--- GameOfLife/GameOfLife/Controller/GameOfLifeViewController.swift
+++ GameOfLife/GameOfLife/Controller/GameOfLifeViewController.swift
@@ -10,8 +10,36 @@ import SwiftUI
 import UIKit
 
 class GameOfLifeViewController: UIViewController {
+	
+	let gameView: UIView = {
+		let view = UIView()
+		view.translatesAutoresizingMaskIntoConstraints = false
+		view.backgroundColor = .label
+		return view
+	}()
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		setupUI()
+		printCoords()
+	}
+	
+	private func setupUI() {
+		view.backgroundColor = .systemBackground
+		view.addSubview(gameView)
+		NSLayoutConstraint.activate([
+			gameView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+			gameView.widthAnchor.constraint(equalToConstant: 300),
+			gameView.heightAnchor.constraint(equalTo: gameView.widthAnchor, multiplier: 1),
+			gameView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+		])
+	}
+	
+	private func printCoords() {
+		guard isViewLoaded else { return }
+		print(gameView.center)
+		print(gameView.bounds.size.width)
+		print(gameView.bounds.size.height)
 	}
 }
 
@@ -20,5 +48,6 @@ struct GameOfLifePreview: PreviewProvider {
 	static var previews: some View {
 		GameOfLifeViewController().asPreview()
 			.edgesIgnoringSafeArea(.all)
+			.colorScheme(.dark)
 	}
 }
