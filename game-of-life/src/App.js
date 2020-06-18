13e9e91783312134c0a73cc316d60a5384ba736a
Tevvin <Tevvin7395@icloud.com>
2020-06-17 22:19:11 -0400

    About componet added

--- game-of-life/src/App.js
+++ game-of-life/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import About from './components/about/about.js';
 
 function App() {
   return (
     <div className="App">
       <h1 className="appname">"Game Of Life"</h1>
+      <About />
     </div>
   );
 }
---
+++ game-of-life/src/components/about/about.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export class About extends Component {
+    render() {
+        return (
+            <div className="about-section">
+                <h3>About this Algorithm:</h3>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the 
+                industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and 
+                scrambled it to make a type specimen book.</p>
+            </div>
+        )
+    }
+}
+
+export default About
+
