c75f09fbc67b5b40706ce783ed102b1422fc698c
Hector <hectoremil21@hotmail.com>
2020-06-20 18:39:04 -0400

    Implement run_game method

--- game.py
+++ game.py
@@ -4,3 +4,16 @@ class Game:
         self.rules = rules
         self.max_size = max_size
 
+    def run_game(self, it):
+        state = self.initial_state
+        previous_state = None
+        progression = []
+        i = 0
+        while (not state.equals(previous_state) and i < it):
+            i += 1
+            previous_state = state.copy()
+            progression.append(previous_state.grid)
+            state = state.apply_rules(self.rules, self.max_size)
+
+        progression.append(state.grid)
+        return progression
