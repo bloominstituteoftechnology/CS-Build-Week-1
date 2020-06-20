0e64ebd828ef1ae33abb461755c210351934c99f
Hector <hectoremil21@hotmail.com>
2020-06-20 19:09:03 -0400

    Implement sparse_set_rules.py methods

---
+++ sparse_set_rules.py
@@ -0,0 +1,20 @@
+class SparseSetRules(Rule):
+    def apply_rules(self,  grid, max_size, get_neighbours):
+        counter = {}
+        for elem in grid:
+            if elem not in counter:
+                counter[elem] = 0
+            nb = get_neighbours(elem, max_size)
+            for n in nb:
+                if n not in counter:
+                    counter[n] = 1
+                else:
+                    counter[n] += 1
+
+        for c in counter:
+            if(counter[c] < 2 or counter[c] > 3):
+                grid.discard(c)
+            if counter[c] == 3:
+                grid.add(c)
+
+        return grid
