947fba12b74f32676f189361c1500682307d0961
Hector <hectoremil21@hotmail.com>
2020-06-20 20:14:46 -0400

    implement minimal pygame program that creates a window

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
-import IPython.display
-from io import BytesIO as bio
-import PIL.Image
-import matplotlib.pyplot as plt
-import matplotlib
-import time
-from sparse_set_rules import SparseSetRules
-from sparse_set_state import SparseSetState
-from game import Game
-matplotlib.use('GTK3Agg')
+import pygame
 
-MAX_ITER = 1500
-MAX_SIZE = 80
-board = {(39, 40), (39, 41), (40, 39), (40, 40), (41,  40)}
-rules = SparseSetRules()
-game = Game(SparseSetState(board), rules, MAX_SIZE)
-t = time.time()
-rw = game.run_game(MAX_ITER)
-print(time.time() - t)
+def main():
+
+    pygame.init()
+
+    pygame.display.set_caption("minimal program")
+
+    screen = pygame.display.set_mode((1080, 720))
+
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+
+if __name__ == "__main__":
+    main()
--- rule.py
+++
@@ -1,6 +0,0 @@
-from abc import ABC, abstractmethod
-
-class Rule(ABC):
-    @abstractmethod
-    def apply_rules(self, grid, max_size, get_neighbours):
-        pass
