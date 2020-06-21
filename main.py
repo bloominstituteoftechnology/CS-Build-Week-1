57ce659447ed8d7e416a0cf2ccdacf402a118109
Hector <hectoremil21@hotmail.com>
2020-06-20 20:26:57 -0400

    Create second window when first window is closed.

--- main.py
+++ main.py
@@ -14,9 +14,27 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+                second_window()
             if event.type == pygame.MOUSEBUTTONUP:
                 print(pygame.mouse.get_pos())
 
+
+def second_window():
+    pygame.init()
+
+    pygame.display.set_caption("minimal program")
+
+    screen = pygame.display.set_mode((300, 300))
+
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEBUTTONUP:
+                print("Second window says:")
+                print(pygame.mouse.get_pos())
 
 if __name__ == "__main__":
     main()
