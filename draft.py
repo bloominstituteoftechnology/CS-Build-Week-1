862c1191fe99de0fafb0aa6167b679cb73d08a1a
Hector <hectoremil21@hotmail.com>
2020-06-21 21:50:35 -0400

    Fix positioning of board printing.

--- draft.py
+++ draft.py
@@ -1,4 +1,7 @@
 import pygame
+import copy
+import time
+from os import system
 
 def find_neighbors(i, j, arr):
     neighbors = []
@@ -38,11 +41,41 @@ def find_neighbors(i, j, arr):
 
 
 def main():
-    l = [ [0] * 10 for i in range(10) ]
+    max = 10
+    l = [ [0] * 10 for i in range(max) ]
     # print(l)
-    l[4][4] = 1
+    # l[4][4] = 1
+    # l[4][3] = 1
+    # l[4][5] = 1
 
-    print(len(l))
+    #glider pattern
+    l[4][4] = 1
+    l[3][4] = 1
+    l[2][4] = 1
+    l[2][5] = 1
+    l[3][6] = 1
+
+    new_arr = copy.deepcopy(l)
+
+    while True:
+        _ = system('cls')
+        for m in new_arr:
+            for o in m:
+                print(o, end='\t')
+            print()
+
+        for y in range(max):
+            for x in range(max):
+                new_total = find_neighbors(y, x, l)
+                if l[y][x] == 1:
+                    if new_total < 2 or new_total > 3:
+                        new_arr[y][x] = 0
+                else:
+                    if new_total == 3:
+                        new_arr[y][x] = 1
+
+        l = copy.deepcopy(new_arr)
+        time.sleep(1)
 
 if __name__ == '__main__':
     main()
