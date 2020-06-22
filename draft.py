f95df3e1b4f017450ade1b351f6735b43d5d1418
Hector <hectoremil21@hotmail.com>
2020-06-21 20:40:22 -0400

    First pass implementation of find_neighbors() method

--- draft.py
+++ draft.py
@@ -1,12 +1,39 @@
 import pygame
-import copy
 
 def find_neighbors(i, j, arr):
     neighbors = []
+    MAX = len(arr)
+    total = 0
 
     #Find top left
-    if arr[i-1][j-1]:
-    return neighbors
+    if i - 1 >= 0 and j - 1 >= 0:
+        neighbors.append(arr[i-1][j-1])
+    #Find top middle
+    if i - 1 >= 0:
+        neighbors.append(arr[i-1][j])
+    #Find top right
+    if i - 1 >= 0 and j + 1 < MAX:
+        neighbors.append(arr[i-1][j+1])
+    #Find left
+    if j - 1 >= 0:
+        neighbors.append(arr[i][j-1])
+    #Find right
+    if j + 1 < MAX:
+        neighbors.append(arr[i][j+1])
+    #Find bottom left
+    if i + 1 < MAX and j - 1 >= 0:
+        neighbors.append(arr[i+1][j-1])
+    #Find bottom middle
+    if i + 1 < MAX:
+        neighbors.append(arr[i+1][j])
+    #Find bottom right
+    if i + 1 < MAX and j + 1 < MAX:
+        neighbors.append(arr[i+1][j+1])
+
+    for num in neighbors:
+        total += num
+
+    return total
 
 
 
@@ -15,8 +42,7 @@ def main():
     # print(l)
     l[4][4] = 1
 
-    find_neighbors(0, 1, l)
-
+    print(len(l))
 
 if __name__ == '__main__':
     main()
