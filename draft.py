41cbbbfe0eb1d876ababa5d1f3cb8d57fe682ac4
Hector <hectoremil21@hotmail.com>
2020-06-21 20:24:42 -0400

    Working on find_neighbors() method

--- draft.py
+++ draft.py
@@ -1,8 +1,22 @@
 import pygame
+import copy
 
-l = [ [0] * 10 for i in range(10) ]
-# print(l)
-l[4][4] = 1
+def find_neighbors(i, j, arr):
+    neighbors = []
 
-for a in l:
-    print(a)
+    #Find top left
+    if arr[i-1][j-1]:
+    return neighbors
+
+
+
+def main():
+    l = [ [0] * 10 for i in range(10) ]
+    # print(l)
+    l[4][4] = 1
+
+    find_neighbors(0, 1, l)
+
+
+if __name__ == '__main__':
+    main()
