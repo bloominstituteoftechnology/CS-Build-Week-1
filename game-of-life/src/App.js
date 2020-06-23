95598d7c8de588bb42615c29098b2e2b47ca833d
Tanner Williams <tannerwill756@gmail.com>
2020-06-23 12:42:55 -0400

    added rules and more color features

--- game-of-life/src/App.js
+++ game-of-life/src/App.js
@@ -3,6 +3,7 @@ import produce from "immer";
 import DropdownButton from "react-bootstrap/DropdownButton";
 import Dropdown from "react-bootstrap/Dropdown";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
 
 const numRows = 25;
 const numCols = 25;
@@ -34,7 +35,7 @@ const App = () => {
 
   const [running, setRunning] = useState(false);
 
-  const [bgColor, setBgColor] = useState("orange");
+  const [bgColor, setBgColor] = useState("Orange");
 
   const runningRef = useRef(running);
   runningRef.current = running;
@@ -72,90 +73,129 @@ const App = () => {
 
   return (
     <>
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: `repeat(${numCols}, 20px)`,
-        }}
-      >
-        {grid.map((rows, i) =>
-          rows.map((col, k) => (
-            <div
-              key={`${i}-${k}`}
-              onClick={() => {
-                const newGrid = produce(grid, (gridCopy) => {
-                  gridCopy[i][k] = grid[i][k] ? 0 : 1;
-                });
-                setGrid(newGrid);
-              }}
-              style={{
-                width: 20,
-                height: 20,
-                backgroundColor: grid[i][k] ? bgColor : undefined,
-                border: "solid 1px black",
-              }}
-            />
-          ))
-        )}
-      </div>
-      <button
-        onClick={() => {
-          setRunning(!running);
-          if (!running) {
-            runningRef.current = true;
-            runSim();
-          }
-        }}
-      >
-        {running ? "stop" : "start"}
-      </button>
-      <button
-        onClick={() => {
-          setGrid(generateEmptyGrid());
-        }}
-      >
-        clear
-      </button>
-      <button
-        onClick={() => {
-          const rows = [];
-          for (let i = 0; i < numRows; i++) {
-            rows.push(
-              Array.from(Array(numCols), () => (Math.random() > 0.7 ? 1 : 0))
-            );
-          }
+      <div>
+        <h1>Conway's Game of Life</h1>
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: `repeat(${numCols}, 20px)`,
+          }}
+        >
+          {grid.map((rows, i) =>
+            rows.map((col, k) => (
+              <div
+                key={`${i}-${k}`}
+                onClick={() => {
+                  const newGrid = produce(grid, (gridCopy) => {
+                    gridCopy[i][k] = grid[i][k] ? 0 : 1;
+                  });
+                  setGrid(newGrid);
+                }}
+                style={{
+                  width: 20,
+                  height: 20,
+                  backgroundColor: grid[i][k] ? bgColor : undefined,
+                  border: "solid 1px black",
+                }}
+              />
+            ))
+          )}
+        </div>
+        <button
+          onClick={() => {
+            setRunning(!running);
+            if (!running) {
+              runningRef.current = true;
+              runSim();
+            }
+          }}
+        >
+          {running ? "stop" : "start"}
+        </button>
+        <button
+          onClick={() => {
+            setGrid(generateEmptyGrid());
+          }}
+        >
+          clear
+        </button>
+        <button
+          onClick={() => {
+            const rows = [];
+            for (let i = 0; i < numRows; i++) {
+              rows.push(
+                Array.from(Array(numCols), () => (Math.random() > 0.7 ? 1 : 0))
+              );
+            }
 
-          setGrid(rows);
-        }}
-      >
-        random
-      </button>
-      <DropdownButton id="dropdown-basic-button" title="Choose a color">
-        <Dropdown.Item
-          href="#/action-1"
-          onClick={() => {
-            setBgColor("purple");
+            setGrid(rows);
           }}
         >
-          Purple
-        </Dropdown.Item>
-        <Dropdown.Item
-          href="#/action-2"
-          onClick={() => {
-            setBgColor("yellow");
-          }}
+          random
+        </button>
+        <DropdownButton
+          id="dropdown-basic-button"
+          title={bgColor === "Orange" ? "Choose a Color" : bgColor}
         >
-          Yellow
-        </Dropdown.Item>
-        <Dropdown.Item
-          href="#/action-3"
-          onClick={() => {
-            setBgColor("pink");
-          }}
-        >
-          Pink
-        </Dropdown.Item>
-      </DropdownButton>
+          <Dropdown.Item
+            href="#/action-1"
+            onClick={() => {
+              setBgColor("Orange");
+            }}
+          >
+            Default (Orange)
+          </Dropdown.Item>
+          <Dropdown.Item
+            href="#/action-2"
+            onClick={() => {
+              setBgColor("Purple");
+            }}
+          >
+            Purple
+          </Dropdown.Item>
+          <Dropdown.Item
+            href="#/action-3"
+            onClick={() => {
+              setBgColor("Yellow");
+            }}
+          >
+            Yellow
+          </Dropdown.Item>
+          <Dropdown.Item
+            href="#/action-4"
+            onClick={() => {
+              setBgColor("Pink");
+            }}
+          >
+            Pink
+          </Dropdown.Item>
+        </DropdownButton>
+      </div>
+
+      <div className="rules">
+        <h2>Rules:</h2>
+
+        <li>
+          Any live cell with fewer than two live neighbours dies, as if by
+          underpopulation.
+        </li>
+        <li>
+          Any live cell with two or three live neighbours lives on to the next
+          generation.
+        </li>
+        <li>
+          Any live cell with more than three live neighbours dies, as if by
+          overpopulation.
+        </li>
+        <li>
+          Any dead cell with exactly three live neighbours becomes a live cell,
+          as if by reproduction.
+        </li>
+      </div>
+
+      <div>
+        <h3>About this Algorithm:</h3>
+      </div>
     </>
   );
 };
