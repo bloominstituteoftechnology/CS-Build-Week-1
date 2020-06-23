4ea5777f49f5275625351a24394deb9a5fe1dbe6
evan <aspaasevan@gmail.com>
2020-06-23 09:58:09 -0700

    installed and wrapped react-router-dom around app

--- react/game_of_life/src/index.js
+++ react/game_of_life/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {BrowserRouter as Router} from 'react-router-dom';
 import App from './App';
 
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>,
+  </Router>,
   document.getElementById('root')
 );
 
