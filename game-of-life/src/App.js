358818c0fd0dbf1660a8043b6953c240601cf7fe
Tanner Williams <tannerwill756@gmail.com>
2020-06-23 11:23:23 -0400

    game logic

--- game-of-life/src/App.js
+++ game-of-life/src/App.js
@@ -31,6 +31,39 @@ const App = () => {
 
   const [running, setRunning] = useState(false);
 
+  const runningRef = useRef(running);
+  runningRef.current = running;
+
+  const runSim = useCallback(() => {
+    if (!renningRef.current) {
+      return;
+    }
+
+    setGrid((g) => {
+      return produce(g, (gridCopy) => {
+        for (let i = 0; i < numRows; i++) {
+          for (let k = 0; k < numCols; i++) {
+            let neighbors = 0;
+            operations.forEach(([x, y]) => {
+              const newI = i + x;
+              const newK = k + y;
+              if (newI >= 0 && newI < numRows && newK >= 0 && newK < numCols) {
+                neighbors += g[newI][newK];
+              }
+            });
+            if (neighbors < 2 || neighbors > 3) {
+              gridCopy[i][k] = 0;
+            } else if (g[i][k] === 0 && neighbors === 3) {
+              gridCopy[i][k] = 1;
+            }
+          }
+        }
+      });
+    });
+
+    setTimeout(runSim, 1000);
+  }, []);
+
   return (
     <>
       <button onClick={setRunning(!running)}>
