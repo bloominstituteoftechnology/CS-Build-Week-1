cc28a25b639a78f6b2925969fb9347fedae066f3
Mark Gerrior <github@gerrior.com>
2020-06-25 13:14:36 -0700

    Added custom GridView

---
+++ Game of Life/Game of Life/GridView.swift
@@ -0,0 +1,21 @@
+//
+//  GridView.swift
+//  Game of Life
+//
+//  Created by Mark Gerrior on 6/25/20.
+//  Copyright © 2020 Mark Gerrior. All rights reserved.
+//
+
+import UIKit
+
+class GridView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
