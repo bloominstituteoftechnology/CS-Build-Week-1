e17f032115802d974b5da5c6c59dad80c22c3fd7
Mark Gerrior <github@gerrior.com>
2020-06-24 21:23:34 -0700

    Base Project

---
+++ Game of Life/Game of Life/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Game of Life
+//
+//  Created by Mark Gerrior on 6/24/20.
+//  Copyright © 2020 Mark Gerrior. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
