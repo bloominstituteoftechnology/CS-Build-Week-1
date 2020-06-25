aac37e9dcbb30278691fb3be6847ff0a8f81a7ac
Mark Gerrior <github@gerrior.com>
2020-06-25 10:42:55 -0700

    Got Rules page working

---
+++ Game of Life/Game of Life/RulesViewController.swift
@@ -0,0 +1,22 @@
+//
+//  RulesViewController.swift
+//  Game of Life
+//
+//  Created by Mark Gerrior on 6/25/20.
+//  Copyright © 2020 Mark Gerrior. All rights reserved.
+//
+
+import UIKit
+
+class RulesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func okButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
