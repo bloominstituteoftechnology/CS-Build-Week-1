f3d8129b72b539731dc906361c045204eb880ec9
Chris Dobek <dobek.christopher@gmail.com>
2020-07-25 11:19:37 -0400

    Project Created with a very basic functioning storyboard

---
+++ Game of Life/Game of Life/View Controllers/GameOfLifeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Game of Life
+//
+//  Created by Chris Dobek on 7/25/20.
+//  Copyright © 2020 Chris Dobek. All rights reserved.
+//
+
+import UIKit
+
+class GameOfLifeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Game of Life/Game of Life/View Controllers/InfoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  InfoViewController.swift
+//  Game of Life
+//
+//  Created by Chris Dobek on 7/25/20.
+//  Copyright © 2020 Chris Dobek. All rights reserved.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
