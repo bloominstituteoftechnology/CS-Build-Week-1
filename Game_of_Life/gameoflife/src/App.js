983ea4066e78c8bbc1ca3636313f975d3a189639
Veto Ramirez <vetorobert3@gmail.com>
2020-08-22 10:13:16 -0700

    Made logic. Working on components and UI.

--- Game_of_Life/gameoflife/src/App.js
+++ Game_of_Life/gameoflife/src/App.js
@@ -3,16 +3,25 @@ import produce from 'immer'
 
 const Rows = 25;
 const Col = 25;
+// Used to find neighbors from a cell
+const surround = [
+    [0, 1],
+    [0, -1],
+    [1, 0],
+    [1, 1],
+    [1, -1],
+    [-1, 0],
+    [-1, 1],
+    [-1, -1]
+]
 
 function App() {
     // State for square toggle
     const [grid, setGrid] = useState(() => {
-        const rows = [];
-        
+        const rows = [];    
         for (let i = 0; i < Rows; i++) {
             rows.push(Array.from(Array(Col), () => 0));
         };
-
         return rows;
     });
 
@@ -26,18 +35,47 @@ function App() {
             return;
         }
         // simulate
-        for (let i = 0; i < Rows; i++) {
-            for ()
-        }
+        setGrid((g) => {
+            return produce(g, gridCopy => {
+                for (let i = 0; i < Rows; i++) {
+                    // checking for neighbors
+                    for (let j = 0; j < Col; j++) {
+                        let neighbors = 0;
+                        
+                            surround.forEach(([x, y]) => {
+                                const genI = i + x;
+                                const genJ = j + y;
+                                // making sure we are in the grib and not out of it
+                                if (genI >= 0 && genI < Rows && genJ >= 0 && genJ < Col) {
+                                    neighbors += g[genI][genJ]
+                                }
+                            })
+
+                            // determines if a cell dies or is born depending on above condition
+                            if (neighbors < 2 || neighbors > 3) {
+                                gridCopy[i][j] = 0;
+                            } else if (g[i][j] === 0 && neighbors === 3) {
+                                gridCopy[i][j] = 1;
+                            }                        
+                          
+                    }
+                }
+            })
+        })
+
         setTimeout(runSimulation, 1000);
     }, []);
 
     return (
         <>
-            // Button used to start and pause simulation
+            {/* Button used to start and pause simulation */}
             <button 
                 onClick={() => {
                     setStart(!start);
+                    if (!start) {
+                        runningRef.current = true;
+                        runSimulation();
+                    }
             }}>
                 {start ? 'pause' : 'start'}
             </button>
