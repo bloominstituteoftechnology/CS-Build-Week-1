7545992a68f5dbd3a91d2988fb797b7cbaa1c792
Devin Cooper <37602228+DevinDCooper@users.noreply.github.com>
2020-08-26 20:29:25 -0700

    Rules

--- game-of-life/src/Game.js
+++ game-of-life/src/Game.js
@@ -196,6 +196,12 @@ class Game extends React.Component {
                     }
                     <button className="button" onClick={this.handleRandom}>Random</button>
                     <button className="button" onClick={this.handleClear}>Clear</button>
+
+                    <h3>Rules:</h3>
+                    <p>1. Any live cell with fewer than two live neighbours dies, as if by underpopulation.</p>
+                    <p>2. Any live cell with two or three live neighbours lives on to the next generation.</p>
+                    <p>3. Any live cell with more than three live neighbours dies, as if by overpopulation.</p>
+                    <p>4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.</p>
                 </div>
             </div>
         );
