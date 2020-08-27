f5c130169b3dd48715e3126313df1166b6b4a04e
Devin Cooper <37602228+DevinDCooper@users.noreply.github.com>
2020-08-26 20:17:58 -0700

    CSS_done_build_done

---
+++ game-of-life/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+import Game from './Game';
+
+class App extends Component {
+
+    render() {
+
+        return (
+            <div className="App">
+                <h1>Dev's Game of Life</h1>
+                <Game />
+            </div>
+        );
+    }
+}
+
+export default App;
