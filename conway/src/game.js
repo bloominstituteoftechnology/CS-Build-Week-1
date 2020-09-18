7c441a27a328157d70c12579154d9f9fcdb84e71
Samuel Kraus <53798819+Skraus5628@users.noreply.github.com>
2020-09-18 12:16:13 -0500

    Set up clickable cells, added run/stop function button

--- conway/src/game.js
+++ conway/src/game.js
@@ -5,6 +5,21 @@ const CELL_SIZE = 20;
 const WIDTH = 800;
 const HEIGHT = 600;
 
+class Cell extends React.Component {
+
+    render() {
+        const { x, y } = this.props;
+        return (
+            <div className="Cell" style={{
+                left: `${CELL_SIZE * x + 1}px`,
+                top: `${CELL_SIZE * y + 1}px`,
+                width: `${CELL_SIZE - 1}px`,
+                height: `${CELL_SIZE - 1}px`,
+            }} />
+        );
+    }
+}
+
 class Game extends React.Component{
     constructor(){
         super();
@@ -16,8 +31,8 @@ class Game extends React.Component{
 
     state = {
         cells: [],
-        isRunning: false,
         interval: 100,
+        isRunning: false,
     }
 
     // Create an empty board
@@ -30,6 +45,18 @@ class Game extends React.Component{
             }
         }
         return board;
+    }
+
+    // Calculate the position of a board element
+
+    getElementOffset(){
+        const rect = this.boardRef.getBoundingClientRect();
+        const doc = document.documentElement;
+
+        return{
+            x: (rect.left + window.pageXOffset) - doc.clientLeft,
+            y: (rect.top + window.pageYOffset) - doc.clientTop,
+        };
     }
 
     // Create cells from this.Board
@@ -45,7 +72,40 @@ class Game extends React.Component{
         return cells;
     }
 
+    handleClick = (event) => {
+        const elemOffset = this.getElementOffset();
+        const offsetX = event.clientX - elemOffset.x;
+        const offsetY = event.clientY - elemOffset.y;
+
+        const x = Math.floor(offsetX / CELL_SIZE);
+        const y = Math.floor(offsetY / CELL_SIZE);
+
+        if (x >= 0 && x <= this.cols && y >= 0 && y <= this.rows){
+            this.board[y][x] = !this.board[y][x];
+        }
+
+        this.setState({ cells: this.makeCells() });
+    }
+
+// Game helpers
+
+    runGame = () =>{
+        this.setState({ isRunning: true });
+        this.runIteration();
+    }
+
+    stopGame = () =>{
+        this.setState({ isRunning: false });
+        if (this.timeoutHandler){
+            window.clearTimeout(this.timeoutHandler);
+            this.timeoutHandler = null;
+        }
+    }
+
+
+
     render(){
+        const { cells, interval, isRunning } =this.state;
         return (
             <div>
                 <div className="Board" 
@@ -54,6 +114,19 @@ class Game extends React.Component{
                         this.boardRef = n;
                     }}
                     >
+                    
+                    {cells.map(cell =>(
+                        <Cell x={cell.x} y={cell.y} key={`${cell.x}, ${cell.y}`}/>
+                    ))}
+                </div>
+
+                <div className="controls">
+                    Update every <input value={this.state.interval} onChange={this.handleIntervalChange}/> msec
+                    {isRunning ? 
+                    <button className="button" onClick={this.stopGame}>Stop</button> :
+                    <button className="button" onClick={this.runGame}>Run</button>
+                    }
+                
                 </div>
             </div>
         );
