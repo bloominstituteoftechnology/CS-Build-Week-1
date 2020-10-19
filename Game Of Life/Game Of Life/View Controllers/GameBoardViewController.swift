6981e36be29b2b943881441697350a51ebd5ed23
Jarren <jarrenmcampos@gmail.com>
2020-10-19 00:38:40 -0700

    new version of a dictionary of views

--- Game Of Life/Game Of Life/View Controllers/GameBoardViewController.swift
+++ Game Of Life/Game Of Life/View Controllers/GameBoardViewController.swift
@@ -8,20 +8,49 @@
 import UIKit
 
 class GameBoardViewController: UIViewController {
+    let numViewPerRow = 15
     
-    static var rowCount = 15
-    static var columnCount = 10
+    var cells = [String: UIView]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(make2DArray(rows: GameBoardViewController.rowCount, cols: GameBoardViewController.columnCount))
+        
+        let width = view.frame.width / CGFloat(numViewPerRow)
+                
+        for j in 0...20 {
+            for i in 0...numViewPerRow {
+                let cellView = UIView()
+                cellView.backgroundColor = .secondarySystemBackground
+                cellView.frame = CGRect(x: CGFloat(i) * width, y: CGFloat(j) * width, width: width, height: width)
+                cellView.layer.borderWidth = 0.5
+                cellView.layer.borderColor = UIColor.label.cgColor
+                view.addSubview(cellView)
+                
+                let key = "\(i)|\(j)"
+                cells[key] = cellView
+            }
+        }
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTouch)))
+//        cells["10|10"]?.backgroundColor = .red
     }
-    
-    func make2DArray(rows: Int, cols: Int) {
+    @objc func handleTouch(gesture: UIGestureRecognizer) {
+        let location = gesture.location(in: view)
+        print(location)
         
-        let arr = Matrix(rows: rows, columns: cols)
+        let width = view.frame.width / CGFloat(numViewPerRow)
         
-        print(arr)
+        let i = Int(location.x / width)
+        let j = Int(location.y / width)
 
+        print("Column: \(i)", "Row: \(j)")
+        
+        let key = "\(i)|\(j)"
+        let cellView = cells[key]
+        
+        if cellView?.backgroundColor == .label {
+            cellView?.backgroundColor = .secondarySystemBackground
+        } else {
+            cellView?.backgroundColor = .label
+        }
     }
 }
---
+++ Game Of Life/Game Of Life/View Controllers/SettingsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsViewController.swift
+//  Game Of Life
+//
+//  Created by Jarren Campos on 10/19/20.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
