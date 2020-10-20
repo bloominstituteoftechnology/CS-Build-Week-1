a4cc49036bb4e5aed65898aad2b21e563949f151
rfamilara <techknohauz.com>
2020-10-19 22:21:29 -0500

    Create code for fuction handle click -function handleCellClick

--- objectives/rules-game-life/src/index.js
+++ objectives/rules-game-life/src/index.js
@@ -5,3 +5,20 @@ var speed = 150;
 var running = false;
 var count = 0
 var gen = document.getElementById('counter')
+
+function handleCellClick(e){
+    if (!running){
+        var pos = e.target.id.split('r')
+        pos = pos[1].split('c')
+        pos[0] = parseInt(pos[0])
+        pos[1] = parseInt(pos[1])
+        if(canvasArr[pos[0]][pos[1]] === 0){
+            canvasArr[pos[0]][pos[1]] = 1
+        }else{
+            canvasArr[pos[0]][pos[1]] = 0
+        }
+
+        var cell = e.target
+        cell.classList.toggle('on')
+    }
+}
